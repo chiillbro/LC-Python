4b2f74d0929fee83824b8ee641883ecbd53a18d9
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-09 18:20:13 +0530

    Time: 2 ms (12.65%), Space: 17.9 MB (11.44%) - LeetHub

--- 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.py
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.py
@@ -1,9 +1,11 @@
 class Solution:
     def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
-        res = 0
+        # res = 0
 
-        for h in hours:
-            if h >= target:
-                res += 1
+        # for h in hours:
+        #     if h >= target:
+        #         res += 1
         
-        return res
+        # return res
+
+        return sum(1 for h in hours if h >= target)
