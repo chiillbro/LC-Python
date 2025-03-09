30206def4601defbbbef81331cb5b37eef1da98b
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-09 11:37:48 +0530

    Time: 662 ms (98.34%), Space: 21.2 MB (90.7%) - LeetHub

--- 3483-alternating-groups-ii/3483-alternating-groups-ii.py
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.py
@@ -17,20 +17,36 @@ class Solution:
 
         # return count
 
-        alt = [0] * N
+        # alt = [0] * N
 
-        for i in range(N):
-            alt[i] = 1 if colors[i] != colors[(i+1)%N] else 0
+        # for i in range(N):
+        #     alt[i] = 1 if colors[i] != colors[(i+1)%N] else 0
         
-        prefix = [0] * (N + k + 1)
+        # prefix = [0] * (N + k + 1)
 
-        for i in range(N + k):
-            prefix[i+1] = prefix[i] + alt[i % N]
+        # for i in range(N + k):
+        #     prefix[i+1] = prefix[i] + alt[i % N]
         
-        for i in range(N):
-            s = prefix[i + k - 1] - prefix[i]
+        # for i in range(N):
+        #     s = prefix[i + k - 1] - prefix[i]
 
-            if s == k-1:
-                count += 1
+        #     if s == k-1:
+        #         count += 1
+
+        l = []
+
+        for i in range(N-1):
+            if colors[i] == colors[i+1]:
+                l.append(i)
+        
+        if colors[N-1] == colors[0]:
+            l.append(N-1)
+        if len(l) == 0:
+            return N
+        
+        l.append(l[0] + N)
+
+        for i in range(len(l) - 1):
+            count += max(0, l[i+1] - l[i] - k + 1)
         
         return count
