693d88118732ee79e2f476dd89cbd398392a5406
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-09 11:07:14 +0530

    Time: 809 ms (13.95%), Space: 28.2 MB (5.32%) - LeetHub

---
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.py
@@ -0,0 +1,36 @@
+class Solution:
+    def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
+        N = len(colors)
+        count = 0
+
+        # for i in range(N):
+        #     current_window = [colors[(i + j) % N] for j in range(k)]
+
+        #     is_alternating = True
+        #     for j in range(1, k - 1):
+        #         if current_window[j] == current_window[j-1] or current_window[j] == current_window[j+1]:
+        #             is_alternating = False
+        #             break
+            
+        #     if is_alternating:
+        #         count += 1
+
+        # return count
+
+        alt = [0] * N
+
+        for i in range(N):
+            alt[i] = 1 if colors[i] != colors[(i+1)%N] else 0
+        
+        prefix = [0] * (2*N + 1)
+
+        for i in range(2*N):
+            prefix[i+1] = prefix[i] + alt[i % N]
+        
+        for i in range(N):
+            s = prefix[i + k - 1] - prefix[i]
+
+            if s == k-1:
+                count += 1
+        
+        return count
