1db0bc6b728aec07fa4d0fb165b83966f636b71c
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-11 21:08:31 +0530

    Time: 813 ms (98.27%), Space: 18.6 MB (67.63%) - LeetHub

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
@@ -1,56 +1,62 @@
 class Solution:
     def countOfSubstrings(self, word: str, k: int) -> int:
         N = len(word)
-        # vowel_set = {'a', 'e', 'i', 'o', 'u'}
+        vowel_set = {'a', 'e', 'i', 'o', 'u'}
 
-        # freq_map = defaultdict(int)
-        # cons_count = 0
-        # left = res = 0
+        freq_map = defaultdict(int)
+        cons_count = low = high = res = 0
 
-        # for right in range(N):
-        #     char = word[right]
-        #     freq_map[char] += 1
-        #     if char not in vowel_set:
-        #         cons_count += 1
+        for c in word:
+            if c in vowel_set:
+                freq_map[c] += 1
+            else:
+                cons_count += 1
             
-        #     while cons_count > k:
-        #         freq_map[word[left]] -= 1
-        #         if word[left] not in vowel_set:
-        #             cons_count -= 1
-        #         left += 1
-                
-        #     if cons_count == k and all(freq_map[v] > 0 for v in vowel_set):
-        #         temp = left
-        #         char_map = [0] * 26
-        #         while (temp <= right and 
-        #             word[temp] in vowel_set and 
-        #             (freq_map[word[temp]] + char_map[ord(word[temp]) - ord('a')])  > 1):
-        #             char_map[ord(word[temp]) - ord('a')] += 1
-        #             temp += 1
-        #         res += (temp - left + 1)
-
-        # return res
-
-        def atleast(k):
-            vowel = defaultdict(int)
-            res = non_vowel = l = 0
-            
-            for r in range(N):
-                if word[r] in 'aeiou':
-                    vowel[word[r]] += 1
+            while cons_count > k:
+                if word[high] in vowel_set:
+                    freq_map[word[high]] -= 1
+                    if freq_map[word[high]] == 0:
+                        freq_map.pop(word[high])
                 else:
-                    non_vowel += 1
+                    cons_count -= 1
                 
-                while len(vowel) == 5 and non_vowel >= k:
-                    res += N - r
-                    if word[l] in 'aeiou':
-                        vowel[word[l]] -= 1
-                        if vowel[word[l]] == 0:
-                            vowel.pop(word[l])
-                    else:
-                        non_vowel -= 1
-                    
-                    l += 1
-            return res
+                high += 1
+                low = high
+                
+            while cons_count == k and high < N:
+                if word[high] in vowel_set and freq_map[word[high]] > 1:
+                    freq_map[word[high]] -= 1
+                    high += 1
+                else:
+                    break
+            
+            if cons_count == k and len(freq_map) == 5:
+                res += (high - low + 1)
 
-        return atleast(k) - atleast(k+1)
+        return res
+
+        # ** Second Approach **
+
+        # def atleast(k):
+        #     vowel = defaultdict(int)
+        #     res = non_vowel = l = 0
+            
+        #     for r in range(N):
+        #         if word[r] in 'aeiou':
+        #             vowel[word[r]] += 1
+        #         else:
+        #             non_vowel += 1
+                
+        #         while len(vowel) == 5 and non_vowel >= k:
+        #             res += N - r
+        #             if word[l] in 'aeiou':
+        #                 vowel[word[l]] -= 1
+        #                 if vowel[word[l]] == 0:
+        #                     vowel.pop(word[l])
+        #             else:
+        #                 non_vowel -= 1
+                    
+        #             l += 1
+        #     return res
+
+        # return atleast(k) - atleast(k+1)
