5fdd55932c85b1f2f18834fa9b5e5f68e17e4220
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-13 07:21:52 +0530

    Time: 147 ms (88.99%), Space: 63.3 MB (56.4%) - LeetHub

--- 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.py
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.py
@@ -18,35 +18,54 @@ class Solution:
         
         # return -1 if track_map else res
 
-        # ** Second Approach Binary Search and Difference Array **
-        left, right = 0, len(queries)
-        if not self.canFormZeroArray(nums, queries, right):
-            return -1
+        # ** Second Approach Better -> Binary Search and Difference Array **
+        # left, right = 0, len(queries)
+        # if not self.canFormZeroArray(nums, queries, right):
+        #     return -1
         
-        while left <= right:
-            mid = left + (right - left) // 2
+        # while left <= right:
+        #     mid = left + (right - left) // 2
 
-            if self.canFormZeroArray(nums, queries, mid):
-                right = mid - 1
-            else:
-                left = mid + 1
+        #     if self.canFormZeroArray(nums, queries, mid):
+        #         right = mid - 1
+        #     else:
+        #         left = mid + 1
         
-        return left
+        # return left
 
-    def canFormZeroArray(self, nums, queries, k):
-        N = len(nums)
+        # ** Third Approach Optimal -> Line Sweep Technique **
         total_sum = 0
-        difference_sum = [0] * (N + 1)
+        difference_array = [0] * (N+1)
+        k = 0
 
-        for query_idx in range(k):
-            l, r, v = queries[query_idx]
-            difference_sum[l] += v
-            difference_sum[r+1] -= v
+        for i in range(N):
+            while total_sum + difference_array[i] < nums[i]:
+                k += 1
+                if k > len(queries):
+                    return -1
+                
+                l, r, v = queries[k-1]
+                if r >= i:
+                    difference_array[max(l, i)] += v
+                    difference_array[r + 1] -= v
+                
+            total_sum += difference_array[i]
         
-        for num_idx in range(N):
-            total_sum += difference_sum[num_idx]
-            if total_sum < nums[num_idx]:
-                return False
-        return True
+        return k
+    # def canFormZeroArray(self, nums, queries, k):
+    #     N = len(nums)
+    #     total_sum = 0
+    #     difference_sum = [0] * (N + 1)
+
+    #     for query_idx in range(k):
+    #         l, r, v = queries[query_idx]
+    #         difference_sum[l] += v
+    #         difference_sum[r+1] -= v
+        
+    #     for num_idx in range(N):
+    #         total_sum += difference_sum[num_idx]
+    #         if total_sum < nums[num_idx]:
+    #             return False
+    #     return True
 
 
