509096c16e5a821de4ce4b203eb6c4f701cf24e3
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-16 13:57:41 +0530

    Time: 24 ms (95.21%), Space: 21.8 MB (84.79%) - LeetHub

--- 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.py
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.py
@@ -1,27 +1,27 @@
 class Solution:
     def repairCars(self, ranks: List[int], cars: int) -> int:
         N = len(ranks)
-        # maxRank = max(ranks)
+        maxRank = max(ranks)
 
         # ranks.sort()
         # This is a brilliant approach to create freq array to use ranks in sorted fashion without actually sorting(TC O(n logn)) it
-        # freq = [0] * (maxRank + 1)
-        # for rank in ranks:
-        #     freq[rank] += 1
+        freq = [0] * (maxRank + 1)
+        for rank in ranks:
+            freq[rank] += 1
 
         def canRepair(time):
             cars_repaired = 0
             
-            # for rank in range(1, maxRank + 1):
-            #     if freq[rank]:
-            #         cars_repaired += freq[rank] * int(math.sqrt(time // rank))
-            for rank in ranks:
-                cars_repaired += int((time // rank) ** 0.5)
+            for rank in range(1, maxRank + 1):
+                if freq[rank]:
+                    cars_repaired += freq[rank] * int((time // rank) ** 0.5)
+            # for rank in ranks:
+            #     cars_repaired += int((time // rank) ** 0.5)
             
             return cars_repaired >= cars
 
-        # low, high = 1, min(ranks) * (cars * cars)
-        low, high = 1, cars * cars * ranks[0]
+        low, high = 1, min(ranks) * (cars * cars)
+        # low, high = 1, cars * cars * ranks[0]
 
         while low <= high:
             mid = (high + low) >> 1
