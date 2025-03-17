366bbb202fdc7955aae025a02e6aaec4a0cfd6b5
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-17 09:20:47 +0530

    Time: 3 ms (70.74%), Space: 18 MB (16.13%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.py
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.py
@@ -2,24 +2,23 @@ class Solution:
     def removeOuterParentheses(self, s: str) -> str:
         stack = []
 
-        start_cnt = end_cnt = 0
+        depth = 0
 
         curStart = -1
 
         for i, p in enumerate(s):
             if p == ')':
-                if (not stack or stack[-1] == ')') and start_cnt == end_cnt:
+                if (not stack or stack[-1] == ')') and not depth:
                     curStart = -1
-                    end_cnt = 0
+                    depth = 0
                 else:
-                    end_cnt += 1
+                    depth -= 1
                     stack.append(p)
             else:
                 if curStart == -1:
                     curStart = i
-                    start_cnt = 0
                 else:
-                    start_cnt += 1
+                    depth += 1
                     stack.append(p)
 
         return ''.join(stack)
