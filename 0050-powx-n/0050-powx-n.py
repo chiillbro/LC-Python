30522aba0199c1b6a84a52612f9f6dd3ebf53813
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-19 21:53:59 +0530

    Time: 0 ms (100%), Space: 17.8 MB (74.61%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -5,13 +5,31 @@ class Solution:
         if not x:
             return 0
         extra = 1
-        sign = 0 if n < 0 else 1
-        n = abs(n)
-        while n > 0:
+        # sign = 0 if n < 0 else 1
+        # n = abs(n)
+        # while n > 0:
+        #     if n & 1:
+        #         extra *= x
+        #         n -= 1
+        #     else:
+        #         x *= x
+        #         n >>= 1
+        # return 1 / extra if not sign else extra
+
+
+        def calculate(x, n):
+            nonlocal extra
+            if n == 0:
+                return
+            
             if n & 1:
                 extra *= x
                 n -= 1
             else:
                 x *= x
                 n >>= 1
-        return 1 / extra if not sign else extra
+            
+            calculate(x, n)
+
+        calculate(x, abs(n))
+        return extra if n > 0 else 1 / extra
