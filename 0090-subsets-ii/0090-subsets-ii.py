6e5ff98b0daca5e85a6da8a27daa6a9261d7c8d8
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-19 06:50:02 +0530

    Time: 0 ms (100%), Space: 18.1 MB (36.88%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -4,11 +4,27 @@ class Solution:
         res = set()
         n = len(nums)
 
-        for mask in range(1 << n):
-            subset = []
+        def backtrack(i : int, sol : List[int]):
+            if i == n:
+                res.add(tuple(sol))
+                return
+            
+            backtrack(i + 1, sol)
+            sol.append(nums[i])
+            backtrack(i + 1, sol)
+            sol.pop()
+        
+        backtrack(0, [])
 
-            for i in range(n):
-                if mask & (1 << i):
-                    subset.append(nums[i])
-            res.add(tuple(subset))
         return [list(t) for t in res]
+
+        # for mask in range(1 << n):
+        #     subset = []
+
+        #     for i in range(n):
+        #         if mask & (1 << i):
+        #             subset.append(nums[i])
+        #     res.add(tuple(subset))
+        # return [list(t) for t in res]
+
+        
