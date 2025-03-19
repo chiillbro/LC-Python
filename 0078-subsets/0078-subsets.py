ed0ab234c15ccbae792702c893fda07d96166e74
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-19 06:33:24 +0530

    Time: 0 ms (100%), Space: 17.8 MB (65.1%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,28 +1,28 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res, sol = [], []
+        res = []
         n = len(nums)
 
         # ** Approach one : using Backtracking **
-        # def backtrack(i):
-        #     if i == n:
-        #         res.append(sol.copy())
-        #         return
-        #     backtrack(i + 1)
+        def backtrack(i : int, sol : List[int] = []):
+            if i == n:
+                res.append(sol[:])
+                return
+            backtrack(i + 1)
 
-        #     sol.append(nums[i])
-        #     backtrack(i + 1)
-        #     sol.pop()
+            sol.append(nums[i])
+            backtrack(i + 1)
+            sol.pop()
         
-        # backtrack(0)
+        backtrack(0)
 
         ## Approach two : Using Bit Manipulation
 
-        for mask in range(1 << n):
-            subset = []
+        # for mask in range(1 << n):
+        #     subset = []
 
-            for i in range(n):
-                if mask & (1 << i):
-                    subset.append(nums[i])
-            res.append(subset)
+        #     for i in range(n):
+        #         if mask & (1 << i):
+        #             subset.append(nums[i])
+        #     res.append(subset)
         return res
