a6b405813a96b39c7643d62993b791735ece4bbd
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-19 06:56:41 +0530

    Time: 0 ms (100%), Space: 18 MB (57%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,23 +1,21 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        res = set()
+        res = []
         n = len(nums)
 
-        def backtrack(i : int, sol : List[int]):
-            if i == n:
-                res.add(tuple(sol))
-                return
-            
-            backtrack(i + 1, sol)
-            sol.append(nums[i])
-            backtrack(i + 1, sol)
-            sol.pop()
+        def backtrack(start : int, sol : List[int]):
+            res.append(sol[:])
+            for i in range(start, n):
+                if i > start and nums[i] == nums[i-1]:
+                    continue
+                
+                sol.append(nums[i])
+                backtrack(i + 1, sol)
+                sol.pop()
         
         backtrack(0, [])
-
-        return [list(t) for t in res]
-
+        return res
         # for mask in range(1 << n):
         #     subset = []
 
