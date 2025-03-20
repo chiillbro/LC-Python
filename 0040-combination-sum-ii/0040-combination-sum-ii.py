748594af33ba1b01f84f7402eb0458c6760d2e5f
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-20 21:41:36 +0530

    Time: 3 ms (91.7%), Space: 17.9 MB (57.1%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        res = []
+        n = len(candidates)
+        def calculateSum(curRem, i, sol):
+            if curRem == 0:
+                res.append(sol[:])
+                return
+            elif i == n: return
+
+            for j in range(i, n):
+                cur = candidates[j]
+                if j > i and cur == candidates[j-1]: continue
+                if cur > curRem:
+                    break
+                sol.append(cur)
+                calculateSum(curRem - cur, j + 1, sol)
+                sol.pop()
+        
+        calculateSum(target, 0, [])
+        return res
