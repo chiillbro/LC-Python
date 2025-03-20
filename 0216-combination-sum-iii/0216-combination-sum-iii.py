c38eb8046a5316954f49972aa969f574b4f3a0b3
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-20 21:57:33 +0530

    Time: 1 ms (34.96%), Space: 17.8 MB (59.83%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        nums = list(range(1, 10))
+        res = []
+
+        def calculateSum(curRem, i, cnt, sol):
+            if cnt == k and curRem == 0:
+                res.append(sol[:])
+                return
+            elif i == 9 or cnt > k:
+                return
+            
+            if nums[i] <= curRem and cnt < k:
+                sol.append(nums[i])
+                calculateSum(curRem - nums[i], i + 1, cnt + 1, sol)
+                sol.pop()
+                calculateSum(curRem, i + 1, cnt, sol)
+            
+        calculateSum(n, 0, 0, [])
+        return res
