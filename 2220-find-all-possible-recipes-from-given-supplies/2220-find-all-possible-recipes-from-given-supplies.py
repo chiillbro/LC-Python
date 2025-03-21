6edb3f70b21b9b4a48a0bf94ac9e4580d3cca01f
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-21 09:14:06 +0530

    Time: 115 ms (95.44%), Space: 27.6 MB (6.43%) - LeetHub

--- 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.py
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.py
@@ -1,6 +1,6 @@
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
-        res = []
+        # res = []
         # supplies_set = set(supplies)
 
         # for i, recipe in enumerate(recipes):
@@ -14,20 +14,37 @@ class Solution:
         #         supplies_set.add(recipe)
         #         res.append(recipe)
 
-        available = set(supplies)
-        recipe_queue = deque([i for i in range(len(recipes))])
-        last_size = -1
+        # ** Typical BFS Approach **
 
-        while len(available) > last_size:
-            last_size = len(available)
-            queue_size = len(recipe_queue)
-            while queue_size:
-                queue_size -= 1
-                recipe_idx = recipe_queue.popleft()
-                if all(ingredient in available for ingredient in ingredients[recipe_idx]):
-                    available.add(recipes[recipe_idx])
-                    res.append(recipes[recipe_idx])
-                else:
-                    recipe_queue.append(recipe_idx)
+        # available = set(supplies)
+        # recipe_queue = deque([i for i in range(len(recipes))])
+        # last_size = -1
 
-        return res
+        # while len(available) > last_size:
+        #     last_size = len(available)
+        #     queue_size = len(recipe_queue)
+        #     while queue_size:
+        #         queue_size -= 1
+        #         recipe_idx = recipe_queue.popleft()
+        #         if all(ingredient in available for ingredient in ingredients[recipe_idx]):
+        #             available.add(recipes[recipe_idx])
+        #             res.append(recipes[recipe_idx])
+        #         else:
+        #             recipe_queue.append(recipe_idx)
+
+        # ** Typical DFS Approach **
+
+        can_make = {supp : True for supp in supplies}
+        recipe_to_idx = {recipe : idx for idx, recipe in enumerate(recipes)}
+
+        def checkRecipe(recipe, visited):
+            if can_make.get(recipe, False): return True
+            if recipe in visited or recipe not in recipe_to_idx: return False
+
+            visited.add(recipe)
+
+            can_make[recipe] = all(checkRecipe(ingre, visited) for ingre in ingredients[recipe_to_idx[recipe]])
+
+            return can_make[recipe]
+
+        return [recipe for recipe in recipes if checkRecipe(recipe, set())]
