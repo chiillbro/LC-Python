3aab658ed8ecedbe9ed528bcd5bab93118915bd5
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-22 09:30:42 +0530

    Time: 53 ms (49.9%), Space: 18.4 MB (53.54%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.py
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.py
@@ -1,22 +1,55 @@
 class Solution:
     def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        # ** Approach 1 : Adjacency List **
+        # graph = [[vertex] for vertex in range(n)]
+        # adjacency_list = defaultdict(int)
 
-        graph = [[vertex] for vertex in range(n)]
-        adjacency_list = defaultdict(int)
+        # for u, v in edges:
+        #     graph[u].append(v)
+        #     graph[v].append(u)
+        
+
+        # for vertex in range(n):
+        #     neighbors = tuple(sorted(graph[vertex]))
+        #     adjacency_list[neighbors] += 1
+        
+
+        # return sum(
+        #     1 for neighbors, freq in adjacency_list.items()
+        #     if len(neighbors) == freq
+        # )
+
+
+        # ** Approach 2 : Traditional Graph techniques DFS **
+
+        adjacency_list = defaultdict(list)
 
         for u, v in edges:
-            graph[u].append(v)
-            graph[v].append(u)
+            adjacency_list[u].append(v)
+            adjacency_list[v].append(u)
+        
+        visited = set()
+        complete_count = 0
+
+        def _dfs(curr : int, info : List[int]):
+            visited.add(curr)
+            info[0] += 1
+            info[1] += len(adjacency_list[curr])
+
+            for neighbor in adjacency_list[curr]:
+                if neighbor not in visited:
+                    _dfs(neighbor, info)
         
 
         for vertex in range(n):
-            neighbors = tuple(sorted(graph[vertex]))
-            adjacency_list[neighbors] += 1
-        
+            if vertex not in visited:
+                info = [0, 0]
+                _dfs(vertex, info)
 
-        return sum(
-            1 for neighbors, freq in adjacency_list.items()
-            if len(neighbors) == freq
-        )
+                if info[0] * (info[0] - 1) == info[1]:
+                    complete_count += 1
+            
+        return complete_count
+
         
         
