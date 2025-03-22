62c41651bc6393737aa07ea966e067aa9e1f209f
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-22 09:41:40 +0530

    Time: 42 ms (73.81%), Space: 18.4 MB (53.54%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.py
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.py
@@ -20,36 +20,74 @@ class Solution:
         # )
 
 
+
         # ** Approach 2 : Traditional Graph techniques DFS **
 
+        # adjacency_list = defaultdict(list)
+
+        # for u, v in edges:
+        #     adjacency_list[u].append(v)
+        #     adjacency_list[v].append(u)
+        
+        # visited = set()
+        # complete_count = 0
+
+        # def _dfs(curr : int, info : List[int]):
+        #     visited.add(curr)
+        #     info[0] += 1
+        #     info[1] += len(adjacency_list[curr])
+
+        #     for neighbor in adjacency_list[curr]:
+        #         if neighbor not in visited:
+        #             _dfs(neighbor, info)
+        
+
+        # for vertex in range(n):
+        #     if vertex not in visited:
+        #         info = [0, 0]
+        #         _dfs(vertex, info)
+
+        #         if info[0] * (info[0] - 1) == info[1]:
+        #             complete_count += 1
+            
+        # return complete_count
+
+
+
+        # ** Approach 3 : Traditional Graph techniques BFS **
+        
         adjacency_list = defaultdict(list)
 
         for u, v in edges:
             adjacency_list[u].append(v)
             adjacency_list[v].append(u)
         
-        visited = set()
-        complete_count = 0
-
-        def _dfs(curr : int, info : List[int]):
-            visited.add(curr)
-            info[0] += 1
-            info[1] += len(adjacency_list[curr])
-
-            for neighbor in adjacency_list[curr]:
-                if neighbor not in visited:
-                    _dfs(neighbor, info)
-        
+        visited, complete_count = set(), 0
 
         for vertex in range(n):
-            if vertex not in visited:
-                info = [0, 0]
-                _dfs(vertex, info)
-
-                if info[0] * (info[0] - 1) == info[1]:
-                    complete_count += 1
+            if vertex in visited:
+                continue
             
-        return complete_count
+            component = []
+            queue = deque([vertex])
+            visited.add(vertex)
+
+            while queue:
+                curr = queue.popleft()
+                component.append(curr)
+
+                for neighbor in adjacency_list[curr]:
+                    if neighbor not in visited:
+                        queue.append(neighbor)
+                        visited.add(neighbor)
+                
+            is_complete = True
+            for node in component:
+                if len(adjacency_list[node]) != len(component) - 1:
+                    is_complete = False
+                    break
+            
+            complete_count += 1 if is_complete else 0
 
         
-        
+        return complete_count
