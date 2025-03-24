b2ebee57a413fb5664bd2e8099cd2e2b9f6eacb6
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-24 09:21:34 +0530

    Time: 3 ms (20.4%), Space: 18.3 MB (86.75%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -9,32 +9,32 @@ class Solution:
         if not root: return []
 
         # ** Iterative Approach using double-ended queue ** #
-        # res = []
-        # queue = deque([root])
-
-        # while queue:
-        #     level = []
-        #     for _ in range(len(queue)):
-        #         node = queue.popleft()
-        #         level.append(node.val)
-        #         if node.left:
-        #             queue.append(node.left)
-        #         if node.right:
-        #             queue.append(node.right)
-        #     res.append(level)
-        # return res
-
         res = []
-        self._helper(root, 0, res)
+        queue = deque([root])
+
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(level)
         return res
+
+    #     res = []
+    #     self._helper(root, 0, res)
+    #     return res
     
-    def _helper(self, node: Optional[TreeNode], level: int, res: List[List[int]]) -> None:
-        if not node:
-            return
+    # def _helper(self, node: Optional[TreeNode], level: int, res: List[List[int]]) -> None:
+    #     if not node:
+    #         return
         
-        if len(res) <= level:
-            res.append([])
+    #     if len(res) <= level:
+    #         res.append([])
         
-        res[level].append(node.val)
-        self._helper(node.left, level + 1, res)
-        self._helper(node.right, level + 1, res)
+    #     res[level].append(node.val)
+    #     self._helper(node.left, level + 1, res)
+    #     self._helper(node.right, level + 1, res)
