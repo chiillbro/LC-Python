0339c3aa3be2813456c70a865d890671df79caab
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-24 06:39:51 +0530

    Time: 191 ms (45.34%), Space: 52.8 MB (79.34%) - LeetHub

--- 3430-count-days-without-meetings/3430-count-days-without-meetings.py
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.py
@@ -1,34 +1,32 @@
 class Solution:
     def countDays(self, days: int, meetings: List[List[int]]) -> int:
-        merged_meetings = self._mergeIntervals(sorted(meetings))
+        meetings.sort()
 
-        res = 0
-        end = 0
+        res = meetings[0][0] - 1
+        prev_start, prev_end = meetings[0][0], meetings[0][1]
         # [[2, 8]]
         # [[1, 3], [5, 7], [9, 10]]
 
-        for cur_s, cur_e in merged_meetings:
-            if cur_s > end:
-                res += cur_s - end - 1
-            end = cur_e
+        for cur_s, cur_e in meetings[1:]:
+            if cur_s > prev_end + 1:
+                res += cur_s - prev_end - 1
+            prev_end = max(cur_e, prev_end)
         
-        s = merged_meetings[-1][1]
-        if s < days:
-            res += days - s
+        if prev_end < days:
+            res += days - prev_end
         
-
         return res
 
 
-    def _mergeIntervals(self, intervals: List[List[int]]) -> List[List[int]]:
-        merged_intervals = [intervals[0]]
+    # def _mergeIntervals(self, intervals: List[List[int]]) -> List[List[int]]:
+    #     merged_intervals = [intervals[0]]
 
-        for cur_s, cur_e in intervals[1:]:
-            prev_s, prev_e = merged_intervals[-1]
+    #     for cur_s, cur_e in intervals[1:]:
+    #         prev_s, prev_e = merged_intervals[-1]
 
-            if cur_s <= prev_e:
-                merged_intervals[-1] = [min(cur_s, prev_s), max(cur_e, prev_e)]
-            else:
-                merged_intervals.append([cur_s, cur_e])
+    #         if cur_s <= prev_e:
+    #             merged_intervals[-1] = [min(cur_s, prev_s), max(cur_e, prev_e)]
+    #         else:
+    #             merged_intervals.append([cur_s, cur_e])
         
-        return merged_intervals
+    #     return merged_intervals
