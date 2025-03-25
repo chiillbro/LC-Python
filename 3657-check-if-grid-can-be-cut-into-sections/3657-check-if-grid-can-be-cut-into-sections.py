28c79b768da3891bd4830695d037a66fdc4a8b9e
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-25 06:31:13 +0530

    Time: 635 ms (25%), Space: 83.5 MB (82.43%) - LeetHub

--- 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.py
+++ 3657-check-if-grid-can-be-cut-into-sections/3657-check-if-grid-can-be-cut-into-sections.py
@@ -9,17 +9,16 @@ class Solution:
 
         y_intervals.sort(); x_intervals.sort()
 
-        return max(self._mergeIntervals(x_intervals)[1], self._mergeIntervals(y_intervals)[1]) >= 3
+        return max(self._mergeIntervals(x_intervals), self._mergeIntervals(y_intervals)) >= 2
     
     def _mergeIntervals(self, intervals: List[List[int]]) -> List[List[int]]:
-        merged = [intervals[0]]
+        cuts = 0
+        prev_e = intervals[0][1]
 
         for cur_s, cur_e in intervals[1:]:
-            prev_s, prev_e = merged[-1]
-
-            if cur_s < prev_e:
-                merged[-1] = [min(cur_s, prev_s), max(cur_e, prev_e)]
-            else:
-                merged.append([cur_s, cur_e])
+            if prev_e <= cur_s:
+                cuts += 1
+            
+            prev_e = max(prev_e, cur_e)
         
-        return merged, len(merged)
+        return cuts
