cd8ef957291c320f8d990170a7ee20be3593afc9
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-26 22:21:34 +0530

    Time: 0 ms (100%), Space: 17.8 MB (54.8%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -22,5 +22,30 @@ class Solution:
         # return res
 
         # ** Recursive Approach ** #
-        return [] if not root else self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
+        # return [] if not root else self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
 
+
+        # ** Space Optimized Approach: Morris Traversal ** #
+
+        res = []
+
+        cur = root
+        while cur:
+            if not cur.left:
+                res.append(cur.val)
+                cur = cur.right
+            else:
+                prev = cur.left
+                while prev.right and prev.right != cur:
+                    prev = prev.right
+                
+                if not prev.right:
+                    prev.right = cur
+                    cur = cur.left
+                else:
+                    prev.right = None
+                    res.append(cur.val)
+                    cur = cur.right
+            
+        
+        return res
