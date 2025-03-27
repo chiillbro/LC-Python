64170725287654dfc5dd41b699071394e1d64a5d
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-27 10:36:32 +0530

    Time: 0 ms (100%), Space: 19.8 MB (31.1%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -6,39 +6,39 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        # prev: Optiona[TreeNode] = None
+        prev: Optiona[TreeNode] = None
 
-        # def helper(node: Optional[TreeNode]) -> bool:
-        #     nonlocal prev
-        #     if not node:
-        #         return True
+        def helper(node: Optional[TreeNode]) -> bool:
+            nonlocal prev
+            if not node:
+                return True
             
-        #     if not helper(node.left):
-        #         return False
+            if not helper(node.left):
+                return False
             
-        #     if prev and prev.val >= node.val:
-        #         return False
+            if prev and prev.val >= node.val:
+                return False
             
-        #     prev = node
+            prev = node
 
-        #     return helper(node.right)
+            return helper(node.right)
 
-        # return helper(root)
+        return helper(root)
 
         # ** Second Approach: check recursively whether a node's val is in the range of values it should be 
 
-        def helper(node: Optional[TreeNode], cur_min, cur_max) -> bool:
-            if not node: return True
+        # def helper(node: Optional[TreeNode], cur_min, cur_max) -> bool:
+        #     if not node: return True
 
-            if not (cur_min < node.val < cur_max):
-                return False
+        #     if not (cur_min < node.val < cur_max):
+        #         return False
 
-            if not helper(node.left, cur_min, node.val):
-                return False
+        #     if not helper(node.left, cur_min, node.val):
+        #         return False
             
-            if not helper(node.right, node.val, cur_max): 
-                return False
+        #     if not helper(node.right, node.val, cur_max): 
+        #         return False
             
-            return True
+        #     return True
             
-        return helper(root, float('-inf'), float('inf'))
+        # return helper(root, float('-inf'), float('inf'))
