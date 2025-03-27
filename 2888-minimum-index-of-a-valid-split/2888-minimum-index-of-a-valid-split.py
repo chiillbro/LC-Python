6000328704cdef5ed4efcb5f3781a57f34fc5bbf
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-27 08:04:48 +0530

    Time: 51 ms (82.18%), Space: 32.1 MB (80.2%) - LeetHub

--- 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.py
+++ 2888-minimum-index-of-a-valid-split/2888-minimum-index-of-a-valid-split.py
@@ -1,20 +1,37 @@
 class Solution:
     def minimumIndex(self, nums: List[int]) -> int:
         N = len(nums)
-        count = Counter(nums)
+        # count = Counter(nums)
 
-        dominant = -1
+        candidate = None
+        count = 0
 
-        for key, val in count.items():
-            if val << 1 > N:
-                dominant = key
-                break
+        for num in nums:
+            if not count:
+                candidate = num
+                count = 1
+            
+            elif num == candidate:
+                count += 1
+            else:
+                count -= 1
         
-        if dominant == -1:
+        total = sum(1 for num in nums if num == candidate)
+        if total << 1 <= N:
             return -1
         
+        dominant = candidate
+
+        # for key, val in count.items():
+        #     if val << 1 > N:
+        #         dominant = key
+        #         break
+        
+        # if dominant == -1:
+        #     return -1
+        
         left_count = 0
-        right_count = count[dominant]
+        right_count = total
 
         for i in range(N-1):
             if nums[i] == dominant:
