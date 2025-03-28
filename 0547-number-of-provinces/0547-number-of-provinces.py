37711b97170fb4897f3bd6172ed19a75c0baef4c
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-28 22:37:15 +0530

    Time: 3 ms (89.88%), Space: 18.9 MB (69.15%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -2,19 +2,38 @@ class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         connected_components = 0
         visited = set()
-
         n = len(isConnected)
 
-        def dfs(node):
+        # ** DFS Approach ** #
+        # def dfs(node):
+        #     visited.add(node)
+
+        #     for i in range(n):
+        #         if isConnected[node][i] and i not in visited:
+        #             dfs(i)
+        
+        # for i in range(n):
+        #     if i not in visited:
+        #         connected_components += 1
+        #         dfs(i)
+
+        # ** BFS Approach ** #
+
+        def bfs(node):
+            queue = deque([node])
             visited.add(node)
 
-            for i in range(n):
-                if isConnected[node][i] and i not in visited:
-                    dfs(i)
+            while queue:
+                node = queue.popleft()
+                for i in range(n):
+                    if isConnected[node][i] and i not in visited:
+                        queue.append(i)
+                        visited.add(i)
         
         for i in range(n):
             if i not in visited:
                 connected_components += 1
-                dfs(i)
-        
+                bfs(i)
+
+                    
         return connected_components
