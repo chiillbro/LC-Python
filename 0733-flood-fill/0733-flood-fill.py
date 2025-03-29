6302846a8f7fb780d905560dcae89d88c4dc7b6f
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-29 10:45:13 +0530

    Time: 0 ms (100%), Space: 18 MB (49.92%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,24 +1,33 @@
 class Solution:
+    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         original = image[sr][sc]
         if color == original:
             return image
 
         m, n = len(image), len(image[0])
-        queue = deque([(sr, sc)])
-        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
-        while queue:
-            for _ in range(len(queue)):
-                r, c = queue.popleft()
-                image[r][c] = color
+        # ** DFS Approach ** #
 
-                for dir_r, dir_c in directions:
-                    row, col = r + dir_r, c + dir_c
-                    if (0 <= row < m) and (0 <= col < n) and image[row][col] == original:
-                        queue.append((row, col))
+        def dfs(row, col):
+            if (0 <= row < m) and (0 <= col < n) and image[row][col] == original:
+                image[row][col] = color
+
+                for dir_r, dir_c in self.directions:
+                    dfs(dir_r + row, dir_c + col) 
+
+        # ** BFS Approach ** #
+        # queue = deque([(sr, sc)])
+
+        # while queue:
+        #     for _ in range(len(queue)):
+        #         r, c = queue.popleft()
+        #         image[r][c] = color
+
+        #         for dir_r, dir_c in self.directions:
+        #             row, col = r + dir_r, c + dir_c
+        #             if (0 <= row < m) and (0 <= col < n) and image[row][col] == original:
+        #                 queue.append((row, col))
         
+        dfs(sr, sc)
         return image
-
-
-        
