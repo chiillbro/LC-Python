84612a17933011da541d91645242890850f64366
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-31 22:55:45 +0530

    Time: 0 ms (100%), Space: 18.8 MB (9.82%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.py
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.py
@@ -10,21 +10,43 @@ class Solution:
         V = len(graph)
         color = [-1] * V
 
+        # for i in range(V):
+        #     if color[i] != -1:
+        #         continue
+        #     queue = deque([i])
+        #     color[i] = 0
+
+        #     while queue:
+        #         cur = queue.popleft()
+        #         for neigh in graph[cur]:
+        #             if color[neigh] == -1:
+        #                 color[neigh] = 1 - color[cur]
+        #                 queue.append(neigh)
+        #             elif color[neigh] == color[cur]:
+        #                 return False
+        
+        # return True
+
+
+        # ** DFS Appraoch
+
+        def dfs(node, col):
+            color[node] = col
+            for neigh in graph[node]:
+                if color[neigh] == -1:
+                    if not dfs(neigh, 1 - col):
+                        return False
+                elif color[neigh] == col:
+                    return False
+            return True
+        
         for i in range(V):
             if color[i] != -1:
                 continue
-            queue = deque([i])
-            color[i] = 0
-
-            while queue:
-                cur = queue.popleft()
-                for neigh in graph[cur]:
-                    if color[neigh] == -1:
-                        color[neigh] = 1 - color[cur]
-                        queue.append(neigh)
-                    elif color[neigh] == color[cur]:
-                        return False
+            if not dfs(i, 0):
+                return False
         
         return True
+
         
         
