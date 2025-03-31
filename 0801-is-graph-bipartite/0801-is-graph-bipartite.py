e390b856b8f494507f9c8a4abddb04ade2a1023a
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-31 22:40:49 +0530

    Time: 3 ms (45.02%), Space: 18.5 MB (53.38%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.py
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.py
@@ -1,5 +1,10 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
+        """
+        Try picking up any two colors and color up all the nodes of the graph with alteranting colors for each edge, by the end if no two adjacent nodes are colored same (i.e., evey edge has perfect alterante colors), then that graph is said to be a Bipartite Graph..
+
+        That's what the following bfs does.
+        """
         V = len(graph)
         color = [-1] * V
 
