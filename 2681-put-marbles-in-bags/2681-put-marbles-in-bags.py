51b7e3b5601d6bbb4c4423371270750e3e3d68ec
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-03-31 07:58:59 +0530

    Time: 131 ms (58.68%), Space: 30.4 MB (29.34%) - LeetHub

---
+++ 2681-put-marbles-in-bags/2681-put-marbles-in-bags.py
@@ -0,0 +1,12 @@
+class Solution:
+    def putMarbles(self, weights: List[int], k: int) -> int:
+        n = len(weights)
+        pair_weights = [weights[i] + weights[i+ 1] for i in range(n-1)]
+        pair_weights.sort()
+
+        ans = 0
+
+        for i in range(k-1):
+            ans += pair_weights[n-2-i] - pair_weights[i]
+        
+        return ans
