dffab91c4da20c08f9ef478ce45004f0c8ae7425
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-04-01 07:53:42 +0530

    Time: 77 ms (74.97%), Space: 60.6 MB (87.06%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
@@ -2,11 +2,27 @@ class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
         n = len(questions)
         # ** Recursive Top-Down Approach ** #
-        @lru_cache(maxsize=None)
-        def dfs(i):
-            if i >= n: return 0
+        # @lru_cache(maxsize=None)
+        # def dfs(i):
+        #     if i >= n: return 0
 
-            pts, skip = questions[i]
-            return max(pts+dfs(i + skip + 1), dfs(i + 1))
+        #     pts, skip = questions[i]
+        #     return max(pts+dfs(i + skip + 1), dfs(i + 1))
         
-        return dfs(0)
+        # return dfs(0)
+
+        # ** Optimized Bottom-Up Approach: DP ** #
+
+        dp = [0] * n
+        dp[n-1] = questions[n-1][0]
+
+        for i in range(n-2, -1, -1):
+            pts, skip = questions[i]
+            dp[i] = pts
+
+            if i + skip < n-1:
+                dp[i] += dp[i+skip+1]
+            
+            dp[i] = max(dp[i], dp[i+1])
+        
+        return dp[0]
