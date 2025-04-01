f48951376f97af7a4220672f5112626e1cf1c081
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-04-01 23:16:31 +0530

    Time: 47 ms (53.44%), Space: 23.6 MB (74.65%) - LeetHub

---
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
@@ -0,0 +1,24 @@
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        V = len(graph)
+        rev_graph = [[] for _ in range(V)]
+
+        in_degree = [0] * V
+        for i in range(V):
+            for neigh in graph[i]:
+                rev_graph[neigh].append(i)
+                in_degree[i] += 1
+        
+        queue = deque([node for node, degree in enumerate(in_degree) if not degree])
+
+        safe_nodes = []
+        while queue:
+            for _ in range(len(queue)):
+                cur = queue.popleft()
+                safe_nodes.append(cur)
+                for neigh in rev_graph[cur]:
+                    in_degree[neigh] -= 1
+                    if not in_degree[neigh]:
+                        queue.append(neigh)
+        
+        return sorted(safe_nodes)
