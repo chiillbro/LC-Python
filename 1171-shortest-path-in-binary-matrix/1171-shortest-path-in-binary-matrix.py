1bbc1442e2908019295dc082c414c3b86f677332
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-04-03 20:50:10 +0530

    Time: 195 ms (62.74%), Space: 18.3 MB (73.9%) - LeetHub

---
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -0,0 +1,29 @@
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        if grid[0][0] or grid[n-1][n-1]:
+            return -1
+        
+        if n == 1 and not grid[0][0]:
+            return 1
+
+        visited = [[False] * n for _ in range(n)]
+        dist = [[float("inf")] * n for _ in range(n)]
+        dist[0][0] == 1
+        visited[0][0] = True
+        heap = [(1, 0, 0)]
+
+        directions = [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)]
+        while heap:
+            distance, cur_row, cur_col = heappop(heap)
+            
+            for dr, dc in directions:
+                new_r, new_c = cur_row + dr, cur_col + dc
+
+                if (0 <= new_r < n) and (0 <= new_c < n) and not visited[new_r][new_c] and not grid[new_r][new_c]:
+                    if distance + 1 < dist[new_r][new_c]:
+                        dist[new_r][new_c] = distance + 1
+                        heappush(heap, (distance + 1, new_r, new_c))
+        
+
+        return dist[n-1][n-1] if dist[n-1][n-1] != float("inf") else -1
