ec3d53b788a3487b8fe03888c32dc7b538373baf
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-04-03 20:51:38 +0530

    Time: 192 ms (63.88%), Space: 18.5 MB (65.1%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.py
@@ -7,10 +7,10 @@ class Solution:
         if n == 1 and not grid[0][0]:
             return 1
 
-        visited = [[False] * n for _ in range(n)]
+        # visited = [[False] * n for _ in range(n)]
         dist = [[float("inf")] * n for _ in range(n)]
         dist[0][0] == 1
-        visited[0][0] = True
+        # visited[0][0] = True
         heap = [(1, 0, 0)]
 
         directions = [(-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)]
@@ -20,7 +20,7 @@ class Solution:
             for dr, dc in directions:
                 new_r, new_c = cur_row + dr, cur_col + dc
 
-                if (0 <= new_r < n) and (0 <= new_c < n) and not visited[new_r][new_c] and not grid[new_r][new_c]:
+                if (0 <= new_r < n) and (0 <= new_c < n) and not grid[new_r][new_c]:
                     if distance + 1 < dist[new_r][new_c]:
                         dist[new_r][new_c] = distance + 1
                         heappush(heap, (distance + 1, new_r, new_c))
