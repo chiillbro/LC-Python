a0ea44a1af4084739d1961f8c6f9d25e202e80a7
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-04-04 08:27:19 +0530

    Time: 4 ms (37.41%), Space: 18.1 MB (71.52%) - LeetHub

--- 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.py
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.py
@@ -9,37 +9,46 @@ class Solution:
         if not root:
             return None
         
-        # postorder = []
-        # stack1, stack2 = [(0, root)], []
-        # depth = 0
-        # lca = root
+        queue = deque([root])
+        parent = {root: None}
 
-        # while stack1:
-        #     depth, current = stack1.pop()
-        #     stack2.append((depth, current))
-        #     if current.left and current.right:
-        #         lca = 
-        #     if current.left:
-        #         stack1.append((depth + 1, current.left))
-        #     if current.right:
-        #         stack1.append((depth + 2, current.right))
+        while queue:
+            level = []
+
+            for _ in range(len(queue)):
+                current = queue.popleft()
+                level.append(current)
+                if current.left:
+                    parent[current.left] = current
+                    queue.append(current.left)
+                
+                if current.right:
+                    parent[current.right] = current
+                    queue.append(current.right)
             
-        # while stack2:
-        #     postorder.appedn(stack2.pop().val)
-
-
-        def dfs(root):
-            if not root:
-                return 0, None
-
-            left, right = dfs(root.left), dfs(root.right)
-
-            if left[0] > right[0]:
-                return left[0] + 1, left[1]
-            elif left[0] < right[0]:
-                return right[0] + 1, right[1]
-            
-            return left[0] + 1, root
+            if not queue:
+                deepest = level
         
-        return dfs(root)[1]
+        if len(deepest) == 1:
+            return deepest[0]
+        
+        while len({node for node in deepest}) > 1:
+            deepest = [parent[node] for node in deepest]
+        
+        return deepest[0]
+
+
+        # def dfs(root):
+        #     if not root:
+        #         return 0, None
+
+        #     left, right = dfs(root.left), dfs(root.right)
+
+        #     if left[0] > right[0]:
+        #         return left[0] + 1, left[1]
+        #     elif left[0] < right[0]:
+        #         return right[0] + 1, right[1]
             
+        #     return left[0] + 1, root
+        
+        # return dfs(root)[1]
