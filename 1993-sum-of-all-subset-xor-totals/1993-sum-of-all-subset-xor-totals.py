5854e7b5742caf53817165d6a4eb795d3e252584
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-04-05 20:01:17 +0530

    Time: 0 ms (100%), Space: 17.7 MB (83.05%) - LeetHub

--- 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.py
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.py
@@ -1,10 +1,14 @@
+from functools import reduce
+import operator
+
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
         n = len(nums)
 
-        ans = 0
-
         # ** Approach one: Using backtracking to generate all subsets and for every subset, calcualte XOR and add it to the answer ** #
+        # ** TC, SC : O(N * 2 ^ N) **
+
+        # ans = 0
         # def backtrack(i: int, path: List[int]) -> None:
         #     nonlocal ans
         #     if i == n:
@@ -24,16 +28,27 @@ class Solution:
         # return ans
 
 
-        # ** Approach 2: Now, we do something like optimized Bactracking by calculating the XOR while generating the subsets and passing current xor for that subset as an parameter **
+        # ** Approach 2: Now, we do something like optimized Bactracking by calculating the XOR while generating the subsets and passing current xor for that subset as an parameter ** #
+        # ** TC : O(2 ^ N), SC: O(N) ** which is acceptable if N is small, as it usually for this problem **
 
-        def optimizedBacktrack(i: int, xor: int) -> int:
-            if i == n:
-                return xor
+        # def optimized_backtrack(i: int, xor: int) -> int:
+        #     if i == n:
+        #         return xor
             
-            without_cur = optimizedBacktrack(i + 1, xor)
+        #     without_cur = optimizedBacktrack(i + 1, xor)
 
-            with_cur = optimizedBacktrack(i + 1, xor ^ nums[i])
+        #     with_cur = optimizedBacktrack(i + 1, xor ^ nums[i])
 
-            return without_cur + with_cur
+        #     return without_cur + with_cur
 
-        return optimizedBacktrack(0, 0)
+        # return optimizedBacktrack(0, 0)
+
+
+        # ** Approach 3: Mathematical - useful for larger n **
+        # TC: O(N), SC: O(1)
+
+        # It can be proven that sum of the XOR of all th subsets is
+
+        # = 2^(n-1) * (bitwise OR of all elements in the nums)
+
+        return (1 << (n - 1)) * reduce(operator.or_, nums, 0)
