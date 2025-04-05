5b2d4d4c28d61957b2193be9598da41e60846256
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-04-05 21:23:05 +0530

    Time: 919 ms (5.01%), Space: 19.8 MB (55.95%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.py
+++ 0744-network-delay-time/0744-network-delay-time.py
@@ -1,22 +1,41 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        adj_list = {i: [] for i in range(1, n+1)}
 
-        for u, v, w in times:
-            adj_list[u].append((v, w))
+        # ** Approach one: using Dijkstra's Algorithm, since weights are positive ** #
+        # ** TC: O(E log V), SC: O()
+
+        # adj_list = {i: [] for i in range(1, n+1)}
+        # for u, v, w in times:
+        #     adj_list[u].append((v, w))
         
-        delays = [float("inf")] * (n+1)
+        # delays = [float("inf")] * (n+1)
+        # delays[k] = 0
+
+        # heap = [(0, k)]
+
+        # while heap:
+        #     cur_delay, cur_node = heappop(heap)
+
+        #     for neigh, time in adj_list[cur_node]:
+        #         if cur_delay + time < delays[neigh]:
+        #             delays[neigh] = cur_delay + time
+        #             heappush(heap, (cur_delay + time, neigh))
+        
+        # ans = max(delays[1:])
+        # return ans if ans != float('inf') else -1
+
+
+        # ** Approach 2: Using Bellman Ford - but unnecessary extra complexity, which runs in O(V * E) **
+
+        delays = [float('inf')] * (n + 1)
         delays[k] = 0
 
-        heap = [(0, k)]
-
-        while heap:
-            cur_delay, cur_node = heappop(heap)
-
-            for neigh, time in adj_list[cur_node]:
-                if cur_delay + time < delays[neigh]:
-                    delays[neigh] = cur_delay + time
-                    heappush(heap, (cur_delay + time, neigh))
+        for _ in range(n):
+            for u, v, time in times:
+                if delays[u] == float("inf"):
+                    continue
+                if delays[u] + time < delays[v]:
+                    delays[v] = delays[u] + time
         
-        ans = max(delays[1:])
-        return ans if ans != float('inf') else -1
+        max_time = max(delays[1:])
+        return -1 if max_time == float('inf') else max_time
