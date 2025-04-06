f87c50a4231b92249f53daa54ba8b54aa1fc4997
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-04-06 13:30:28 +0530

    Time: 182 ms (82.03%), Space: 18 MB (59.42%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.py
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.py
@@ -1,6 +1,32 @@
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
         n = len(nums)
+        # ** Approach One: Brute Force (generating all subsets and performing the constraint check) ** #
+        # TC : O(2^n * n^2), SC: O(n) gives TLE
+
+        # best_subset = []
+        # def isValid(arr):
+        #     for i in range(len(arr)):
+        #         for j in range(i+1, len(arr)):
+        #             if arr[j] % arr[i] !=0 and arr[i] % arr[j] != 0:
+        #                 return False
+            
+        #     return True
+        # for mask in range(1, 1 << n):
+        #     subset = []
+        #     for i in range(n):
+        #         if mask & (1 << i):
+        #             subset.append(nums[i])
+            
+        #     if isValid(subset) and len(subset) > len(best_subset):
+        #         best_subset = subset
+        
+        # return best_subset
+
+
+        # ** Approach Two: DP (based on the observation that sorting the nums array benefits to code up with optimal solution) **
+        # TC: O(n^2), SC: O(n)
+
         nums.sort()
 
         dp = [1] * n # dp[i] represents the largest subset ending at i
