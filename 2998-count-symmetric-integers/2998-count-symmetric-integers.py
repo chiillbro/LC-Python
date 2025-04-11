251bff7d3a64e3e221430ac8298aaf31d403c84f
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-04-11 09:19:47 +0530

    Time: 618 ms (54.17%), Space: 17.8 MB (81.26%) - LeetHub

--- 2998-count-symmetric-integers/2998-count-symmetric-integers.py
+++ 2998-count-symmetric-integers/2998-count-symmetric-integers.py
@@ -3,11 +3,10 @@ class Solution:
         count = 0
 
         for num in range(low, high + 1):
-            num_str = str(num)
-            if len(num_str) & 1:
+            digits = list(map(int, str(num)))
+            if len(digits) & 1:
                 continue
-            
-            digits = list(map(int, num_str))
+                
             half = len(digits) >> 1
             if sum(digits[:half]) == sum(digits[half:]):
                 count += 1
