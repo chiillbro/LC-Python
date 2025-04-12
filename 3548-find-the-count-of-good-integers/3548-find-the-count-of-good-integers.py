f8d502c77c1885424242bc972f0838c9204edf3e
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-04-12 10:49:55 +0530

    Time: 864 ms (95.16%), Space: 18.9 MB (98.39%) - LeetHub

--- 3548-find-the-count-of-good-integers/3548-find-the-count-of-good-integers.py
+++ 3548-find-the-count-of-good-integers/3548-find-the-count-of-good-integers.py
@@ -1,63 +1,35 @@
+from math import factorial
+
 class Solution:
-    def factorial(self, number):
-        chakra = 1
-        for i in range(1, number + 1):
-            chakra *= i
-        return chakra
+    def countGoodIntegers(self, n: int, k: int) -> int:
+        unique = set()
 
-    def generatePalindromes(self, cloneID, index, validPalindromes, k):
-        if index >= (len(cloneID) + 1) // 2:
-            if int(cloneID) % k == 0:
-                validPalindromes.append(cloneID)
-            return
+        base = 10 ** ((n-1) >> 1)
+        skip = n & 1
 
-        if index != 0:
-            temp = cloneID
-            temp = temp[:index] + '0' + temp[index+1:]
-            temp = temp[:len(temp) - index - 1] + '0' + temp[len(temp) - index:]
-            self.generatePalindromes(temp, index + 1, validPalindromes, k)
+        for i in range(base, base * 10):
+            s = str(i)
+            s += s[::-1][skip:]
 
-        for digit in range(1, 10):
-            temp = cloneID
-            temp = temp[:index] + str(digit) + temp[index+1:]
-            temp = temp[:len(temp) - index - 1] + str(digit) + temp[len(temp) - index:]
-            self.generatePalindromes(temp, index + 1, validPalindromes, k)
+            palidromicInt = int(s)
 
-    def countGoodIntegers(self, n, k):
-        validPalindromes = []
-        baseForm = "0" * n
-        self.generatePalindromes(baseForm, 0, validPalindromes, k)
+            if not (palidromicInt % k):
+                sorted_s = "".join(sorted(s))
+                unique.add(sorted_s)
         
-        chakraPatterns = set()
 
-        for shadowClone in validPalindromes:
-            frequency = ['0'] * 10
-            for chakra in shadowClone:
-                idx = int(chakra)
-                if frequency[idx] == '9':
-                    frequency[idx] = 'A'  # beyond 9 digits (special case)
-                else:
-                    frequency[idx] = str(int(frequency[idx]) + 1)
-            chakraPatterns.add(''.join(frequency))
+        fac = [factorial(i) for i in range(n + 1)]
+        ans = 0
 
-        basePermutations = self.factorial(n)
-        totalCount = 0
-
-        for pattern in chakraPatterns:
-            permutation = basePermutations
-            for freq in pattern:
-                divisor = 10 if freq == 'A' else int(freq)
-                permutation //= self.factorial(divisor)
-
-            if pattern[0] != '0':
-                zeroCount = int(pattern[0]) - 1
-                zeroRestrictedPerm = self.factorial(n - 1)
-                for freq in pattern[1:]:
-                    divisor = 10 if freq == 'A' else int(freq)
-                    zeroRestrictedPerm //= self.factorial(divisor)
-                zeroRestrictedPerm //= self.factorial(zeroCount)
-                permutation -= zeroRestrictedPerm
-
-            totalCount += permutation
-
-        return int(totalCount)
+        for s in unique:
+            cnt = [0] * 10
+            for c in s:
+                cnt[int(c)] += 1
+            
+            tot = (n - cnt[0]) * fac[n-1]
+            for x in cnt:
+                tot //= fac[x]
+            
+            ans += tot
+        
+        return ans
