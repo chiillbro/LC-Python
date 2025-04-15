2ee25e10023b18116794dcb84b591c51113a0d11
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-04-15 22:11:52 +0530

    Time: 633 ms (96.04%), Space: 35.4 MB (77%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
@@ -1,48 +1,27 @@
+
+class FenwickTree:
+    def __init__(self, n):
+        self.n = n
+        self.tree = [0] * (n + 1)
+    
+    def update(self, index):
+        while index <= self.n:
+            self.tree[index] += 1
+            index += index & -index
+    
+    def query(self, index):
+        curr = 0
+        while index > 0:
+            curr += self.tree[index]
+            index -= index & -index
+        return curr
+
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        # def sort(indexed_nums):
-        #     half = len(indexed_nums) // 2
-        #     if half:
-        #         left, right = sort(indexed_nums[:half]), sort(indexed_nums[half:])
-        #         for i in range(len(enum))[::-1]:
-        #             if not right or (left and left[-1][1] > right[-1][1]):
-        #                 counts[left[-1][0]] += len(right)
-        #                 indexed_nums[i] = left.pop()
-        #             else:
-        #                 indexed_nums[i] = right.pop()
-        #     return indexed_nums
-
-        def merge_sort(arr):
-            if len(arr) <= 1: return arr
-
-            mid = len(arr) // 2
-            left = merge_sort(arr[:mid])
-            right = merge_sort(arr[mid:])
-            
-            merged = []
-            i = j = 0
-
-            while i < len(left) and j < len(right):
-                if left[i][1] <= right[j][1]:
-                    counts[left[i][0]] += j
-                    merged.append(left[i])
-                    i += 1
-                else:
-                    merged.append(right[j])
-                    j += 1
-            
-            while i < len(left):
-                counts[left[i][0]] += j
-                merged.append(left[i])
-                i += 1
-            
-            merged.extend(right[j:])
-
-            return merged
-
-
-        counts = [0] * len(nums)
-        indexed_nums = list(enumerate(nums))
-
-        merge_sort(indexed_nums)
-        return counts
+        
+        ans = [0] * len(nums)
+        tree = FenwickTree(20000)
+        for i in range(len(nums) - 1, -1, -1):
+            ans[i] = tree.query(nums[i] + 10001 - 1)
+            tree.update(nums[i] + 10001)
+        return ans 
