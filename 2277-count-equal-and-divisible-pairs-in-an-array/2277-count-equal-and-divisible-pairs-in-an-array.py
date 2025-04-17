6ac7df8d6f6dc428e9928c9136f0cd09160aae1b
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-04-17 23:09:20 +0530

    Time: 3 ms (99.33%), Space: 17.8 MB (48.17%) - LeetHub

--- 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.py
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.py
@@ -2,12 +2,24 @@ class Solution:
     def countPairs(self, nums: List[int], k: int) -> int:
         res = 0
 
-        for i in range(len(nums) - 1):
-            for j in range(i + 1, len(nums)):
-                if nums[i] == nums[j] and not ((i * j) % k):
-                    res += 1
-        
+        # for i in range(len(nums) - 1):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] == nums[j] and not ((i * j) % k):
+        #             res += 1
+
+
+        # ** Optimal Approach: Using GCD O(n * d(g)) ** #
+
+        freq = defaultdict(lambda: defaultdict(int))
+
+        for j, v in enumerate(nums):
+            g_j = math.gcd(j, k)
+
+            for g_i, count_i in freq[v].items():
+                if (g_i * g_j) % k == 0:
+                    res += count_i
+            
+            freq[v][g_j] += 1
+
         return res
 
-        
-            
