6629d59f6d879bef1e845a673bdead992eaf0b8b
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-04-22 22:31:35 +0530

    Time: 19 ms (96.83%), Space: 25.4 MB (39.68%) - LeetHub

--- 2415-count-the-number-of-ideal-arrays/2415-count-the-number-of-ideal-arrays.py
+++ 2415-count-the-number-of-ideal-arrays/2415-count-the-number-of-ideal-arrays.py
@@ -1,7 +1,17 @@
+# ** prime‐factor + stars‑and‑bars solution ** #
+
 MOD = 10 ** 9 + 7
 MAX_N = 10 ** 4 + 10
+
+# MAX_P is chosen to exceed the maximum possible exponent E of any prime in any x≤10^4
+ 
+# The worst case is 2^13 = 8192 (exponent 13), and 2^14 = 16384 exceeds 10000, so no exponent >13 occurs.
+# We round up to 15 for safety.
+# This bounds how large each Ei can be, so we know how big our combination table needs to be in the second dimension
+
 MAX_P = 15
 
+# Precomputing Prime‐Factor Exponents (ps[x]) via a Sieve
 sieve = [0] * MAX_N
 
 for i in range(2, MAX_N):
@@ -9,6 +19,8 @@ for i in range(2, MAX_N):
         for j in range(i, MAX_N, i):
             sieve[j] = i
 
+
+# Factor out each x into exponents
 ps = [[] for _ in range(MAX_N)]
 
 for i in range(2, MAX_N):
@@ -21,13 +33,21 @@ for i in range(2, MAX_N):
             cnt += 1
         ps[i].append(cnt)
 
-c = [[0] * (MAX_P + 1) for _ in range(MAX_N + MAX_P)]
-c[0][0] = 1
+# After this, ps[x] is the list [E1, E2, ...]
+
+
+# Precomputing Combinations (i / j)
+
+# We need to compute 
+# (n + E - 1) / E for 0 ≤ E ≤15 and 0≤\U0001d45b+\U0001d438−1≤10^4+15 and 0 ≤ n+E−1 ≤ 10^4 + 15. We build Pascal’s triangle up to those limits
+
+C = [[0] * (MAX_P + 1) for _ in range(MAX_N + MAX_P)]
+C[0][0] = 1
 
 for i in range(1, MAX_N + MAX_P):
-    c[i][0] = 1
+    C[i][0] = 1
     for j in range(1, min(i, MAX_P) + 1):
-        c[i][j] = (c[i - 1][j] + c[i-1][j-1]) % MOD
+        C[i][j] = (C[i - 1][j] + C[i-1][j-1]) % MOD
 
 
 class Solution:
@@ -35,9 +55,9 @@ class Solution:
         ans = 0
 
         for x in range(1, maxValue + 1):
-            mul = 1
-            for p in ps[x]:
-                mul = mul * c[n + p - 1][p] % MOD
-            ans = (ans + mul) % MOD
-        
+            ways = 1
+            # for each prime‐exponent E in x:
+            for E in ps[x]:  
+                ways = ways * C[n + E - 1][E] % MOD
+            ans = (ans + ways) % MOD
         return ans
