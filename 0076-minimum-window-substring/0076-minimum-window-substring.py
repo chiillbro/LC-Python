9e7a0a1172c116fe7f06747932062936d112b647
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-04-30 00:39:46 +0530

    Time: 71 ms (68.45%), Space: 18.4 MB (16.92%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,51 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+
+        # Approach 1 using two for loops, O(N ^ 2) results in TLE
+        n, m = len(s), len(t)
+
+        if n < m:
+            return ""
+        
+        start, max_len = -1, float('inf')
+
+        # for i in range(n):
+        #     freq_map, count = Counter(t), 0
+        #     for j in range(i, n):
+        #         if freq_map[s[j]] > 0: count += 1
+
+        #         freq_map[s[j]] -= 1
+
+        #         if count == m and j - i + 1 < max_len:
+        #             max_len = j - i + 1
+        #             start = i
+        #             break
+        
+
+        # return s[start:start + max_len] if max_len != float("inf") else ""
+
+
+        # Optimal Solution using Sliding Window
+        
+        count = 0
+        freq_map = Counter(t)
+        
+        left = 0
+
+        for right in range(n):
+            if freq_map[s[right]] > 0: count += 1
+
+            freq_map[s[right]] -= 1
+
+            while count == m:
+                if right - left + 1 < max_len:
+                    max_len = right - left + 1
+                    start = left
+                
+                freq_map[s[left]] += 1
+                if freq_map[s[left]] > 0: count -= 1
+                left += 1
+        
+        return "" if start == -1 else s[start:start + max_len]
+
+        
