0087a28876c9a266521b0171aeeea450b33cd2a6
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-04-29 18:21:27 +0530

    Time: 104 ms (63.98%), Space: 18.1 MB (21.77%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
@@ -1,17 +1,21 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        N = len(s)
-        res = l = 0
-        count_map = defaultdict(int)
+        # using Sliding Window Approach
 
-        for r in range(N):
-            count_map[s[r]] += 1
+        n = len(s)
 
-            while len(count_map) == 3:
-                res += N - r
-                count_map[s[l]] -= 1
-                if count_map[s[l]] == 0:
-                    count_map.pop(s[l])
-                l += 1
-        
-        return res        
+        freq_map = defaultdict(int)
+        left = res = 0
+
+        for right in range(n):
+            freq_map[s[right]] += 1
+
+            while len(freq_map) == 3:
+                res += n - right
+                freq_map[s[left]] -= 1
+                if not freq_map[s[left]]:
+                    freq_map.pop(s[left])
+                
+                left += 1
+            
+        return res
