f1156a2fa4b97f195101dc1c21324d7df99f21b9
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-04-29 17:21:31 +0530

    Time: 91 ms (69.49%), Space: 24.6 MB (30.04%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
@@ -19,4 +19,24 @@ class Solution:
         return res
         
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        return self.sliding_window_atmost(nums, k) - self.sliding_window_atmost(nums, k - 1)
+        # Sliding Window Approach using "At Most" Trick
+        # return self.sliding_window_atmost(nums, k) - self.sliding_window_atmost(nums, k - 1)
+
+        # Standard and fundamental prefix sum using hashing approach
+        n = len(nums)
+
+        res = current_sum = 0
+
+        prefix_map = defaultdict(int)
+        prefix_map[0] = 1
+
+        for num in nums:
+            current_sum += num & 1
+
+            prefix_sum_needed = current_sum - k
+            if prefix_sum_needed in prefix_map:
+                res += prefix_map[prefix_sum_needed]
+            
+            prefix_map[current_sum] += 1
+        
+        return res
