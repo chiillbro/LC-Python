0d9e6ddf3bfdf887847f24786b4430a8fd2765b8
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-04-29 18:24:20 +0530

    Time: 67 ms (92.13%), Space: 18 MB (21.77%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
@@ -4,18 +4,29 @@ class Solution:
 
         n = len(s)
 
-        freq_map = defaultdict(int)
-        left = res = 0
+        # freq_map = defaultdict(int)
+        # left = res = 0
 
-        for right in range(n):
-            freq_map[s[right]] += 1
+        # for right in range(n):
+        #     freq_map[s[right]] += 1
 
-            while len(freq_map) == 3:
-                res += n - right
-                freq_map[s[left]] -= 1
-                if not freq_map[s[left]]:
-                    freq_map.pop(s[left])
+        #     while len(freq_map) == 3:
+        #         res += n - right
+        #         freq_map[s[left]] -= 1
+        #         if not freq_map[s[left]]:
+        #             freq_map.pop(s[left])
                 
-                left += 1
+        #         left += 1
+
+        # Last Position Tracking Approach
+
+        last = [-1] * 3
+
+        res = 0
+        
+        for i, char in enumerate(s):
+            last[ord(char) - ord('a')] = i
+
+            res += 1 + min(last)
             
         return res
