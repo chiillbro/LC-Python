2d5f176c7ad4d07db943b480a99634b337839e19
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-04-30 11:38:55 +0530

    Time: 6 ms (95.81%), Space: 17.9 MB (94.83%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.py
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.py
@@ -1,42 +1,22 @@
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
+        # using Sliding Window Approach
+
         n = len(nums)
+        distinct = len(set(nums))
 
-        # counter = Counter(nums)
-        # unique_len = len(counter.keys())
+        freq_map = defaultdict(int)
+        left = res = 0
 
-        # # for i, num in enumerate(nums):
-        # i = 0
-        # while len(counter.keys()) == unique_len and i < n:
-        #     num = nums[i]
-        #     counter[num] -= 1
-        #     if counter[num] == 0:
-        #         counter.pop(num)
+        for right in range(n):
+            freq_map[nums[right]] += 1
 
-        unique = len(set(nums))
-        counter = defaultdict(int)
-        res = right = 0
-
-        for left in range(n):
-            if left > 0:
-                remove = nums[left-1]
-                counter[remove] -= 1
-                if not counter[remove]:
-                    counter.pop(remove)
-            
-            while right < n and len(counter) < unique:
-                add = nums[right]
-                counter[add] += 1
-                right += 1
-            
-            if len(counter) == unique:
-                res += n - right + 1
-            
-        return res
-
-
-
+            while len(freq_map) == distinct:
+                res += n - right
+                freq_map[nums[left]] -= 1
+                if not freq_map[nums[left]]:
+                    freq_map.pop(nums[left])
+                
+                left += 1
         
-
-
-
+        return res
