69fe659f68530e2ee13106f7b61d25846a890bac
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-05-02 18:00:09 +0530

    Time: 264 ms (34.54%), Space: 23.3 MB (38.49%) - LeetHub

--- 0868-push-dominoes/0868-push-dominoes.py
+++ 0868-push-dominoes/0868-push-dominoes.py
@@ -30,21 +30,51 @@ class Solution:
         # return ''.join(stack)
 
 
-        symbols = [(i, c) for i, c in enumerate(dominoes) if c != '.']
+    #     symbols = [(i, c) for i, c in enumerate(dominoes) if c != '.']
 
-        symbols = [(-1, 'L')] + symbols + [(len(dominoes), 'R')]
+    #     symbols = [(-1, 'L')] + symbols + [(len(dominoes), 'R')]
 
-        ans = list(dominoes)
+    #     ans = list(dominoes)
 
-        for (i, x), (j, y) in zip(symbols, symbols[1:]):
-            if x == y:
-                for k in range(i+1, j):
-                    ans[k] = x
-            elif x > y:
-                for k in range(i+1, j):
-                    ans[k] = '.LR'[self.cmp(k-i, j-k)]
+    #     for (i, x), (j, y) in zip(symbols, symbols[1:]):
+    #         if x == y:
+    #             for k in range(i+1, j):
+    #                 ans[k] = x
+    #         elif x > y:
+    #             for k in range(i+1, j):
+    #                 ans[k] = '.LR'[self.cmp(k-i, j-k)]
         
-        return ''.join(ans)
+    #     return ''.join(ans)
+
+
+        N = len(dominoes)
+
+        force = [0] * N
+
+        f = 0
+
+        for i, c in enumerate(dominoes):
+            if c == 'R':
+                f = N
+            elif c == 'L':
+                f = 0
+            else: f = max(f-1, 0)
+
+            force[i] += f
+
+        f = 0
+        for i in range(N-1, -1, -1):
+            c = dominoes[i]
+            if c == 'R':
+                f = 0
+            elif c == 'L':
+                f = N
+            else: f = max(f-1, 0)
+
+            force[i] -= f
+        
+        return ''.join('.' if f == 0 else 'R' if f > 0 else 'L' for f in force)
+
     
 
     def cmp(self, a, b):
