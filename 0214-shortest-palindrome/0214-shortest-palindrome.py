20775cb6243a8aff1f9462d3278967d3f8af8d10
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-05-04 19:24:39 +0530

    Time: 38 ms (92.5%), Space: 21.9 MB (16.12%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -1,7 +1,48 @@
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
-        count = self._kmp_search(s[::-1], s)
-        return s[count:][::-1] + s
+        # count = self._kmp_search(s[::-1], s)
+        # return s[count:][::-1] + s
+
+        return self.shortestPalindrome_KMP(s)
+    
+    def shortestPalindrome_KMP(self, s: str) -> str:
+        if not s:
+            return s
+        
+        s_rev = s[::-1]
+
+        temp = s + "#" + s_rev
+
+        lps = self._compute_lps(temp)
+
+        k = lps[-1]
+
+        suffix_to_reverse = s[k:]
+        prefix_to_add = suffix_to_reverse[::-1]
+
+        return prefix_to_add + s
+    
+    def _compute_lps(self, pattern: str) -> List[int]:
+        m = len(pattern)
+
+        lps = [0] * m
+        length = 0
+        i = 1
+
+        while i < m:
+            if pattern[i] == pattern[length]:
+                length += 1
+                lps[i] = length
+                i += 1
+            else:
+                if length:
+                    length = lps[length - 1]
+                else:
+                    lps[i] = 0
+                    i += 1
+        
+        return lps
+
 
     def _kmp_search(self, text: str, pattern: str) -> int:
         final_str = pattern + "#" + text
