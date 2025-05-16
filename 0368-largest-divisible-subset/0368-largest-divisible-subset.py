ad0dd323bf31fa64adb1a3adba863703991c8885
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-05-17 00:23:08 +0530

    Time: 179 ms (82.96%), Space: 17.7 MB (97.83%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.py
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.py
@@ -10,7 +10,7 @@ class Solution:
 
         for i in range(n):
             for j in range(i):
-                if (nums[i] % nums[j] == 0) and dp[j] + 1 > dp[i]:
+                if nums[i] % nums[j] == 0 and dp[j] + 1 >= dp[i]:
                     dp[i] = dp[j] + 1
                     prev[i] = j
             
