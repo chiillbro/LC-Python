292e3a5c62eb6f06c2711fddfd3153a0ee763bb9
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-05-17 01:06:24 +0530

    Time: 171 ms (40.2%), Space: 52.7 MB (90.97%) - LeetHub

---
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+        n = len(envelopes)
+        # def sort_func(a, b):
+        #     if a[0] > b[0]:
+        #         return -1
+        #     if a[0] < b[0]:
+        #         return 1
+        #     else:
+        #         if b[0] > b[1]:
+        #             return 1
+        #         else:
+        #             return -1
+
+        envelopes.sort(key=lambda x:(x[0], -x[1]))
+
+        LIS = []
+        size = 0
+
+        for w, h in envelopes:
+            if not LIS or h > LIS[-1]:
+                LIS.append(h); size += 1
+            else:
+                l, r = 0, size
+                while l <= r:
+                    m = (l + r) >> 1
+                    if LIS[m] < h:
+                        l = m + 1
+                    else:
+                        r = m - 1
+                LIS[l] = h
+        return size
