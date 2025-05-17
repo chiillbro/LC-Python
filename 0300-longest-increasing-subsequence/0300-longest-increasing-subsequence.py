9bea444ef567690746298ecdb196aee2a06bf6ce
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-05-17 09:12:32 +0530

    Time: 3 ms (95.57%), Space: 18.1 MB (54.36%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,12 +1,22 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        # DP Approach
-        n = len(nums)
-        dp = [1] * n # as every element is itself a subsequence with length 1
+        # Plain DP Approach TC: O(N^2)
+        # n = len(nums)
+        # dp = [1] * n # as every element is itself a subsequence with length 1
 
-        for i in range(n):
-            for j in range(i):
-                if nums[i] > nums[j]:
-                    dp[i] = max(dp[i], dp[j] + 1)
+        # for i in range(n):
+        #     for j in range(i):
+        #         if nums[i] > nums[j]:
+        #             dp[i] = max(dp[i], dp[j] + 1)
         
-        return max(dp)
+        # return max(dp)
+
+        LIS = []
+
+        for num in nums:
+            insert_idx = bisect_left(LIS, num)
+            if insert_idx == len(LIS):
+                LIS.append(num)
+            else:
+                LIS[insert_idx] = num
+        return len(LIS)
