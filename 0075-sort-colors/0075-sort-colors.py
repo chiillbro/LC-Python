977c944eea2256212326330f5ac976d7eac24d7e
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-05-17 20:45:40 +0530

    Time: 0 ms (100%), Space: 17.9 MB (11.82%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,28 +4,44 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
-        zeroes = ones = twos = 0
+        # zeroes = ones = twos = 0
 
-        for num in nums:
-            if not num:
-                zeroes += 1
-            elif num == 1:
-                ones += 1
-            else:
-                twos += 1
+        # for num in nums:
+        #     if not num:
+        #         zeroes += 1
+        #     elif num == 1:
+        #         ones += 1
+        #     else:
+        #         twos += 1
         
+        # i = 0
+        # while zeroes:
+        #     nums[i] = 0
+        #     i += 1
+        #     zeroes -= 1
+        
+        # while ones:
+        #     nums[i] = 1
+        #     i += 1
+        #     ones -= 1
+        # while twos:
+        #     nums[i] = 2
+        #     i += 1
+        #     twos -= 1
+
+
+
+        # Approach 2 - Two-pointers
+        l, h = 0, n-1
         i = 0
-        while zeroes:
-            nums[i] = 0
-            i += 1
-            zeroes -= 1
-        
-        while ones:
-            nums[i] = 1
-            i += 1
-            ones -= 1
-        while twos:
-            nums[i] = 2
-            i += 1
-            twos -= 1
+
+        while i <= h:
+            if not nums[i]:
+                nums[i], nums[l] = nums[l], nums[i]
+                l += 1; i += 1
+            elif nums[i] == 2:
+                nums[i], nums[h] = nums[h], nums[i]
+                h -= 1
+            else:
+                i += 1
     
