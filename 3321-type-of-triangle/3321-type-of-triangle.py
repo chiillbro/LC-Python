e088c030757f37d77ca2166dba609f313c0fcf4c
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-05-19 08:30:50 +0530

    Time: 0 ms (100%), Space: 17.9 MB (32.78%) - LeetHub

---
+++ 3321-type-of-triangle/3321-type-of-triangle.py
@@ -0,0 +1,20 @@
+class Solution:
+    def triangleType(self, nums: List[int]) -> str:
+        # counter = Counter(nums)
+        _sum = sum(nums)
+
+        if nums.count(side := _sum // 3) == 3: return "equilateral"
+
+        for num in nums:
+            if not (_sum - num > num):
+                return "none"
+        if nums.count(nums[0]) == 2 or nums.count(nums[1]) == 2:
+            return "isosceles"
+        
+        return "scalene"
+
+        # if len(counter) == 2: return "isosceles"
+
+        # if len(counter) == 3: return "scalene"
+
+        # return "none"
