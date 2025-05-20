ba3c419d5b66a3cee4be75211e73a72356003726
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-05-20 22:46:11 +0530

    Time: 7 ms (80.15%), Space: 19.5 MB (75.17%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -10,13 +10,13 @@ class Solution:
         skip = take = 0
 
         for num in uniques:
-            max_points_so_far = max(skip, take)
-            if prev == num - 1:
-                take = num * counts[num] + skip
-                skip = max_points_so_far
+            max_points_up_to_prev = max(skip, take) # The best we could have done up to the prev block was max(skip, take)
+            if prev == num - 1: # This means, num is an immediate neighbor of prev, if we take num, we could not have taken prev
+                take = num * counts[num] + skip # new take would be the current block points num * counts[num] + the points we got by skipping prev which would be skip
+                skip = max_points_up_to_prev # now new skip would be what if I have skipped this particular block, then I would have gotten the max_points before this block that is max_points_up_to_prev
             else:
-                take = num * counts[num] + max_points_so_far
-                skip = max_points_so_far
+                take = num * counts[num] + max_points_up_to_prev # skip the 
+                skip = max_points_up_to_prev # If I skip the current block, my score is just whatever best score I had before the block which is max_points_up_to_prev
             
             prev = num
         
