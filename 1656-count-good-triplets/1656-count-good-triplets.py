9e36ec2c7f2dd7ec96007e84887ab40b1de99842
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-05-21 09:53:09 +0530

    Time: 169 ms (88.85%), Space: 17.9 MB (43.57%) - LeetHub

--- 1656-count-good-triplets/1656-count-good-triplets.py
+++ 1656-count-good-triplets/1656-count-good-triplets.py
@@ -1,29 +1,39 @@
 class Solution:
     def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        n = len(arr)
         res = 0
 
-        # Approach 1: Using enumeration (TC: O(N^3))
-        # for i in range(len(arr) - 2):
-        #     for j in range(i + 1, len(arr) - 1):
-        #         for k in range(j + 1, len(arr)):
-        #             x, y, z = abs(arr[i] - arr[j]), abs(arr[j] - arr[k]), abs(arr[i] - arr[k])
-        #             if x <= a and y <= b and z <= c: res += 1
-        
-        # Approach 2: Using optimized enumeration (TC: O(N^2 + N*S)) where S is the upper limit - in this problem - it's 1000
-        total = [0] * 1001
+        # for i in range(n):
+        #     for j in range(i+1, n):
+        #         ij = abs(arr[i] - arr[j])
+        #         if ij > a:
+        #             continue
+                
+        #         for k in range(j+1, n):
+        #             jk = abs(arr[j] - arr[k])
+        #             ik = abs(arr[i] - arr[k])
 
-        for j in range(len(arr) - 1):
-            for k in range(j + 1, len(arr)):
+        #             if jk <= b and ik <= c:
+        #                 res += 1
+
+        # Somewhat Efficient - Cleverly using the constraints
+        totals = [0] * 1001
+
+        for j in range(n):
+            for k in range(j+1, n):
                 if abs(arr[j] - arr[k]) <= b:
                     lj, rj = arr[j] - a, arr[j] + a
                     lk, rk = arr[k] - c, arr[k] + c
-                    l = max(0, lj, lk)
-                    r = min(1000, rj, rk)
+
+                    l, r = max(0, lj, lk), min(1000, rj, rk)
 
                     if l <= r:
-                        res += total[r] if not l else total[r] - total[l - 1]
-            
-            for k in range(arr[j], 1001):
-                total[k] += 1
+                        res += totals[r] if not l else totals[r] - totals[l-1]
+
+            for i in range(arr[j], 1001):
+                totals[i] += 1
+
+        return res 
         
         return res
+                
