799d3ef04fe5608c208bd4a92b1d8d3be2af045e
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-05-21 10:35:10 +0530

    Time: 0 ms (100%), Space: 17.8 MB (70.17%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.py
+++ 2050-count-good-numbers/2050-count-good-numbers.py
@@ -1,17 +1,11 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
-        MOD = pow(10, 9) + 7
+        MOD = 10**9 + 7
 
-        # for even indices, there are 5 choices (0, 2, 4, 6, 8)
-        # for odd indices, there are 4 choices (2, 3, 5, ,7)
-
-        # 2. total good numbers -> 
-        # if n is even, the total no.of good numbers is 5^(n/2) * 4 ^(n/2) = 20 ^ (n/2)
-        # if n is odd, the total no.of good numbers is 5 * 20^((n-1)/2)
-
-        def expo(x: int, n : int) -> int:
+        # Efficient exponentiation - Exponentiation by Squaring or Binary Exponentiation
+        def expo(x, n):
             """
-            returns the x ** n value
+            Calculates x**n in an efficient way
             """
             if not n:
                 return 1
@@ -19,9 +13,21 @@ class Solution:
             if n & 1:
                 return x * expo(x, n - 1) % MOD
             
-            return expo(x * x % MOD, n >> 1) 
-        
-        return 5 ** (n%2) * expo(20, n // 2) %MOD
+            return expo((x*x) % MOD, n >> 1)
 
 
-        
+        # if n is even: there are 5 choices - (0. 2, 4, 6, 8)
+        # if n is odd: there are 4 choices - (2, 3, 5, 7)
+
+        # so, if n == 1: which is odd, good numbers = 5^1 * 4^0 = 5
+        # if n == 2: which is even, good numbers = 5^1 * 4^1 = 20
+        # if n == 3: which is odd, good numbers = 5^2 * 4^1 = 100
+        # if n == 4: which is even, good numbers = 5^2 & 4^2 = 400
+
+        # Number of even spots = (n + 1) // 2 (integer division, rounds down for n/2 if n is even, and is correct for odd n) or more simply, ceil(n/2)
+        # Number of odd spots = n // 2 (integer division)
+
+
+        # so (even, odd) pairs are n // 2 it can be simplified to (5 * 4) ^ (n//2)
+        # and the remaining 1 spot if n is odd else 0 -> n%2, these spot has 5 choices to be filled with even numbers - 5^(n%2)
+        return 5 ** (n%2) * expo(20, n >> 1) % MOD
