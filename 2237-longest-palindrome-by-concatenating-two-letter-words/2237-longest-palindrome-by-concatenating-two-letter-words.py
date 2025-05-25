7ccc5e1e4128141f2907db96a1175eba524a67c7
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-05-25 20:40:18 +0530

    Time: 154 ms (29.78%), Space: 35.6 MB (40.25%) - LeetHub

--- 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/2237-longest-palindrome-by-concatenating-two-letter-words.py
@@ -5,7 +5,7 @@ class Solution:
         freq = defaultdict(int)
 
         res = 0
-        same_set = set()
+        same_word_set = set()
 
         for word in words:
             rev = word[::-1]
@@ -14,12 +14,12 @@ class Solution:
                 freq[rev] -= 1
                 if not freq[rev]:
                     freq.pop(rev)
-            elif word == rev and word not in same_set:
-                same_set.add(word)
-            elif word == rev and word in same_set:
+            elif word == rev and word not in same_word_set:
+                same_word_set.add(word)
+            elif word == rev and word in same_word_set:
                 res += 4
-                same_set.remove(word)
+                same_word_set.remove(word)
             else:
                 freq[word] += 1
         
-        return res if not same_set else res + 2
+        return res if not same_word_set else res + 2
