2dfd27b40b42e14571c4f1f9342ca4073123a36f
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-05-27 15:06:46 +0530

    Time: 0 ms (100%), Space: 17.7 MB (90.64%) - LeetHub

--- 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.py
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.py
@@ -4,6 +4,13 @@ class Solution:
 
         # Approach two: Math
 
+        # sum of numbers that are divisible by m, num2 = 1m + 2m + 3m + 4m + .... + km
+        # where k = n//m, the maximum number of times within the range n
+        # simplified = (1 + 2 + 3 + 4 + ... + k)m
+        # (k(k+1) // 2)m
+        # num1 = total_sum - num2 (divisible nums sum)
+        # num1 - num2 = (n(n+1) // 2) - (k * (k+1) * m)
+
         k = n // m
 
         return (n * (n + 1) >> 1) - k * (k + 1) * m
