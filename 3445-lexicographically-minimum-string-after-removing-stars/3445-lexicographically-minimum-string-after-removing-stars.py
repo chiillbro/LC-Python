d98c866d2f401dd5eecd588fe67028bfe878bd56
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-06-07 22:03:41 +0530

    Time: 591 ms (57.56%), Space: 29.1 MB (33.09%) - LeetHub

--- 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.py
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.py
@@ -1,20 +1,39 @@
 class Solution:
     def clearStars(self, s: str) -> str:
+        
+        # Approach 1- Greedy: TC: O(N * 26), SC: O(N + 26)
+        # lu = defaultdict(list)
+        # # lu = [[] for _ in range(26)]
+        # arr = list(s)
 
-        lu = defaultdict(list)
-        # lu = [[] for _ in range(26)]
-        arr = list(s)
+        # # has_star = 0
 
-        # has_star = 0
+        # for i, char in enumerate(s):
+        #     if char == "*":
+        #         for j in range(26):
+        #             if lu[j]:
+        #                 arr[lu[j].pop()] = "*"
+        #                 break
+        #     else:
+        #         lu[ord(char) - 97].append(i)
+        
+        
+        # return "".join(char for char in arr if char != "*")
+
+        # Approach 2 - Using Priority Queue, TC: O(N * log N), SC: O(N)
+
+        heap = []
+
+        s_arr = list(s)
 
         for i, char in enumerate(s):
-            if char == "*":
-                for j in range(26):
-                    if lu[j]:
-                        arr[lu[j].pop()] = "*"
-                        break
+            if char != "*":
+                heappush(heap, (char, -i))
+            
             else:
-                lu[ord(char) - 97].append(i)
+                _, j = heappop(heap)
+                s_arr[-j] = ''
+                s_arr[i] = ''
         
-        
-        return "".join(char for char in arr if char != "*")
+
+        return ''.join(s_arr)
