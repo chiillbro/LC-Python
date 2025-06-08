5e0880070e9c7e571cb9542adcc484c5306b6eae
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-06-08 19:37:17 +0530

    Time: 4 ms (68.91%), Space: 18.1 MB (21.39%) - LeetHub

--- 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.py
+++ 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.py
@@ -1,28 +1,44 @@
 class UnionFind:
-    def __init__(self, N):
-        self.root = list(range(N))
+    def __init__(self, n):
+        self.parent = list(range(n))
+    
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
 
-    def Find(self, x):
-        if self.root[x] != x:
-            self.root[x] = self.Find(self.root[x])  # path compression
-        return self.root[x]
+        return self.parent[x]
 
-    def Union(self, x, y):
-        x = self.Find(x)
-        y = self.Find(y)
-        if x==y: return 
-        if y > x:
-            self.root[y]=x
+    def union(self, x, y):
+        x_root = self.find(x)
+        y_root = self.find(y)
+
+        if x_root == y_root:
+            return
+        
+        if x_root > y_root:
+            self.parent[x_root] = y_root
         else:
-            self.root[x]=y
+            self.parent[y_root] = x_root
+
 class Solution:
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
-        G=UnionFind(26)
-        n, m=len(s1), len(baseStr)
-        for i in range(n):
-            G.Union(ord(s1[i])-97, ord(s2[i])-97)
-        ans=""
-        for x in baseStr:
-            ans+=(chr(G.Find(ord(x)-97)+97))
-        return ans
         
+        n = len(s1)
+        uf = UnionFind(26)
+
+        for i in range(n):
+            s1_char_val = ord(s1[i]) - 97
+            s2_char_val = ord(s2[i]) - 97
+
+            uf.union(s1_char_val, s2_char_val)
+        
+
+        res = []
+
+        for char in baseStr:
+            char_val = ord(char) - 97
+            parent_char_val = uf.find(char_val)
+
+            res.append(chr(parent_char_val + 97))
+
+        return ''.join(res)
