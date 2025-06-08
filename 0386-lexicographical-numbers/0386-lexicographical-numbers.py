5bd258bdb24e5c76afb29d013a05779e160db7f0
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-06-08 12:18:35 +0530

    Time: 11 ms (72.66%), Space: 21.3 MB (58.51%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.py
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -1,46 +1,46 @@
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
-        # res = []
-
-        # cur = 1
-
-        # for _ in range(n):
-        #     res.append(cur)
-
-        #     if cur * 10 <= n:
-        #         cur *= 10
-            
-        #     else:
-        #         if cur >= n:
-        #             cur //= 10
-                
-        #         cur += 1
-
-        #         while not (cur % 10):
-        #             cur //= 10
-        
-        # return res
-
         res = []
 
+        cur = 1
 
-        def dfs(cur):
-            if cur > n:
-                return
-            
+        for _ in range(n):
             res.append(cur)
 
-            for i in range(10):
-                next_num = cur * 10 + i
+            if cur * 10 <= n:
+                cur *= 10
+            
+            else:
+                if cur >= n:
+                    cur //= 10
+                
+                cur += 1
 
-                if next_num > n:
-                    break
-                dfs(next_num)
-
-
-        for i in range(1, 10):
-            if i > n:
-                break
-            dfs(i)
-
+                while not (cur % 10):
+                    cur //= 10
+        
         return res
+
+        # res = []
+
+
+        # def dfs(cur):
+        #     if cur > n:
+        #         return
+            
+        #     res.append(cur)
+
+        #     for i in range(10):
+        #         next_num = cur * 10 + i
+
+        #         if next_num > n:
+        #             break
+        #         dfs(next_num)
+
+
+        # for i in range(1, 10):
+        #     if i > n:
+        #         break
+        #     dfs(i)
+
+        # return res
