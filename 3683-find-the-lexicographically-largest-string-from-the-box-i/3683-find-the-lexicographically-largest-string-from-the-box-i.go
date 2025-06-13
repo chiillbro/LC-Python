bb292d8c95b55f8a63b44d69eeac6f4cdd737642
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-06-13 20:58:22 +0530

    Time: 0 ms (100%), Space: 4.9 MB (77.73%) - LeetHub

---
+++ 3683-find-the-lexicographically-largest-string-from-the-box-i/3683-find-the-lexicographically-largest-string-from-the-box-i.go
@@ -0,0 +1,30 @@
+func answerString(word string, numFriends int) string {
+    if numFriends == 1 {
+        return word
+    }
+    n := len(word)
+
+    var res string
+
+    for i := 0; i < n; i++ {
+        res = max(res, word[i: min(n - numFriends + i + 1, n)])
+    }
+
+    return res
+}
+
+// func max[T string | int](a, b T) T {
+//         if a > b {
+//             return a
+//         }
+
+//         return b
+//     }
+
+// func min[T string | int](a, b T) T {
+//         if b < a {
+//             return b
+//         }
+
+//         return a
+//     }
