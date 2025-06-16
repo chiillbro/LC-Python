e6cf40a401b9c10adba7055578720d7ec770aea1
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-06-16 15:07:56 +0530

    Time: 0 ms (100%), Space: 18 MB (37.93%) - LeetHub

--- 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.py
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.py
@@ -18,16 +18,33 @@ class Solution:
         # okay, I've thought of a different approach, this approach solves the problem in O(n) but consumes O(n) space, let's see
 
 
-        suffix_max = [0] * n
-        suffix_max[n-1] = nums[-1]
+        # suffix_max = [0] * n
+        # suffix_max[n-1] = nums[-1]
 
-        for i in range(n-2, -1, -1):
-            suffix_max[i] = max(suffix_max[i+1], nums[i])
+        # for i in range(n-2, -1, -1):
+        #     suffix_max[i] = max(suffix_max[i+1], nums[i])
         
+        # _max_diff = -1
+
+        # for i in range(n-1):
+        #     if suffix_max[i+1] > nums[i]:
+        #         _max_diff = max(_max_diff, suffix_max[i+1] - nums[i])
+    
+        # return _max_diff
+
+
+        # okay, the above approach is not bad, but I've got the optimal solution from the editorial which I think is actually clever (and somewhat resembles Boyer's Moore voting algorithm)
+
+        prev_min = nums[0]
+
         _max_diff = -1
 
-        for i in range(n-1):
-            if suffix_max[i+1] > nums[i]:
-                _max_diff = max(_max_diff, suffix_max[i+1] - nums[i])
-    
+        for i in range(1, n):
+            if nums[i] > prev_min:
+                _max_diff = max(_max_diff, nums[i] - prev_min)
+            else:
+                prev_min = nums[i]
+
         return _max_diff
+
+
