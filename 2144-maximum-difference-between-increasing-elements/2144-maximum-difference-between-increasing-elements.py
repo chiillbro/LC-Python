fe80620c3789dd29467fd683f0bed7ce948f214d
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-06-16 15:01:34 +0530

    Time: 3 ms (51.98%), Space: 17.8 MB (59.91%) - LeetHub

--- 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.py
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.py
@@ -1,13 +1,33 @@
 class Solution:
     def maximumDifference(self, nums: List[int]) -> int:
         # nums: int[]
-        # brute force:
+        # Brute force:
         n = len(nums)
 
+        # _max_diff = -1
+        # for i in range(n-1):
+        #     for j in range(i+1, n):
+        #         if nums[j] > nums[i]:
+        #             _max_diff = max(_max_diff, nums[j] - nums[i])
+    
+        # return _max_diff
+
+
+        # now, the question is how to optimize this
+
+        # okay, I've thought of a different approach, this approach solves the problem in O(n) but consumes O(n) space
+
+
+        suffix_max = [0] * n
+        suffix_max[n-1] = nums[-1]
+
+        for i in range(n-2, -1, -1):
+            suffix_max[i] = max(suffix_max[i+1], nums[i])
+        
         _max_diff = -1
+
         for i in range(n-1):
-            for j in range(i+1, n):
-                if nums[j] > nums[i]:
-                    _max_diff = max(_max_diff, nums[j] - nums[i])
+            if suffix_max[i+1] > nums[i]:
+                _max_diff = max(_max_diff, suffix_max[i+1] - nums[i])
     
         return _max_diff
