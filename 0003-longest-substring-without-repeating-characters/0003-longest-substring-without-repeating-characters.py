06568234f4005fb33035fd1cbdde07af174c5cc5
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-06-17 14:07:19 +0530

    Time: 21 ms (36.87%), Space: 17.8 MB (52.94%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,21 +1,59 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # can be solved using sliding window and a set tracker
+        # can be solved using sliding window and a dict or a set
 
-        track = defaultdict(int)
+        # Approach 1
+
+        # track = defaultdict(int)
+        # left = 0
+
+        # ans = 0
+        # for right in range(len(s)):
+        #     track[s[right]] += 1
+
+        #     while len(track) < right - left + 1:
+        #         track[s[left]] -= 1
+        #         if not track[s[left]]:
+        #             del track[s[left]]
+        #         left += 1
+
+        #     ans = max(ans, len(track))
+        
+
+        # return ans
+
+
+        # Approach 2
+        
+        # track = set()
+        # left = 0
+
+        # ans = 0
+
+        # for right in range(len(s)):
+        #     while s[right] in track:
+        #         track.remove(s[left])
+        #         left += 1
+            
+        #     track.add(s[right])
+
+        #     ans = max(ans, len(track))
+
+        # return ans
+
+
+        # Approach 3
+        char_mask = [-1] * 256
         left = 0
 
         ans = 0
+
         for right in range(len(s)):
-            track[s[right]] += 1
+            if char_mask[ord(s[right])] != -1:
+                left = max(left, char_mask[ord(s[right])] + 1)
+            
+            char_mask[ord(s[right])] = right
 
-            while len(track) < right - left + 1:
-                track[s[left]] -= 1
-                if not track[s[left]]:
-                    del track[s[left]]
-                left += 1
-
-            ans = max(ans, len(track))
+            ans = max(ans, right - left + 1)
         
-
         return ans
