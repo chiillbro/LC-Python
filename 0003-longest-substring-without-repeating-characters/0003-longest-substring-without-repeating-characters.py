75aaf27017181cb6c8dd6ee58ad3aa5a3c80f0af
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-06-17 14:01:43 +0530

    Time: 23 ms (32.36%), Space: 17.7 MB (82.05%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,43 +1,21 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        n = len(s)
-        left = res = 0
+        # can be solved using sliding window and a set tracker
 
-        # counter = defaultdict(int)
+        track = defaultdict(int)
+        left = 0
 
-        # for right in range(n):
-        #     counter[s[right]] += 1
-        #     # if right > 0:
-        #     while left <= right and len(counter) < (right - left + 1):
-        #         counter[s[left]] -= 1
-        #         if not counter[s[left]]:
-        #             counter.pop(s[left])
-        #         left += 1
-            
-        #     res = max(res, right - left + 1)
+        ans = 0
+        for right in range(len(s)):
+            track[s[right]] += 1
+
+            while len(track) < right - left + 1:
+                track[s[left]] -= 1
+                if not track[s[left]]:
+                    del track[s[left]]
+                left += 1
+
+            ans = max(ans, len(track))
         
 
-        # More Cleaner and Direct approach using a Set
-
-        # unique = set()
-
-        # for right in range(n):
-        #     while s[right] in unique:
-        #         unique.remove(s[left])
-        #         left += 1
-            
-        #     unique.add(s[right])
-
-        #     res = max(res, right - left + 1)
-
-        char_mask = [-1] * 256
-
-        for right in range(n):
-            if char_mask[ord(s[right])] != -1:
-                left = max(char_mask[ord(s[right])] + 1, left)
-            
-            char_mask[ord(s[right])] = right
-
-            res = max(res, right - left + 1)
-            
-        return res
+        return ans
