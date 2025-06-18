57e9f06a4d040a37be10188fab03cb6c3073ab46
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-06-18 21:35:29 +0530

    Time: 71 ms (66.1%), Space: 18.4 MB (34.3%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,51 +1,45 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-
-        # Approach 1 using two for loops, O(N ^ 2) results in TLE
         n, m = len(s), len(t)
-
-        if n < m:
-            return ""
-        
-        start, min_wind = -1, float('inf')
-
-        # for i in range(n):
-        #     freq_map, count = Counter(t), 0
-        #     for j in range(i, n):
-        #         if freq_map[s[j]] > 0: count += 1
-
-        #         freq_map[s[j]] -= 1
-
-        #         if count == m and j - i + 1 < min_wind:
-        #             min_wind = j - i + 1
-        #             start = i
-        #             break
-        
-
-        # return s[start:start + min_wind] if min_wind != float("inf") else ""
-
-
-        # Optimal Solution using Sliding Window
-        
-        count = 0
-        freq_map = Counter(t)
-        
         left = 0
 
+        t_counts = Counter(t)
+
+        t_counts_len = len(t_counts)
+        start, end = -1, inf
+
+        s_counts = defaultdict(int)
+        
+        match_count = 0
         for right in range(n):
-            if freq_map[s[right]] > 0: count += 1
+            s_counts[s[right]] += 1
+            
+            if s[right] in t_counts and s_counts[s[right]] == t_counts[s[right]]: 
+                match_count += 1
 
-            freq_map[s[right]] -= 1
+            # elif s_counts[s[right]] == t_counts[s[right]] + 1:
+            #     match_count -= 1
+            
+            # if match_count == t_counts_len:
+            #     if right - left + 1 < end - start:
+            #         start, end = left, right
+            while match_count == t_counts_len and left <= right:
+                if right - left < end - start:
+                    start, end = left, right
 
-            while count == m:
-                if right - left + 1 < min_wind:
-                    min_wind = right - left + 1
-                    start = left
+                if s[left] in t_counts and s_counts[s[left]] == t_counts[s[left]]:
+                    match_count -= 1
+                # elif s_counts[s[left]] == t_counts[s[left]] - 1:
+                #     match_count -= 1
+
+                s_counts[s[left]] -= 1
                 
-                freq_map[s[left]] += 1
-                if freq_map[s[left]] > 0: count -= 1
                 left += 1
         
-        return "" if start == -1 else s[start:start + min_wind]
 
-        
+        if start == -1: return ""
+
+        return s[start:end+1]
+
+
+
