d6df6425510500409bc8043653f06b5f0f970ee5
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-06-20 09:16:02 +0530

    Time: 86 ms (60.04%), Space: 28.9 MB (91.86%) - LeetHub

--- 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.py
+++ 2387-partition-array-such-that-maximum-difference-is-k/2387-partition-array-such-that-maximum-difference-is-k.py
@@ -8,6 +8,36 @@ class Solution:
 
         partitions = 1
 
+        # Greedy Approach   
+        # example walk through
+        # [3, 5, 8, 1, 2, 4, 9], k = 2
+
+        # [1, 2, 3] [5, 4], [8, 9] # optimal
+        # [1, 2] [3, 5, 4] [8, 9] # optimal
+        # [1] [2, 3, 4] [5] [8, 9] # worst
+
+        # [1, 2, 3, 4, 5, 8, 9]
+
+        # min = 1
+
+        # partitions = 1
+
+        # cur - min > k # violating condition
+
+        # 2 - 1 > 2 # no
+
+        # 3 - 1 > 2 # no
+        # 4 - 1 = 3 > 2 # yes, it is greater than 2
+
+        # partitions += 1
+        # min = 4
+
+        # with the TC: O(n logn + n) => O(n log n)
+
+        # SC: Python, tim-sort, which has the worst sc of O(n)
+
+
+
         for num in nums:
             if num - cur > k:
                 partitions += 1
