08107c6ae72b90c3767e2ce56dbd45e17a3e78fc
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-06-25 22:03:21 +0530

    Time: 3824 ms (23.88%), Space: 25.7 MB (85.57%) - LeetHub

---
+++ 2150-kth-smallest-product-of-two-sorted-arrays/2150-kth-smallest-product-of-two-sorted-arrays.py
@@ -0,0 +1,30 @@
+class Solution:
+
+    def findCount(self, nums: List[int], x: int, v: int) -> int:
+        if x > 0:
+            return bisect.bisect_right(nums, v // x)
+        elif x < 0:
+            return len(nums) - bisect.bisect_left(nums, -(-v//x))
+        else:
+            return len(nums) if v >= 0 else 0
+
+
+    def kthSmallestProduct(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        n1 = len(nums1)
+
+        left, right = -(10**10), 10**10
+
+        while left <= right:
+            mid = left + ((right - left) >> 1)
+
+            count = 0
+
+            for i in range(n1):
+                count += self.findCount(nums2, nums1[i], mid)
+            
+            if count < k:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return left
