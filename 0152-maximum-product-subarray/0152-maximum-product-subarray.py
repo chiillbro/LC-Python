58361d28d9559e38df85e9e49ac6a50c77c868ef
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-06-26 23:06:55 +0530

    Time: 5 ms (94.32%), Space: 18.1 MB (95.99%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -2,14 +2,40 @@ class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         # Kandane's Algo (Dynamic Programming Approach)
 
-        res = min_so_far = max_so_far = nums[0]
+        # res = min_so_far = max_so_far = nums[0]
 
-        for num in nums[1:]:
-            temp = min(num, min_so_far * num, max_so_far * num)
-            max_so_far = max(num, min_so_far * num, max_so_far * num)
+        # for num in nums[1:]:
+        #     temp = min(num, min_so_far * num, max_so_far * num)
+        #     max_so_far = max(num, min_so_far * num, max_so_far * num)
 
-            min_so_far = temp
+        #     min_so_far = temp
 
-            res = max(res, max_so_far)
+        #     res = max(res, max_so_far)
         
-        return res
+        # return res
+
+
+        # More Intuitve
+
+        n = len(nums)
+        max_product = -math.inf
+        
+        left_product = right_product = 1
+
+        for left in range(n):
+            right = n - left - 1
+
+            left_product *= nums[left]
+            right_product *= nums[right]
+
+            max_product = max(max_product, left_product, right_product)
+
+            if left_product == 0:
+                left_product = 1
+            
+            if right_product == 0:
+                right_product = 1
+        
+        return max_product
+
+            
