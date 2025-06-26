c98c54906c55f015b2b82947da51e312d7ec280f
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-06-26 22:34:05 +0530

    Time: 7 ms (62.44%), Space: 20.6 MB (57.29%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,5 +1,46 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
+        # In any given array, only two elements can exist with the frequency n // 3
+
+        n = len(nums)
+
+        k = n // 3
+
+        candidate1 = candidate2 = None
+        cnt1 = cnt2 = 0
+
+        for num in nums:
+            if num == candidate1:
+                cnt1 += 1
+            elif num == candidate2:
+                cnt2 += 1
+            elif cnt1 == 0:
+                candidate1 = num
+                cnt1 = 1
+            elif cnt2 == 0:
+                candidate2 = num
+                cnt2 = 1
+            else:
+                cnt1 -= 1
+                cnt2 -= 1
+        
+        cnt1 = cnt2 = 0
+
+        for num in nums:
+            if num == candidate1:
+                cnt1 += 1
+        
+            if num == candidate2:
+                cnt2 += 1
+        
+        res = []
+        if cnt1 > k:
+            res.append(candidate1)
+
+        if cnt2 > k and candidate2 != candidate1:
+            res.append(candidate2)
+        
+        return res
 
         n = len(nums) // 3
         # count = Counter(nums)
