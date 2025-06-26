97b400cfd66d80cda42ace54a4377588f82d17f5
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-06-27 01:50:17 +0530

    Time: 23 ms (52.22%), Space: 19.8 MB (89.52%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -12,3 +12,19 @@ class Solution:
             j += 1
         
         return i
+
+
+        # DP, for learning, memory limit exceeded, O(n * m) for both complexities
+
+        # n, m = len(g), len(s)
+        # dp = [[0] * (m+1) for _ in range(n+1)]
+
+
+        # for i in range(1, n+1):
+        #     for j in range(1, m+1):
+        #         if g[i-1] <= s[j-1]:
+        #             dp[i][j] = dp[i-1][j-1] + 1
+        #         else:
+        #             dp[i][j] = dp[i-1][j]
+        
+        # return dp[-1][-1]
