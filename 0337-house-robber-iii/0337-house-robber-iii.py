b81a8a15854c3e05e8fe207b4793113fc9fee9a1
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-06-27 00:16:01 +0530

    Time: 6 ms (40.41%), Space: 20.1 MB (28.84%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.py
+++ 0337-house-robber-iii/0337-house-robber-iii.py
@@ -6,15 +6,45 @@
 #         self.right = right
 class Solution:
     def rob(self, root: Optional[TreeNode]) -> int:
-        def dfs(node):
-            if not node: return 0, 0
 
-            l1, l2 = dfs(node.left)
-            r1, r2 = dfs(node.right)
+        # Greedy, Dynamic Programming
 
-            rob = node.val + l2 + r2
-            not_rob = max(l1, l2) + max(r1, r2)
+        # def dfs(node):
+        #     if not node: return 0, 0
 
-            return rob, not_rob
+        #     l1, l2 = dfs(node.left)
+        #     r1, r2 = dfs(node.right)
+
+        #     rob = node.val + l2 + r2
+        #     not_rob = max(l1, l2) + max(r1, r2)
+
+        #     return rob, not_rob
         
-        return max(dfs(root))
+        # return max(dfs(root))
+
+
+        # Brute Recursion
+
+        # Memoization
+
+        memo = {}
+        def helper(node):
+            if not node: return 0
+
+            if node in memo:
+                return memo[node]
+        
+            val = 0
+
+            if node.left:
+                val += helper(node.left.left) + helper(node.left.right)
+            
+            if node.right:
+                val += helper(node.right.left) + helper(node.right.right)
+            
+            memo[node] = max(val + node.val, helper(node.left) + helper(node.right))
+
+            return memo[node]
+        
+
+        return helper(root)
