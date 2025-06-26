7681c29a7743e54c7028a965d9cbf3017772ff8e
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-06-26 23:35:29 +0530

    Time: 0 ms (100%), Space: 17.7 MB (82.74%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -3,13 +3,20 @@ class Solution:
 
         n = len(nums)
 
-        dp = [0] * (n+1)
+        # dp = [0] * (n+1)
 
         # Base Case
-        dp[1] = nums[0]
+        # dp[1] = nums[0]
+
+        # Space Optimization
+        prev1, prev2 = 0, nums[0]
 
 
         for i in range(2, n+1):
-            dp[i] = max(dp[i-1], dp[i-2] + nums[i-1])
+            # dp[i] = max(dp[i-1], dp[i-2] + nums[i-1])
+            cur = max(prev2, prev1 + nums[i-1])
+            prev1, prev2 = prev2, cur
+
         
-        return dp[n]
+        return prev2
+        # return dp[n]
