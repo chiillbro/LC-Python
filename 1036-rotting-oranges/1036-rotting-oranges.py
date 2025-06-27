b0f747631246a37638e47f1e7276925e7749222c
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-06-27 08:39:34 +0530

    Time: 3 ms (82.01%), Space: 17.8 MB (44.43%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,32 +1,36 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        ROWS, COLS = len(grid), len(grid[0])
+        n, m = len(grid), len(grid[0])
 
+
+        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        grid_copy = grid.copy()
+
+        queue = deque()
         minutes, fresh = 0, 0
-        q = deque()
-
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] == 1:
+        
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 2:
+                    queue.append((i, j))
+                elif grid[i][j] == 1:
                     fresh += 1
-                elif grid[r][c] == 2:
-                    q.append([r, c])
         
-        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        while queue and fresh:
+            level = len(queue)
 
-        while q and fresh:
-            for _ in range(len(q)):
-                rot_row, rot_col = q.popleft()
-                for dr, dc in directions:
-                    row, col = rot_row + dr, rot_col + dc
-                    if (row < 0 or row >= ROWS or
-                        col < 0 or col >= COLS or
-                        grid[row][col] != 1):
-                        continue
-                    grid[row][col] = 2
-                    fresh -= 1
-                    q.append([row, col])
+            for _ in range(level):
+                cur_r, cur_c = queue.popleft()
+
+                for d_r, d_c in directions:
+                    r, c = cur_r + d_r, cur_c + d_c
+
+                    if 0 <= r < n and 0 <= c < m and grid_copy[r][c] == 1:
+                        grid_copy[r][c] = 2
+                        fresh -= 1
+                        queue.append((r, c))
+            
             minutes += 1
-        
-        return minutes if not fresh else -1
 
+
+        return minutes if fresh == 0 else -1
