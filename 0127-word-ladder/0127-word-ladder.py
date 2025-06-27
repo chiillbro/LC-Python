c203aa609846c7e78c5b50357a13dbee32332819
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-06-27 08:49:04 +0530

    Time: 283 ms (53.47%), Space: 18.8 MB (74.38%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,35 +1,30 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        wordList = set(wordList)
-        if endWord not in wordList:
+        wordSet = set(wordList)
+
+        if endWord not in wordSet:
             return 0
         
-        # adj_list = defaultdict(list)
-        # wordList.add(beginWord)
-        # for word in wordList:
-        #     for j in range(len(word)):
-        #         pattern = word[:j] + "*" + word[j+1:]
-        #         adj_list[pattern].append(word)
-
-        visited = set([beginWord])
         queue = deque([beginWord])
+        seen = set([beginWord])
+
         steps = 1
-
         while queue:
-            for i in range(len(queue)):
-                word = queue.popleft()
-                if word == endWord:
-                    return steps
+            for _ in range(len(queue)):
+                cur_word = queue.popleft()
 
-                for j in range(len(word)):
-                    # pattern = word[:j] + "*" + word[j+1:]
-                    # for nei in adj_list[pattern]:
-                    #     if nei not in visited:
-                    for c in "abcdefghijklmnopqrstuvwxyz":
-                        new_word = word[:j] + c + word[j+1:]
-                        if new_word in wordList and new_word not in visited:
-                            visited.add(new_word)
+                if cur_word == endWord:
+                    return steps
+                
+                for i in range(len(cur_word)):
+                    for c in 'abcdefghijklmnopqrstuvwxyz':
+                        new_word = cur_word[:i] + c + cur_word[i+1:]
+
+                        if new_word in wordSet and new_word not in seen:
+                            seen.add(new_word)
                             queue.append(new_word)
+                    
+            
             steps += 1
         
         return 0
