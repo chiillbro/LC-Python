45f0fcf3572021898e4d77b2e476027712557595
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-06-27 09:31:40 +0530

    Time: 3 ms (77%), Space: 19.1 MB (71.94%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,33 +1,29 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        prereq_map = {i : [] for i in range(numCourses)}
+        adj_list = defaultdict(list)
 
-        for crs, prereq in prerequisites:
-            prereq_map[crs].append(prereq)
-        
-        visited = [False] * numCourses
-        onStack = [False] * numCourses
-        res = []
-
-        def dfs(crs):
-            visited[crs] = True
-            onStack[crs] = True
-
-            for pre in prereq_map[crs]:
-                if not visited[pre]:
-                    if not dfs(pre):
-                        return False
-                elif onStack[pre]:
-                    return False
-            
-            res.append(crs)
-            onStack[crs] = False
-            return True
+        for u, v in prerequisites:
+            adj_list[v].append(u)
         
 
-        for crs in range(numCourses):
-            if not visited[crs]:
-                if not dfs(crs):
-                    return []
+        in_degree = [0] * numCourses
+
+        for prereq in range(numCourses):
+            for crs in adj_list[prereq]:
+                in_degree[crs] += 1
         
-        return res
+        queue = deque(i for i in range(numCourses) if in_degree[i] == 0)
+
+        topo = []
+        while queue:
+            cur = queue.popleft()
+            topo.append(cur)
+
+            for crs in adj_list[cur]:
+                in_degree[crs] -= 1
+
+                if in_degree[crs] == 0:
+                    queue.append(crs)
+        
+
+        return topo if len(topo) == numCourses else []
