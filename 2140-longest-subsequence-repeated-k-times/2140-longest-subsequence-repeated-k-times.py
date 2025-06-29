c0896af6acdc47bf0c6e8117062670a40a85173d
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-06-29 07:36:14 +0530

    Time: 899 ms (85.89%), Space: 17.8 MB (69.19%) - LeetHub

--- 2140-longest-subsequence-repeated-k-times/2140-longest-subsequence-repeated-k-times.py
+++ 2140-longest-subsequence-repeated-k-times/2140-longest-subsequence-repeated-k-times.py
@@ -6,7 +6,6 @@ class Solution:
             [c for c, cnt in Counter(s).items() if cnt >= k], reverse=True
         )
 
-
         queue = deque(candidate)
 
         res = ""
@@ -19,9 +18,17 @@ class Solution:
             
             for ch in candidate:
                 nxt = cur + ch
+                # print("nxt", nxt)
                 it = iter(s)
 
+                # the below step does three things
+                    # 1. for every character (ch) in nxt*k
+                    # 2. the step "ch in it", the iterator "it" of s will scan for this ch, if
+                        # found it will advance to next position of found position of ch for checking other characters which makes sense because we are checking for subsequence
+                    # 3. if all of the characters found in a sequence, then append it to queue
                 if all(ch in it for ch in nxt*k):
                     queue.append(nxt)
+
+                # print("queue in loop", queue)
         
         return res
