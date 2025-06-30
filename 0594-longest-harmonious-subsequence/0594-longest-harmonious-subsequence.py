919fbde145d5550ebf0bf0056f6a4516ebdf1eae
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-06-30 09:04:07 +0530

    Time: 43 ms (37.55%), Space: 19.3 MB (11.14%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
@@ -5,11 +5,11 @@ class Solution:
         res = 0
         for num in nums:
             max_freq = None
-            if num + 1 in freq:
-                max_freq = freq[num+1]
-            
             if num - 1 in freq:
-                max_freq = max(max_freq, freq[num-1]) if max_freq != None else freq[num-1]
+                max_freq = freq[num-1]
+            
+            # if num - 1 in freq:
+            #     max_freq = max(max_freq, freq[num-1]) if max_freq != None else freq[num-1]
             
             if max_freq:
                 res = max(res, freq[num] + max_freq)
