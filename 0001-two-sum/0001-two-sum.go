918c8f8a40f015f45c088835d087175cede5df33
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-02 19:48:50 +0530

    Time: 3 ms (43.25%), Space: 5.9 MB (61.81%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,17 +1,19 @@
 func twoSum(nums []int, target int) []int {
     look_up := make(map[int]int)
 
-    for i, num := range nums {
-        look_up[num] = i
-    }
+    // for i, num := range nums {
+    //     look_up[num] = i
+    // }
 
 
     for i, num := range nums {
         compliment := target - num
 
-        if idx, ok := look_up[compliment]; ok && i != idx {
+        if idx, ok := look_up[compliment]; ok {
             return []int{idx, i}
         }
+
+        look_up[num] = i
     }
 
     return []int{}
