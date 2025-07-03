3129a5be97753c30848b82913e73e2bd0a43daea
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-04 01:39:46 +0530

    Time: 5 ms (33.36%), Space: 10.1 MB (58.06%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.go
@@ -0,0 +1,34 @@
+func productExceptSelf(nums []int) []int {
+
+    n := len(nums)
+
+    // pref := [n]int{}
+    mulLeft := make([]int, n)
+
+    mulLeft[0] = nums[0]
+
+    for i := 1; i < n; i++ {
+        mulLeft[i] = mulLeft[i-1] * nums[i]
+    }
+
+    mulRight := make([]int, n)
+
+    mulRight[n-1] = nums[n-1]
+
+    for i := n-2; i >= 0; i-- {
+        mulRight[i] = mulRight[i+1] * nums[i]
+    }
+
+    // res := [n]int{}
+    res := make([]int, n)
+
+    res[0] = mulRight[1]
+
+    res[n-1] = mulLeft[n-2]
+
+    for i := 1; i < n-1; i++ {
+        res[i] = mulLeft[i-1] * mulRight[i+1]
+    }
+
+    return res
+}
