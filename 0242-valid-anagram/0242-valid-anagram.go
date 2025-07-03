908cbfe718f10961746ca497c8b4b2a5bae43c6b
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-03 23:57:54 +0530

    Time: 1 ms (76.82%), Space: 4.8 MB (34.9%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.go
+++ 0242-valid-anagram/0242-valid-anagram.go
@@ -1,21 +1,50 @@
+import (
+    "unicode/utf8"
+)
+
 func isAnagram(s string, t string) bool {
 
-    if len(s) != len(t) {
+    // if len(s) != len(t) {
+    //     return false
+    // }
+
+    // freq := [26]int{} // array
+
+    // for i, char := range s {
+    //     freq[char - 97]++
+    //     freq[t[i] - 97]--
+    // }
+
+    // for _, v := range freq {
+    //     if v != 0 {
+    //         return false
+    //     }
+    // }
+
+    // return true
+
+
+
+    // Follow-up, handling arbitrary unicode, I'll make use of runes as I cannot directly index into
+    // a fixed size array
+
+    if utf8.RuneCountInString(s) != utf8.RuneCountInString(t) {
         return false
     }
 
-    freq := make([]int, 26)
+    freq := make(map[rune]int)
 
-    for i, char := range s {
-        freq[char - 97]++
-        freq[t[i] - 97]--
+    for _, c := range s {
+        freq[c]++
     }
 
-    for _, v := range freq {
-        if v != 0 {
-            return false
+    for _, c := range t {
+        freq[c]--
+
+        if freq[c] == 0 {
+            delete(freq, c)
         }
     }
 
-    return true
+    return len(freq) == 0
 }
