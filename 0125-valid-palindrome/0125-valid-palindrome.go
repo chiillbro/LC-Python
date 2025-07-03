0610a458b622fc0dc0aa6a9219f3462ac9f99e64
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-04 02:37:29 +0530

    Time: 0 ms (100%), Space: 4.7 MB (76.31%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.go
@@ -0,0 +1,29 @@
+import (
+    "unicode"
+    "strings"
+)
+
+func isPalindrome(s string) bool {
+    f := func (r rune) rune {
+        if !unicode.IsLetter(r) && !unicode.IsNumber(r) {
+            return -1
+        }
+
+        return unicode.ToLower(r)
+    }
+
+
+    s = strings.Map(f, s)
+
+    l, r := 0, len(s) - 1
+    for l < r {
+        if s[l] != s[r] {
+            return false
+        }
+
+        l++
+        r--
+    }
+
+    return true
+}
