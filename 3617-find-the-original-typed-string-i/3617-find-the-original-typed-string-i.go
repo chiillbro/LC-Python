0c9872802c2328867b04e412df44e0fcee732d4c
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-03 08:20:22 +0530

    Time: 4 ms (22.49%), Space: 4 MB (76.84%) - LeetHub

--- 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.go
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.go
@@ -1,5 +1,13 @@
+// import (
+//     "fmt"
+// )
+
 func possibleStringCount(word string) int {
-    runes := []rune(word)
+    // runes := []rune("你好你好")
+
+    // fmt.Println("word", word)
+
+    // fmt.Println("runes", runes)
     // counts := make(map[rune]int, len(runes))
 
     // for _, r := range runes {
@@ -15,13 +23,15 @@ func possibleStringCount(word string) int {
 
     // return res
 
-    n := len(runes)
+    n := len(word)
     res := 1
 
     i := 0
     for i < n {
         j := i + 1
-        for j < n && runes[i] == runes[j] {
+        for j < n && word[i] == word[j] {
+            // fmt.Printf("type of %v is %T\n", word[i], word[i])
+            // fmt.Println(word[i], word[j])
             j++
         }
 
