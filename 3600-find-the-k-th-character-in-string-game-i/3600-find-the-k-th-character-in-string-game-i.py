b4a5e04a29dd7f2b5889e4df641b5e6594733c8e
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-03 23:40:49 +0530

    Time: 0 ms (100%), Space: 17.7 MB (85.53%) - LeetHub

--- 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.py
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.py
@@ -1,22 +1,35 @@
 class Solution:
     def kthCharacter(self, k: int) -> str:
-        queue = deque(["a"])
 
-        count = 1
+        # Optimal Approch
 
-        while count < k:
-            print("queue", queue)
-            for i in range(len(queue)):
-                cur = queue[i]
+        return chr(97 + (k-1).bit_count())
 
-                print("cur", cur)
-                nxt = chr(ord(cur) + 1)
+        # res = 0
 
-                print("nxt", nxt)
+        # while k != 1:
+        #     t = k.bit_length() - 1
+        #     if (1 << 1) == k:
+        #         t -= 1
+            
+        #     k = k - (1 << t)
 
-                queue.append(nxt)
+        #     res += 1
+        
+        # return chr(97 + res)
+        # queue = deque(["a"])
 
-                count += 1
+        # count = 1
+
+        # while count < k:
+        #     for i in range(len(queue)):
+        #         cur = queue[i]
+
+        #         nxt = chr(ord(cur) + 1)
+
+        #         queue.append(nxt)
+
+        #         count += 1
 
 
-        return queue[k-1]
+        # return queue[k-1]
