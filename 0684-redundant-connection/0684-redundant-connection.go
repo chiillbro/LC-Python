d3048f24b2456b9723a32f2ffb7d7e0831b91755
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-05 07:50:23 +0530

    Time: 0 ms (100%), Space: 4.8 MB (44.08%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.go
+++ 0684-redundant-connection/0684-redundant-connection.go
@@ -1,66 +1,164 @@
+// var cycleStart int = -1
+
+type DSU struct {
+    parent []int
+    rank []int
+}
+
+func UnionFind (n int) *DSU {
+    parent := make([]int, n)
+
+    for i := range n {
+        parent[i] = i
+    }
+
+    rank := make([]int, n)
+
+    for i := range n {
+        rank[i] = 1
+    }
+
+    return &DSU{
+        parent, rank,
+    }
+}
+
+func (uf *DSU) find(x int) int {
+    if uf.parent[x] != x {
+        uf.parent[x] = uf.find(uf.parent[x])
+    }
+
+    return uf.parent[x]
+}
+
+func (uf *DSU) union(x, y int) bool {
+    xRoot, yRoot := uf.find(x), uf.find(y)
+
+    if xRoot == yRoot {
+        return false
+    }
+
+    if uf.rank[xRoot] < uf.rank[yRoot] {
+        xRoot, yRoot = yRoot, xRoot
+    }
+
+    uf.parent[yRoot] = xRoot
+    uf.rank[xRoot] += uf.rank[yRoot]
+
+    return true
+}
+
 func findRedundantConnection(edges [][]int) []int {
     N := len(edges)
 
-    adjList := make(map[int][]int)
+    // adjList := make(map[int][]int)
+
+    uf := UnionFind(N)
 
     for _, edge := range edges {
-        visited := make([]bool, N)
+        u, v := edge[0]-1, edge[1]-1
 
-        if isConnected(edge[0]-1, edge[1]-1, visited, adjList) {
+        if !uf.union(u, v) {
             return edge
         }
-
-        adjList[edge[0]-1] = append(adjList[edge[0]-1], edge[1]-1)
-        adjList[edge[1]-1] = append(adjList[edge[1]-1], edge[0]-1)
     }
 
     return []int{}
 
+
+    // Approach 1: DFS (Brute)
     // for _, edge := range edges {
-    //     u, v := edge[0], edge[1]
+    //     visited := make([]bool, N)
+
+    //     if isConnected(edge[0]-1, edge[1]-1, visited, adjList) {
+    //         return edge
+    //     }
+
+    //     adjList[edge[0]-1] = append(adjList[edge[0]-1], edge[1]-1)
+    //     adjList[edge[1]-1] = append(adjList[edge[1]-1], edge[0]-1)
+    // }
+
+    // return []int{}
+
+    // for _, edge := range edges {
+    //     u, v := edge[0]-1, edge[1]-1
     //     adjList[u] = append(adjList[u], v)
 
     //     adjList[v] = append(adjList[v], u)
     // }
 
-    // visited := make([]bool, n+1)
+    // parent := make([]int, N)
 
-    // var dfs func (cur, parent int)
+    // for i := range N {
+    //     parent[i] = -1
+    // }
 
-    // dfs = func (cur, parent int) {
-    //     visited[cur] = true
+    // visited := make([]bool, N)
 
-    //     for _, neigh := range adjList[cur] {
+    // var dfs func (cur int)
+
+    // dfs = func (src int) {
+    //     visited[src] = true
+
+    //     for _, neigh := range adjList[src] {
     //         if !visited[neigh] {
-    //             if 
+    //              parent[neigh] = src
+    //              dfs(neigh)
+    //         } else if neigh != parent[src] && cycleStart == -1 {
+    //             cycleStart = neigh
+    //             parent[neigh] = src
     //         }
     //     }
     // }
 
+    // dfs(0)
 
+    // cycleNodes := make(map[int]struct{})
 
-    // return dfs(1, -1)
+    // node := cycleStart
+    // for {
+    //     cycleNodes[node] = struct{}{}
+    //     node = parent[node]
+    //     if node == cycleStart {
+    //         break
+    //     }
+    // }
 
+    // for i := N-1; i >= 0; i-- {
+    //     edge := edges[i]
 
+    //     // if cycleNodes[edge[0]-1] != nil && cycleNodes[edge[1]-1] != nil {
+    //     //     return edge
+    //     // }
+
+    //     _, exists := cycleNodes[edge[0]-1]
+    //     _, ok := cycleNodes[edge[1]-1]
+
+    //     if exists && ok {
+    //         return edge
+    //     }
+    // }
+
+    // return []int{}
 
 }
 
 
-func isConnected(src, parent int, visited []bool, adjList map[int][]int) bool {
-    visited[src] = true
+// func isConnected(src, parent int, visited []bool, adjList map[int][]int) bool {
+//     visited[src] = true
 
-    if src == parent {
-        return true
-    }
+//     if src == parent {
+//         return true
+//     }
 
-    isFound := false
+//     isFound := false
 
-    for _, neigh := range adjList[src] {
-        if !visited[neigh] {
-            isFound = isFound || isConnected(neigh, parent, visited, adjList)
-        }
-    }
+//     for _, neigh := range adjList[src] {
+//         if !visited[neigh] {
+//             isFound = isFound || isConnected(neigh, parent, visited, adjList)
+//         }
+//     }
 
 
-    return isFound
-}
+//     return isFound
+// }
