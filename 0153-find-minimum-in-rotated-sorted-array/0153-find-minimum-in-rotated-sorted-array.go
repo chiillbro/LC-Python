a370b009b7171844217af9b64f3cfb31498c0362
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-06 21:30:05 +0530

    Time: 0 ms (100%), Space: 4.4 MB (60.56%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.go
@@ -0,0 +1,20 @@
+func findMin(nums []int) int {
+    n := len(nums)
+
+
+    left, right := 0, n - 1
+
+    for left <= right {
+        mid := (left + right) >> 1
+
+        if nums[0] <= nums[mid] && nums[n-1] < nums[mid] {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+
+    // fmt.Println("left, right", left, right)
+
+    return nums[left]
+}
