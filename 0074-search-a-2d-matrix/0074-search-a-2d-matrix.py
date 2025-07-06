8d3d617f74a309eef8e43e9d4b80d30ca8944a20
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-06 17:46:06 +0530

    Time: 0 ms (100%), Space: 18.2 MB (71.66%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,29 +1,46 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        left, right = 0, len(matrix) - 1
+        n, m = len(matrix), len(matrix[0])
+
+        left, right = 0, n*m - 1
+
 
         while left <= right:
             mid = (left + right) >> 1
 
-            if matrix[mid][0] == target:
-                return True
-            elif matrix[mid][0] > target:
-                right = mid - 1
-            else:
-                left = mid + 1
-        
-        print('mid', "left", "right", mid, left, right)
-        row  = right
-        left, right = 0, len(matrix[0]) - 1
+            row, col = mid // m, mid%m
 
-        while left <= right:
-            mid = (left + right) >> 1
-
-            if matrix[row][mid] == target:
+            if matrix[row][col] == target:
                 return True
-            elif matrix[row][mid] > target:
+            elif matrix[row][col] > target:
                 right = mid - 1
             else:
                 left = mid + 1
         
         return False
+        # left, right = 0, len(matrix) - 1
+
+        # while left <= right:
+        #     mid = (left + right) >> 1
+
+        #     if matrix[mid][0] == target:
+        #         return True
+        #     elif matrix[mid][0] > target:
+        #         right = mid - 1
+        #     else:
+        #         left = mid + 1
+        
+        # row  = right
+        # left, right = 0, len(matrix[0]) - 1
+
+        # while left <= right:
+        #     mid = (left + right) >> 1
+
+        #     if matrix[row][mid] == target:
+        #         return True
+        #     elif matrix[row][mid] > target:
+        #         right = mid - 1
+        #     else:
+        #         left = mid + 1
+        
+        # return False
