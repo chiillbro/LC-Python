7c7a149617a348bd680aa4450bc9c21795dc04a7
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-07 20:28:17 +0530

    Time: 0 ms (100%), Space: 9.6 MB (91.83%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.go
+++ 0041-first-missing-positive/0041-first-missing-positive.go
@@ -1,19 +1,40 @@
 func firstMissingPositive(nums []int) int {
-    numsSet := make(map[int]struct{}, len(nums))
 
-    for _, num := range nums {
-        numsSet[num] = struct{}{}
-    }
+    // Most optimal and clever approach
 
-    for i := range len(nums) + 2 {
-        if i == 0 {
-            continue
-        }
-        if _, exists := numsSet[i]; !exists {
-            return i
+    n := len(nums)
+    for i := range n {
+        for 1 <= nums[i] && nums[i] <= n && nums[nums[i]-1] != nums[i] {
+            correctIdx := nums[i] - 1
+
+            nums[correctIdx], nums[i] = nums[i], nums[correctIdx]
         }
     }
 
-    return -1
+
+    for i := range n {
+        if nums[i] != i + 1 {
+            return i + 1
+        }
+    }
+
+    return n + 1
+
+    // numsSet := make(map[int]struct{}, len(nums))
+
+    // for _, num := range nums {
+    //     numsSet[num] = struct{}{}
+    // }
+
+    // for i := range len(nums) + 2 {
+    //     if i == 0 {
+    //         continue
+    //     }
+    //     if _, exists := numsSet[i]; !exists {
+    //         return i
+    //     }
+    // }
+
+    // return -1
 
 }
