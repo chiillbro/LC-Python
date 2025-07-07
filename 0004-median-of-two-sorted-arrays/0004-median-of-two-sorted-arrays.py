2664dd1113eab38371f7807a3941cae72e81b9d4
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-07 19:46:48 +0530

    Time: 4 ms (34.25%), Space: 18 MB (82.89%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,81 +1,42 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        # num1, num2 = 0, 0
-        n1, n2 = len(nums1), len(nums2)
-        N = n1 + n2
-        # idx1, idx2 = (N // 2) - 1, (N // 2)
+        n, m = len(nums1), len(nums2)
 
-        # i = j = 0
-
-        # ** Approach 1 using two pass : Better Approach **
-
-        # while i < n1 and j < n2:
-        #     cnt = i + j
-        #     if nums1[i] < nums2[j]:
-        #         if cnt == idx1:
-        #             num1 = nums1[i]
-        #         elif cnt == idx2:
-        #             num2 = nums1[i]
-        #         i += 1
-        #     else:
-        #         if cnt == idx1:
-        #             num1 = nums2[j]
-        #         elif cnt == idx2:
-        #             num2 = nums2[j]
-        #         j += 1
-        
-        # if num1 and num2:
-        #     if (N & 1) == 0:
-        #         return (num1 + num2) / 2.0
-        #     else:
-        #         return float(num2)
-            
-        # while i < n1:
-        #     cnt = i + j
-        #     if cnt == idx1:
-        #         num1 = nums1[i]
-        #     elif cnt == idx2:
-        #         num2 = nums1[i]
-        #     i += 1
-        # while j < n2:
-        #     cnt = i + j
-        #     if cnt == idx1:
-        #         num1 = nums2[j]
-        #     elif cnt == idx2:
-        #         num2 = nums2[j]
-        #     j += 1
-
-        # if (N & 1) == 0:
-        #     return (num1 + num2) / 2.0
-        # else:
-        #     return float(num2)
-
-
-        # ** Binary Search Approach : As the problem asked O(log (n1 + n2)) **
-        if n1 > n2:
+        if m > n:
             return self.findMedianSortedArrays(nums2, nums1)
-        low, high = 0, n1
-        elements = (N + 1) >> 1
 
-        while low <= high:
-            mid1 = (low + high) >> 1
-            mid2 = elements - mid1
-            l1 = l2 = float("-inf")
-            r1 = r2 = float("inf")
+        
+        left, right = 0, m
 
-            if mid1 < n1: r1 = nums1[mid1]
-            if mid2 < n2: r2 = nums2[mid2]
+        elements_required = (n + m + 1) >> 1 # addition of one to n + m accounts for odd number of elements
 
-            if mid1 > 0: l1 = nums1[mid1 - 1]
-            if mid2 > 0: l2 = nums2[mid2 - 1]
+        while left <= right:
+            mid1 = (left + right) >> 1
 
-            if l1 <= r2 and l2 <= r1:
-                if not N & 1:
-                    return (min(r1, r2) + max(l1, l2)) / 2.0
-                else:
-                    return max(l1, l2)
-            elif l1 > r2: high = mid1 - 1
-            else: low = mid1 + 1
+            mid2 = elements_required - mid1
 
+            l1 = l2 = -inf
+            r1 = r2 = inf
 
+            if mid1 > 0:
+                l1 = nums2[mid1-1]
             
+            if mid2 > 0:
+                l2 = nums1[mid2-1]
+            
+            if mid1 < m:
+                r1 = nums2[mid1]
+            
+            if mid2 < n:
+                r2 = nums1[mid2]
+            
+            if l1 <= r2 and l2 <= r1:
+                if (n + m) & 1:
+                    return max(l1, l2) * 1.0
+                else:
+                    return (max(l1, l2) + min(r1, r2)) / 2.0
+            
+            elif l1 > r2:
+                right = mid1 - 1
+            else:
+                left = mid1 + 1
