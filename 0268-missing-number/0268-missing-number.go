faad00aca7b2af30a96cb04da63edc055f0f91e5
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-07 20:12:38 +0530

    Time: 0 ms (100%), Space: 7.8 MB (87.98%) - LeetHub

--- 0268-missing-number/0268-missing-number.go
+++ 0268-missing-number/0268-missing-number.go
@@ -1,13 +1,27 @@
 func missingNumber(nums []int) int {
-    n := len(nums)
-    sumShouldBe := (n * (n + 1)) >> 1
+    // n := len(nums)
+    // sumShouldBe := (n * (n + 1)) >> 1
 
-    actualSum := 0
+    // actualSum := 0
 
-    for _, num := range nums {
-        actualSum += num
+    // for _, num := range nums {
+    //     actualSum += num
+    // }
+
+    // return sumShouldBe - actualSum
+    
+
+
+    // Using Bitwise XOR
+
+    xor1, xor2 := 0, 0
+
+    for i, num := range nums {
+        xor1 ^= num
+        xor2 ^= i + 1
     }
 
-    return sumShouldBe - actualSum
+    return xor1 ^ xor2
+
 
 }
