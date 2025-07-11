c3fd45c83b66b3d0bd444495f67e04c530314710
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-11 21:36:17 +0530

    Time: 255 ms (66.18%), Space: 33.5 MB (89.21%) - LeetHub

--- 2118-maximum-earnings-from-taxi/2118-maximum-earnings-from-taxi.py
+++ 2118-maximum-earnings-from-taxi/2118-maximum-earnings-from-taxi.py
@@ -13,27 +13,42 @@ class Solution:
 
             nxt[i] = bisect_left(starts, cur_end)
         
-        memo = {}
-        def dfs(cur_idx):
-            if cur_idx >= n:
-                return 0
+        # memo = {}
+        # def dfs(cur_idx):
+        #     if cur_idx >= n:
+        #         return 0
             
-            if rides[cur_idx][1] > k:
-                return 0
+        #     if rides[cur_idx][1] > k:
+        #         return 0
             
-            if cur_idx in memo:
-                return memo[cur_idx]
+        #     if cur_idx in memo:
+        #         return memo[cur_idx]
             
-            cur_earn = rides[cur_idx][1] - rides[cur_idx][0] + rides[cur_idx][2]
+        #     cur_earn = rides[cur_idx][1] - rides[cur_idx][0] + rides[cur_idx][2]
 
-            nxt_idx = nxt[cur_idx]
+        #     nxt_idx = nxt[cur_idx]
 
-            pick = cur_earn + dfs(nxt_idx)
+        #     pick = cur_earn + dfs(nxt_idx)
 
-            skip = dfs(cur_idx + 1)
+        #     skip = dfs(cur_idx + 1)
 
-            memo[cur_idx] = max(pick, skip)
+        #     memo[cur_idx] = max(pick, skip)
 
-            return memo[cur_idx]
+        #     return memo[cur_idx]
+
+        # return dfs(0)
+
+        dp = [0] * (n + 1)
+
+        for i in range(n-1, -1, -1):
+                cur_earn = rides[i][1] - rides[i][0] + rides[i][2]
+
+                nxt_idx = nxt[i]
+    
+                pick = cur_earn + dp[nxt_idx]
+    
+                skip = dp[i + 1]
+
+                dp[i] = max(pick, skip)
         
-        return dfs(0)
+        return dp[0]
