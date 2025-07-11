0482fc94efb70dd08e9fa5bfeb83a9dd9c645f28
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-11 21:46:30 +0530

    Time: 124 ms (80.15%), Space: 32.1 MB (64.11%) - LeetHub

--- 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.py
+++ 1352-maximum-profit-in-job-scheduling/1352-maximum-profit-in-job-scheduling.py
@@ -16,20 +16,30 @@ class Solution:
         for i in range(n):
             nxt[i] = bisect_left(starts, jobs[i][1])
         
-        memo = {}
-        def dfs(cur_idx):
-            if cur_idx >= n:
-                return 0
+        # memo = {}
+        # def dfs(cur_idx):
+        #     if cur_idx >= n:
+        #         return 0
 
-            if cur_idx in memo:
-                return memo[cur_idx]
+        #     if cur_idx in memo:
+        #         return memo[cur_idx]
             
-            perform = jobs[cur_idx][2] + dfs(nxt[cur_idx])
+        #     perform = jobs[cur_idx][2] + dfs(nxt[cur_idx])
 
-            skip = dfs(cur_idx + 1)
+        #     skip = dfs(cur_idx + 1)
 
-            memo[cur_idx] = max(perform, skip)
+        #     memo[cur_idx] = max(perform, skip)
 
-            return memo[cur_idx]
+        #     return memo[cur_idx]
+        # return dfs(0)
+
+        dp = [0] * (n + 1)
+
+        for i in range(n-1, -1, -1):
+            perform = jobs[i][2] + dp[nxt[i]]
+
+            skip = dp[i + 1]
+
+            dp[i] = max(perform, skip)
         
-        return dfs(0)
+        return dp[0]
