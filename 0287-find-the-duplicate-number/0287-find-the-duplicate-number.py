c1be8a6a25a4214bc0ce4cdac55ee71dcb34f544
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-11 23:35:42 +0530

    Time: 35 ms (33.65%), Space: 30 MB (48.65%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        tortoise = hare = 0
+
+        while True:
+            if not (hare < len(nums) and nums[hare] < len(nums)):
+                return -1
+
+            tortoise = nums[tortoise]
+
+            hare = nums[nums[hare]]
+
+            if tortoise == hare:
+                break
+        
+        p1 = 0
+
+        p2 = tortoise
+
+        while p1 != p2:
+            p1 = nums[p1]
+            p2 = nums[p2]
+        
+        return p1
