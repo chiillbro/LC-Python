3ad612abcb51cf2e7ac72ebfc9bcd6a39c65bbfc
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-11 22:20:05 +0530

    Time: 172 ms (83.19%), Space: 58.2 MB (72.55%) - LeetHub

--- 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.py
+++ 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.py
@@ -4,23 +4,40 @@ class Solution:
 
         n = len(events)
 
-        starts = [start for start, _, _ in events]
+        # starts = [start for start, _, _ in events]
 
-        nxt = [0] * n
+        # nxt = [0] * n
 
-        for i in range(n):
-            nxt[i] = bisect_right(starts, events[i][1])
+        # for i in range(n):
+        #     nxt[i] = bisect_right(starts, events[i][1])
         
-        dp = [[0] * 3 for _ in range(n + 1)]
+        # dp = [[0] * 3 for _ in range(n + 1)]
 
-        for i in range(n-1, -1, -1):
-            cur_val = events[i][2]
+        # for i in range(n-1, -1, -1):
+        #     cur_val = events[i][2]
 
-            for j in range(1, 3):
-                attend = cur_val + dp[nxt[i]][j-1]
+        #     for j in range(1, 3):
+        #         attend = cur_val + dp[nxt[i]][j-1]
 
-                skip = dp[i + 1][j]
+        #         skip = dp[i + 1][j]
 
-                dp[i][j] = max(attend, skip)
+        #         dp[i][j] = max(attend, skip)
         
-        return dp[0][2]
+        # return dp[0][2]
+
+
+        # Priority Queue Approach
+
+        pq = []
+        ans = 0
+        max_value = 0
+        for event in events:
+            while pq and pq[0][0] < event[0]:
+                max_value = max(pq[0][1], max_value)
+                heappop(pq)
+            
+            ans = max(ans, max_value + event[2])
+
+            heappush(pq, (event[1], event[2]))
+        
+        return ans
