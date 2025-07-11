10becc29e0a8bf105ed5005f7748aa19632a1c31
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-11 21:30:33 +0530

    Time: 322 ms (52.07%), Space: 41.3 MB (30.71%) - LeetHub

---
+++ 2118-maximum-earnings-from-taxi/2118-maximum-earnings-from-taxi.py
@@ -0,0 +1,39 @@
+class Solution:
+    def maxTaxiEarnings(self, k: int, rides: List[List[int]]) -> int:
+        rides.sort()
+
+        n = len(rides)
+
+        starts = [start for start, _, _ in rides]
+
+        nxt = [0] * n
+
+        for i in range(n):
+            cur_end = rides[i][1]
+
+            nxt[i] = bisect_left(starts, cur_end)
+        
+        memo = {}
+        def dfs(cur_idx):
+            if cur_idx >= n:
+                return 0
+            
+            if rides[cur_idx][1] > k:
+                return 0
+            
+            if cur_idx in memo:
+                return memo[cur_idx]
+            
+            cur_earn = rides[cur_idx][1] - rides[cur_idx][0] + rides[cur_idx][2]
+
+            nxt_idx = nxt[cur_idx]
+
+            pick = cur_earn + dfs(nxt_idx)
+
+            skip = dfs(cur_idx + 1)
+
+            memo[cur_idx] = max(pick, skip)
+
+            return memo[cur_idx]
+        
+        return dfs(0)
