ba5ea694ea5a4fa50c16289c1a12fc1dcb49f65b
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-11 22:23:19 +0530

    Time: 298 ms (45.96%), Space: 63.3 MB (25.11%) - LeetHub

--- 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.py
+++ 2164-two-best-non-overlapping-events/2164-two-best-non-overlapping-events.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxTwoEvents(self, events: List[List[int]]) -> int:
-        events.sort()
+        # events.sort()
 
         n = len(events)
 
@@ -28,16 +28,39 @@ class Solution:
 
         # Priority Queue Approach
 
-        pq = []
-        ans = 0
-        max_value = 0
-        for event in events:
-            while pq and pq[0][0] < event[0]:
-                max_value = max(pq[0][1], max_value)
-                heappop(pq)
+        # pq = []
+        # ans = 0
+        # max_value = 0
+        # for event in events:
+        #     while pq and pq[0][0] < event[0]:
+        #         max_value = max(pq[0][1], max_value)
+        #         heappop(pq)
             
-            ans = max(ans, max_value + event[2])
+        #     ans = max(ans, max_value + event[2])
 
-            heappush(pq, (event[1], event[2]))
+        #     heappush(pq, (event[1], event[2]))
         
+        # return ans
+
+
+        # Greedy Approach
+
+        times = []
+
+        for i in range(n):
+            times.append((events[i][0], 1, events[i][2]))
+            times.append((events[i][1] + 1, 0, events[i][2]))
+
+        
+        times.sort()
+
+        ans = max_val = 0
+
+        for time_val in times:
+            if time_val[1]:
+                ans = max(ans, max_val + time_val[2])
+            else:
+                max_val = max(max_val, time_val[2])
+
+    
         return ans
