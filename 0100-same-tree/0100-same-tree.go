93d477e62ccc948de93d8d6482be705f5b113448
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-12 22:00:42 +0530

    Time: 0 ms (100%), Space: 4.2 MB (65.16%) - LeetHub

--- 0100-same-tree/0100-same-tree.go
+++ 0100-same-tree/0100-same-tree.go
@@ -14,38 +14,32 @@ func isSameTree(p *TreeNode, q *TreeNode) bool {
     if p == nil || q == nil {
         return false
     }
-    stack1 := []*TreeNode{p}
 
-    stack2 := []*TreeNode{q}
+    type Pair struct {
+        node1, node2 *TreeNode
+    }
+    stack := make([]Pair, 0)
 
-    for len(stack1) > 0  && len(stack2) > 0 {
-        cur1, cur2 := stack1[len(stack1)-1], stack2[len(stack2)-1]
+    stack = append(stack, Pair{p, q})
+
+    for len(stack) > 0 {
+        nodes := stack[len(stack)-1]
+
+        node1, node2 := nodes.node1, nodes.node2
         
-        stack1 = stack1[:len(stack1)-1]
-        stack2 = stack2[:len(stack2)-1]
+        stack = stack[:len(stack)-1]
+        if node1 == nil && node2 == nil {
+            continue
+        }
 
-        if cur1.Val != cur2.Val {
+        if node1 == nil || node2 == nil || node1.Val != node2.Val {
             return false
         }
 
-        if cur1.Left != nil && cur2.Left == nil {
-            return false
-        } else if cur1.Left == nil && cur2.Left != nil {
-            return false
-        } else if cur1.Left != nil && cur2.Left != nil {
-            stack1 = append(stack1, cur1.Left)
-            stack2 = append(stack2, cur2.Left)
-        }
-
-        if cur1.Right != nil && cur2.Right == nil {
-            return false
-        } else if cur1.Right == nil && cur2.Right != nil {
-            return false
-        } else if cur1.Right != nil && cur2.Right != nil {
-            stack1 = append(stack1, cur1.Right)
-            stack2 = append(stack2, cur2.Right)
-        }
+        stack = append(stack, Pair{node1.Left, node2.Left})
+        stack = append(stack, Pair{node1.Right, node2.Right})
+       
     }
 
-    return len(stack1) == 0 && len(stack2) == 0
+    return len(stack) == 0
 }
