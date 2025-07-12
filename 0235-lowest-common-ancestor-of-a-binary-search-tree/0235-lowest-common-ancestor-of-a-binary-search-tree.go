b6c95d0138a6a2093ebbf4bb2dad2e6b26cc610b
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-12 22:28:04 +0530

    Time: 11 ms (89.96%), Space: 8.7 MB (94.81%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.go
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.go
@@ -8,13 +8,29 @@
  */
 
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-	if p.Val < root.Val && q.Val < root.Val {
-        return lowestCommonAncestor(root.Left, p, q)
+	// if p.Val < root.Val && q.Val < root.Val {
+    //     return lowestCommonAncestor(root.Left, p, q)
+    // }
+
+    // if p.Val > root.Val && q.Val > root.Val {
+    //     return lowestCommonAncestor(root.Right, p, q)
+    // }
+
+    // return root
+
+    // Iterative Soluion
+
+    cur := root
+
+    for cur != nil {
+        if p.Val < cur.Val && q.Val < cur.Val {
+            cur = cur.Left
+        } else if p.Val > cur.Val && q.Val > cur.Val {
+            cur = cur.Right
+        } else {
+            return cur
+        }
     }
 
-    if p.Val > root.Val && q.Val > root.Val {
-        return lowestCommonAncestor(root.Right, p, q)
-    }
-
-    return root
+    return nil
 }
