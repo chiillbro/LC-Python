9ffe04acb8085ac6c2d139fd3bb4c4723505426e
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-12 22:14:07 +0530

    Time: 39 ms (82.33%), Space: 18.1 MB (38.07%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -6,20 +6,15 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if not subRoot:
-            return True
-        if not root:
-            return False
-        
-        if self.isIdentical(root, subRoot):
-            return True
-        
-        return self.isIdentical(root.left, subRoot) or self.isIdentical(root.right, subRoot)
-    
-    def isIdentical(self, node1, node2):
-        if not node1 and not node2:
-            return True
-        if not node1 or not node2 or node1.val != node2.val:
-            return False
-        
-        return self.isIdentical(node1.left, node2.left) and self.isIdentical(node1.right, node2.right)
+        if not subRoot: return True
+        if not root: return False
+
+        if self.isIdentical(root, subRoot): return True
+
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+    def isIdentical(self, tree1, tree2):
+        if not tree1 and not tree2: return True
+        if not tree1 or not tree2: return False
+        if tree1.val != tree2.val: return False
+
+        return self.isIdentical(tree1.left, tree2.left) and self.isIdentical(tree1.right, tree2.right)
