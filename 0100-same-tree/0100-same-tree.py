fcae8f52e9df8767ea9106898b8df570823860ec
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-12 22:04:42 +0530

    Time: 0 ms (100%), Space: 18 MB (20.7%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -6,29 +6,12 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # Recursion
 
-        # ** Recursive Approach ** #
-        # if not p and not q:
-        #     return True
-        # elif not p or not q:
-        #     return False
-        # elif p.val != q.val:
-        #     return False
+        if not p and not q:
+            return True
         
-        # return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-
-        # ** Iterative Approach ** #
-        stack = [(p, q)]
-
-        while stack:
-            node1, node2 = stack.pop()
-
-            if not node1 and not node2: continue
-            
-            if not node1 or not node2 or node1.val != node2.val:
-                return False
-            
-            stack.append((node1.left, node2.left))
-            stack.append((node1.right, node2.right))
+        if not p or not q or p.val != q.val:
+            return False
         
-        return True
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
