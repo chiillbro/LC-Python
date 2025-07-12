96a2072adb69011015658f00008b477018277448
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-12 22:03:37 +0530

    Time: 0 ms (100%), Space: 4.1 MB (65.16%) - LeetHub

--- 0100-same-tree/0100-same-tree.go
+++ 0100-same-tree/0100-same-tree.go
@@ -9,19 +9,19 @@
 func isSameTree(p *TreeNode, q *TreeNode) bool {
 
     // Recursion
-    if p == nil && q == nil {
-        return true
-    }
+    // if p == nil && q == nil {
+    //     return true
+    // }
 
-    if p == nil || q == nil {
-        return false
-    }
+    // if p == nil || q == nil {
+    //     return false
+    // }
 
-    if p.Val != q.Val {
-        return false
-    }
+    // if p.Val != q.Val {
+    //     return false
+    // }
 
-    return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+    // return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
 
     type Pair struct {
         node1, node2 *TreeNode
