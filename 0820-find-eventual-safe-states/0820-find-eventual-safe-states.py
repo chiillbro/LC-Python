a2fcf5744805d77e11ee1ad15220075f095d24e8
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-13 13:25:48 +0530

    Time: 47 ms (56.78%), Space: 23.9 MB (39.09%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.py
@@ -1,62 +1,65 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+
+        # Kahn's BFS Algorithm
         v = len(graph)
 
-        adj_list = defaultdict(list)
+        # adj_list = defaultdict(list)
 
-        in_degree = [0] * v
+        # in_degree = [0] * v
 
-        for node in range(v):
-            in_degree[node] = len(graph[node])
-            for neigh in graph[node]:
-                adj_list[neigh].append(node)
+        # for node in range(v):
+        #     in_degree[node] = len(graph[node])
+        #     for neigh in graph[node]:
+        #         adj_list[neigh].append(node)
         
-        queue = deque()
+        # queue = deque()
 
-        for node, cnt in enumerate(in_degree):
-            if cnt == 0:
-                queue.append(node)
+        # for node, cnt in enumerate(in_degree):
+        #     if cnt == 0:
+        #         queue.append(node)
             
 
-        topo = []
+        # topo = []
 
-        while queue:
-            cur = queue.popleft()
-            topo.append(cur)
-            for neigh in adj_list[cur]:
-                in_degree[neigh] -= 1
+        # while queue:
+        #     cur = queue.popleft()
+        #     topo.append(cur)
+        #     for neigh in adj_list[cur]:
+        #         in_degree[neigh] -= 1
 
-                if in_degree[neigh] == 0:
-                    queue.append(neigh)
+        #         if in_degree[neigh] == 0:
+        #             queue.append(neigh)
 
-        topo.sort()
+        # topo.sort()
+        # return topo
 
-        return topo
 
-        # visited, on_stack = [0] * v, [0] * v
+        # DFS Approach
+        visited, on_stack = [0] * v, [0] * v
 
-        # safe_states = [0] * v
+        safe_states = [0] * v
 
-        # def dfs(cur):
-        #     visited[cur] = 1
+        def dfs(cur):
+            visited[cur] = 1
 
-        #     on_stack[cur] = 1
-        #     safe_state = True
-        #     for neigh in graph[cur]:
-        #         if not visited[neigh]:
-        #             if dfs(neigh):
-        #                 return True
+            on_stack[cur] = 1
+            safe_state = True
+            for neigh in graph[cur]:
+                if not visited[neigh]:
+                    if dfs(neigh):
+                        return True
 
-        #         elif on_stack[neigh]:
-        #             return True
+                elif on_stack[neigh]:
+                    return True
             
-        #     on_stack[cur] = 0
-        #     safe_states[cur] = 1
-        #     return False
+            on_stack[cur] = 0
+            safe_states[cur] = 1
+            return False
         
-        # for i in range(v):
-        #     if not visited[i]:
-        #         dfs(i)
+        for i in range(v):
+            if not visited[i]:
+                dfs(i)
         
 
-        # return [i for i, v in enumerate(safe_states) if v]
+        return [i for i, v in enumerate(safe_states) if v]
