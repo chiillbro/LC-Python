c06240e6f2fb8a40974a4ff94081b4513b7d0f76
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-13 07:51:43 +0530

    Time: 235 ms (72.07%), Space: 37.8 MB (98.89%) - LeetHub

--- 3741-reschedule-meetings-for-maximum-free-time-ii/3741-reschedule-meetings-for-maximum-free-time-ii.py
+++ 3741-reschedule-meetings-for-maximum-free-time-ii/3741-reschedule-meetings-for-maximum-free-time-ii.py
@@ -20,61 +20,84 @@ class Solution:
         #     gaps_right[i] = j - end
         #     j = startTime[i]
         
-        gaps = []
+        # gaps = []
 
-        gaps.append(startTime[0] - 0)
+        # gaps.append(startTime[0] - 0)
 
-        for i in range(n-1):
-            gaps.append(startTime[i+1] - endTime[i])
+        # for i in range(n-1):
+        #     gaps.append(startTime[i+1] - endTime[i])
 
-        gaps.append(eventTime - endTime[n-1])
+        # gaps.append(eventTime - endTime[n-1])
 
-        print("gaps", gaps)
+        # k = len(gaps)
 
-        k = len(gaps)
+        # gaps_left = [gaps[0]]
 
-        gaps_left = [gaps[0]]
-
-        for i in range(1, k):
-            gaps_left.append(max(gaps_left[-1], gaps[i]))
+        # for i in range(1, k):
+        #     gaps_left.append(max(gaps_left[-1], gaps[i]))
         
-        gaps_right = [0] * (k)
+        # gaps_right = [0] * (k)
 
-        gaps_right[-1] = gaps[-1]
+        # gaps_right[-1] = gaps[-1]
 
-        for i in range(k-2, -1, -1):
-            gaps_right[i] = max(gaps_right[i+1], gaps[i])
+        # for i in range(k-2, -1, -1):
+        #     gaps_right[i] = max(gaps_right[i+1], gaps[i])
 
         
-        max_gap = max(gaps)
+        # max_gap = max(gaps)
 
-        print("gaps_left", gaps_left)
+        # for i in range(n):
+        #     left = 0
+        #     if i - 1 >= 0:
+        #         left = gaps_left[i-1]
+            
+        #     right = 0
 
-        print("gaps_right", gaps_right)
+        #     if i + 2 < k:
+        #         right = gaps_right[i+2]
+            
+        #     cur_gap = 0
+        #     cur_dur = endTime[i] - startTime[i]
+        #     if left >= cur_dur or right >= cur_dur:
+        #         cur_gap = gaps[i] + gaps[i+1] + cur_dur
+            
+        #     else:
+        #         cur_gap = gaps[i] + gaps[i+1]
+            
+        #     max_gap = max(max_gap, cur_gap)
+        
+        # return max_gap
+
+
+        # In the above solution, I have used three lists to know whether a particular event can be totally shifted or not, but realized that I can maintain a single list
+
+        can_shift = [0] * n
+
+        left_max = right_max = 0
 
         for i in range(n):
-            left = 0
-            if i - 1 >= 0:
-                left = gaps_left[i-1]
+            if endTime[i] - startTime[i] <= left_max:
+                can_shift[i] = 1
             
-            right = 0
+            left_max = max(left_max, startTime[i] - (0 if i == 0 else endTime[i-1]))
 
-            if i + 2 < k:
-                right = gaps_right[i+2]
+            if endTime[n-i-1] - startTime[n-i-1] <= right_max:
+                can_shift[n-i-1] = 1
             
-            cur_gap = 0
-            cur_dur = endTime[i] - startTime[i]
-            if left >= cur_dur or right >= cur_dur:
-                cur_gap = gaps[i] + gaps[i+1] + cur_dur
-            
-            else:
-                cur_gap = gaps[i] + gaps[i+1]
-            
-            max_gap = max(max_gap, cur_gap)
+            right_max = max(right_max, (eventTime if i == 0 else startTime[n-i]) - endTime[n-i-1])
+
         
-        return max_gap
+        res = 0
 
+        for i in range(n):
+            left = 0 if i == 0 else endTime[i-1]
+            right = eventTime if i == n -1 else startTime[i+1]
 
-        return 0
+            if can_shift[i]:
+                res = max(res, right - left)
 
-            
+            else:
+                res = max(res, right - left - (endTime[i] - startTime[i]))
+
+        
+        return res
