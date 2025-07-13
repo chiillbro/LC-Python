fe117489971aa21bbc0d799ed4c5759a1cf40e8c
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-13 11:28:37 +0530

    Time: 1 ms (55.89%), Space: 18.3 MB (87.01%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.py
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.py
@@ -1,52 +1,23 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        """
-        1. Try picking up any two colors and color up all the nodes of the graph with alteranting colors for each edge, by the end if no two adjacent nodes are colored same (i.e., evey edge has perfect alterante colors), then that graph is said to be a Bipartite Graph..
-
-        2. One more property of Bipartite graph is, if there is a cycle exists in the graph and the path length of that cycle is odd length, then that graph can never be a Bipartite.
-
-        That's what the following bfs does.
-        """
-        V = len(graph)
-        color = [-1] * V
-
-        # for i in range(V):
-        #     if color[i] != -1:
-        #         continue
-        #     queue = deque([i])
-        #     color[i] = 0
-
-        #     while queue:
-        #         cur = queue.popleft()
-        #         for neigh in graph[cur]:
-        #             if color[neigh] == -1:
-        #                 color[neigh] = 1 - color[cur]
-        #                 queue.append(neigh)
-        #             elif color[neigh] == color[cur]:
-        #                 return False
         
-        # return True
-
-
-        # ** DFS Appraoch
-
-        def dfs(node, col):
-            color[node] = col
-            for neigh in graph[node]:
-                if color[neigh] == -1:
-                    if not dfs(neigh, 1 - col):
-                        return False
-                elif color[neigh] == col:
-                    return False
-            return True
-        
-        for i in range(V):
-            if color[i] != -1:
+        n = len(graph)
+        visited = [0] * n
+        # colors I pick - 1 & 2
+        for i in range(n):
+            if visited[i]:
                 continue
-            if not dfs(i, 0):
-                return False
-        
-        return True
+            queue = deque([i])
+            visited[i] = 1
 
-        
-        
+            while queue:
+                cur = queue.popleft()
+
+                for neigh in graph[cur]:
+                    if not visited[neigh]:
+                        visited[neigh] = 1 if visited[cur] == 2 else 2
+                        queue.append(neigh)
+                    elif visited[cur] == visited[neigh]:
+                        return False
+
+        return True
