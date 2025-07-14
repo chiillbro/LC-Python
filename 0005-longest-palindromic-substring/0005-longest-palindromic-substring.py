46713296b6ff3fb7ff7f4411e3ed66878454edf3
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-14 14:24:23 +0530

    Time: 203 ms (94.77%), Space: 17.7 MB (96.22%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -4,53 +4,53 @@ class Solution:
 
         # Two Pointers, expanding around center, TC: O(N^2), SC: O(1)
 
-        # def expandAroundCenter(i, j):
-        #     while i >= 0 and j < n and s[i] == s[j]:
-        #         i -= 1
-        #         j += 1
+        def expandAroundCenter(i, j):
+            while i >= 0 and j < n and s[i] == s[j]:
+                i -= 1
+                j += 1
             
-        #     return s[i+1:j]
+            return s[i+1:j]
         
-        # longest = ""
-        # for idx in range(n):
-        #     p1 = expandAroundCenter(idx, idx)
-        #     p2 = expandAroundCenter(idx, idx + 1)
+        longest = ""
+        for idx in range(n):
+            p1 = expandAroundCenter(idx, idx)
+            p2 = expandAroundCenter(idx, idx + 1)
 
-        #     if len(p1) > len(longest):
-        #         longest = p1
+            if len(p1) > len(longest):
+                longest = p1
             
-        #     if len(p2) > len(longest):
-        #         longest = p2
+            if len(p2) > len(longest):
+                longest = p2
         
-        # return longest
+        return longest
 
 
         # DP approach, TC: O(N^2), SC: O(N^2)
 
-        if n < 2:
-            return s
+        # if n < 2:
+        #     return s
 
-        # dp[i][j] represents whether s[i:j] incusively is a palindrome or not
-        dp = [[False] * n for _ in range(n)]
+        # # dp[i][j] represents whether s[i:j] incusively is a palindrome or not
+        # dp = [[False] * n for _ in range(n)]
 
-        for i in range(n):
-            dp[i][i] = True
+        # for i in range(n):
+        #     dp[i][i] = True
 
-        start, max_len = 0, 1
+        # start, max_len = 0, 1
 
-        for L in range(2, n+1):
-            for i in  range(n-L+1):
-                j = i + L - 1
+        # for L in range(2, n+1):
+        #     for i in  range(n-L+1):
+        #         j = i + L - 1
 
-                if s[i] == s[j]:
-                    if L == 2 or dp[i+1][j-1] == True:
-                        dp[i][j] = True
+        #         if s[i] == s[j]:
+        #             if L == 2 or dp[i+1][j-1] == True:
+        #                 dp[i][j] = True
 
-                        if L > max_len:
-                            start = i
-                            max_len = L
+        #                 if L > max_len:
+        #                     start = i
+        #                     max_len = L
         
-        return s[start:start+max_len]
+        # return s[start:start+max_len]
         
 
 # There is even more optimized approach to solve this problem which is Manacher's algorithm which has a TC of O(N) and SC of O(N)
