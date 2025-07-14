5d098f60bf5d96489bb03f109a7f51915a3b8703
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-14 15:28:53 +0530

    Time: 1092 ms (13.36%), Space: 413.4 MB (5%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,5 +1,22 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
+        
+        @cache
+        def helper(idx, cur_amount):
+            if cur_amount == 0:
+                return 1
+            
+            if idx == len(coins) or cur_amount < 0:
+                return 0
+            
+            use_it = helper(idx, cur_amount - coins[idx])
+            skip = helper(idx + 1, cur_amount)
+
+            return use_it + skip
+
+        return helper(0, amount)
+        
+        # Bottom Up DP
         dp = [0] * (amount + 1)
 
         dp[0] = 1
