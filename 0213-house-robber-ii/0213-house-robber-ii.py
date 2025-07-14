027c471dc4ac99df05100d23c35d32663cfcdfa3
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-14 13:51:44 +0530

    Time: 0 ms (100%), Space: 17.9 MB (17.09%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -7,19 +7,14 @@ class Solution:
         
         return max(self.helper(nums[1:]), self.helper(nums[:-1]))
 
-    def helper(self, nums: List[int]) -> int:
+    def helper(self, nums):
         n = len(nums)
 
-        # dp = [0] * (n+1)
+        dp = [0] * (n + 1)
 
-        # dp[1] = nums[0]
+        dp[1] = nums[0]
 
-        # Space Optimize
-        prev2, prev1 = 0, nums[0]
+        for i in range(2, n+1):
+            dp[i] = max(nums[i-1] + dp[i-2], dp[i-1])
 
-        for i in range(2, n + 1):
-            # dp[i] = max(dp[i-1], dp[i-2] + nums[i-1])
-            cur = max(prev1, prev2 + nums[i-1])
-            prev2, prev1 = prev1, cur
-        
-        return prev1
+        return dp[n]
