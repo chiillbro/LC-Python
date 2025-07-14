d3dbd1072b7bb49dcd94b199e541f3d925cdba0a
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-14 14:50:49 +0530

    Time: 0 ms (100%), Space: 17.7 MB (89.25%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -4,16 +4,22 @@ class Solution:
             return 0
         
         n = len(s)
-
+        
+        # dp[i] represents no.of ways to decode the prefix string s[0:i-1] inclusively
         dp = [0] * (n + 1)
 
+        # empty string can be decoded in 1 way which is to do nothing
         dp[0] = 1
+        
+        # 1 length valid string if it is not "0", then there is one way to decode it "1" to "9"        
         dp[1] = 1
 
         for i in range(2, n+1):
+            # if the current char is not "0", then we can extend all the ways of dp[i-1] to a new way
             if s[i-1] != "0":
                 dp[i] += dp[i-1]
             
+            # if the cur and last chars number is a valid two digit chunk, then it again can extend the ways of dp[i-2] to a new way
             if 10 <= int(s[i-2:i]) <= 26:
                 dp[i] += dp[i-2]
         
