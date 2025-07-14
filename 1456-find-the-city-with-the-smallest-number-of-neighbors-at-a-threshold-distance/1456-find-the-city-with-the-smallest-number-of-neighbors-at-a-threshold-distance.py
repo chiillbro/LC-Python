bd6be1f64c4792b035159899c612a729f0b9094c
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-14 07:01:40 +0530

    Time: 182 ms (76.01%), Space: 19.1 MB (50.18%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -1,31 +1,81 @@
 class Solution:
     def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
-        dist = [[math.inf] * n for _ in range(n)]
+
+        # Floyd's Warshall Algorithm, TC: (O(V^3)), SC: O(V^2)
+
+        # dist = [[math.inf] * n for _ in range(n)]
+
+        # for u, v, w in edges:
+        #     dist[u][v] = w
+        #     dist[v][u] = w
+        
+
+        # for i in range(n):
+        #     dist[i][i] = 0
+        
+
+        # for k in range(n):
+        #     for i in range(n):
+        #         for j in range(n):
+        #             dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+
+        # cityNumber = 0
+        # cityCount = n
+        # for i in range(n):
+        #     cnt = 0
+        #     for j in range(n):
+        #         if dist[i][j] <= distanceThreshold:
+        #             cnt += 1
+            
+        #     if cnt <= cityCount:
+        #         cityCount = cnt
+        #         cityNumber = i
+        
+        # return cityNumber
+
+
+        # Using Dijkstra's Algorithm
+
+        cnts = [0] * n
+
+        adj_list = defaultdict(list)
 
         for u, v, w in edges:
-            dist[u][v] = w
-            dist[v][u] = w
+            adj_list[u].append((v, w))
+            adj_list[v].append((u, w))
         
+        for cur in range(n):
+            dist = [math.inf] * n
 
-        for i in range(n):
-            dist[i][i] = 0
-        
+            dist[cur] = 0
 
-        for k in range(n):
-            for i in range(n):
-                for j in range(n):
-                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+            pq = [(0, cur)]
 
-        cityNumber = 0
-        cityCount = n
-        for i in range(n):
-            cnt = 0
-            for j in range(n):
-                if dist[i][j] <= distanceThreshold:
-                    cnt += 1
+            while pq:
+                cur_dist, cur_node = heappop(pq)
+
+                if cur_dist >= distanceThreshold:
+                    continue
+                
+                for neigh, neigh_wei in adj_list[cur_node]:
+                    new_dist = cur_dist + neigh_wei
+
+                    if new_dist < dist[neigh]:
+                        dist[neigh] = new_dist
+                        heappush(pq, (new_dist, neigh))
             
-            if cnt <= cityCount:
-                cityCount = cnt
-                cityNumber = i
+            cnt = sum(1 for d in dist if d <= distanceThreshold)
+            cnts[cur] = cnt
         
-        return cityNumber
+
+        min_cities = n
+        city_number = 0
+
+        for city, cnt in enumerate(cnts):
+            if cnt <= min_cities:
+                min_cities = cnt
+                city_number = city
+        
+        return city_number
+
+
