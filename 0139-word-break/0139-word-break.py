b02ca36efc5113b32d8a2182d98d671a4e7e2bef
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-14 15:46:35 +0530

    Time: 7 ms (24.37%), Space: 18 MB (24.53%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -3,17 +3,30 @@ class Solution:
 
         wordSet = set(wordDict)
         # Recursion
-        @cache
-        def backtrack(cur: str, idx: int):
+        # @cache
+        # def backtrack(cur: str, idx: int):
             
-            if idx >= len(s):
-                return True if cur in wordSet else False
+        #     if idx >= len(s):
+        #         return True if cur in wordSet else False
             
-            if cur in wordSet:
-                if backtrack(s[idx], idx + 1):
-                    return True
+        #     if cur in wordSet:
+        #         if backtrack(s[idx], idx + 1):
+        #             return True
             
-            return backtrack(cur + s[idx], idx + 1)
+        #     return backtrack(cur + s[idx], idx + 1)
         
 
-        return backtrack("", 0)
+        # return backtrack("", 0)
+
+        # Bottom Up DP
+
+        dp = [False] * (len(s) + 1)
+
+        dp[0] = True
+
+        for i in range(1, len(s) + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordSet:
+                    dp[i] = True
+        
+        return dp[len(s)]
