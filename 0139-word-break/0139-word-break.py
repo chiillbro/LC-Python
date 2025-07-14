2eb5b6619a596a7cc9058fc2ce1e8d00d8279e8e
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-14 15:44:15 +0530

    Time: 19 ms (5.21%), Space: 29.3 MB (5.49%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,33 +1,19 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        word_set = set(wordDict)
 
-        # ** Top Down Approach : Recursion ** #
-        memo = defaultdict(bool)
-        def backtrack(cur, index):
-            key = cur + "_" + str(index)
-            if key in memo:
-                return memo[key]
-            if index == len(s):
-                return True if cur in word_set else False
-
-            if cur in word_set:
-                if backtrack(s[index], index + 1):
-                    memo[key] = True
+        wordSet = set(wordDict)
+        # Recursion
+        @cache
+        def backtrack(cur: str, idx: int):
+            
+            if idx >= len(s):
+                return True if cur in wordSet else False
+            
+            if cur in wordSet:
+                if backtrack(s[idx], idx + 1):
                     return True
             
-            memo[key] = backtrack(cur + s[index], index + 1)
-            return memo[key]
+            return backtrack(cur + s[idx], idx + 1)
         
 
         return backtrack("", 0)
-
-        #. ** Bottom Up Approach : DP ** #
-        # dp = [False] * (len(s) + 1)
-        # dp[0] = True
-
-        # for i in range(1, len(s) + 1):
-        #     for j in range(i):
-        #         if dp[j] and s[j:i] in word_set:
-        #             dp[i] = True
-        # return dp[len(s)]
