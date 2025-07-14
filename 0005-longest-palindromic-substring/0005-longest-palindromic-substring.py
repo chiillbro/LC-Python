f3af7719fb878d5a02d427c8c2b783a31af32a83
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-14 14:23:21 +0530

    Time: 1920 ms (27.7%), Space: 25.4 MB (12.36%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -2,22 +2,53 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
 
-        def expandAroundCenter(i, j):
-            while i >= 0 and j < n and s[i] == s[j]:
-                i -= 1
-                j += 1
-            
-            return s[i+1:j]
-        
-        longest = ""
-        for idx in range(n):
-            p1 = expandAroundCenter(idx, idx)
-            p2 = expandAroundCenter(idx, idx + 1)
+        # Two Pointers, expanding around center, TC: O(N^2), SC: O(1)
 
-            if len(p1) > len(longest):
-                longest = p1
+        # def expandAroundCenter(i, j):
+        #     while i >= 0 and j < n and s[i] == s[j]:
+        #         i -= 1
+        #         j += 1
             
-            if len(p2) > len(longest):
-                longest = p2
+        #     return s[i+1:j]
         
-        return longest
+        # longest = ""
+        # for idx in range(n):
+        #     p1 = expandAroundCenter(idx, idx)
+        #     p2 = expandAroundCenter(idx, idx + 1)
+
+        #     if len(p1) > len(longest):
+        #         longest = p1
+            
+        #     if len(p2) > len(longest):
+        #         longest = p2
+        
+        # return longest
+
+
+        # DP approach, TC: O(N^2), SC: O(N^2)
+
+        if n < 2:
+            return s
+
+        # dp[i][j] represents whether s[i:j] incusively is a palindrome or not
+        dp = [[False] * n for _ in range(n)]
+
+        for i in range(n):
+            dp[i][i] = True
+
+        start, max_len = 0, 1
+
+        for L in range(2, n+1):
+            for i in  range(n-L+1):
+                j = i + L - 1
+
+                if s[i] == s[j]:
+                    if L == 2 or dp[i+1][j-1] == True:
+                        dp[i][j] = True
+
+                        if L > max_len:
+                            start = i
+                            max_len = L
+        
+        return s[start:start+max_len]
+        
