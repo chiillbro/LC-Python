f79544aca4e03e52ee21dafc8f1a20ba199ef2a6
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-14 14:32:24 +0530

    Time: 105 ms (91.29%), Space: 17.9 MB (34.17%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -2,38 +2,38 @@ class Solution:
     def countSubstrings(self, s: str) -> int:
         n = len(s)
 
-        # count = 0
-        # def expandAroundCenter(i, j):
-        #     nonlocal count
-        #     while i >= 0 and j < n and s[i] == s[j]:
-        #         count += 1
-        #         i -= 1
-        #         j += 1
+        count = 0
+        def expandAroundCenter(i, j):
+            nonlocal count
+            while i >= 0 and j < n and s[i] == s[j]:
+                count += 1
+                i -= 1
+                j += 1
 
 
-        # for center in range(n):
-        #     expandAroundCenter(center, center)
-        #     expandAroundCenter(center, center + 1)
+        for center in range(n):
+            expandAroundCenter(center, center)
+            expandAroundCenter(center, center + 1)
         
-        # return count
+        return count
 
 
         # DP Approach
 
-        dp = [[False] * n for _ in range(n)]
+        # dp = [[False] * n for _ in range(n)]
 
-        count = 0
-        for i in range(n):
-            dp[i][i] = True
-            count += 1
+        # count = 0
+        # for i in range(n):
+        #     dp[i][i] = True
+        #     count += 1
         
-        for L in range(2, n+1):
-            for i in range(n - L + 1):
-                j = i + L - 1
-                if s[i] == s[j]:
+        # for L in range(2, n+1):
+        #     for i in range(n - L + 1):
+        #         j = i + L - 1
+        #         if s[i] == s[j]:
                     
-                    if L == 2 or dp[i+1][j-1]:
-                        dp[i][j] = True
-                        count += 1
+        #             if L == 2 or dp[i+1][j-1]:
+        #                 dp[i][j] = True
+        #                 count += 1
         
-        return count
+        # return count
