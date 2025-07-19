2bc0e887d449f236b58c9fb77fa5789c5e59490d
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-19 13:59:04 +0530

    Time: 15 ms (83.11%), Space: 25.2 MB (35.58%) - LeetHub

--- 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/3682-count-the-number-of-arrays-with-k-matching-adjacent-elements.py
+++ 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/3682-count-the-number-of-arrays-with-k-matching-adjacent-elements.py
@@ -1,16 +1,8 @@
 MOD = 10**9 + 7
-maxN = 100000
+MX = 10**5
 
-fact = [1] * (maxN + 1)
-inv_fact = [1] * (maxN + 1)
-
-for i in range(1, maxN + 1):
-    fact[i] = fact[i - 1] * i % MOD
-
-inv_fact[maxN] = pow(fact[maxN], MOD - 2, MOD)
-for i in range(maxN, 0, -1):
-    inv_fact[i - 1] = inv_fact[i] * i % MOD
-
+fact = [0] * MX
+inv_fact = [0] * MX
 
 
 def qpow(x, n):
@@ -40,16 +32,34 @@ def comb(n, m):
 
 class Solution:
     def countGoodArrays(self, n: int, m: int, k: int) -> int:
-        d = n - k
-        if d < 1:
-            return 0
-        term1 = m * pow(m - 1, d - 1, MOD) % MOD
-        num = n - 1
-        den1 = d - 1
-        den2 = k
-        if den1 < 0 or den2 < 0:
-            binom_val = 0
-        else:
-            binom_val = fact[num] * inv_fact[den1] % MOD
-            binom_val = binom_val * inv_fact[den2] % MOD
-        return term1 * binom_val % MOD
+        init()
+        return comb(n - 1, k) * m % MOD * qpow(m - 1, n - k - 1) % MOD
+
+
+# MOD = 10**9 + 7
+# maxN = 100000
+
+# fact = [1] * (maxN + 1)
+# inv_fact = [1] * (maxN + 1)
+
+# for i in range(1, maxN + 1):
+#     fact[i] = fact[i - 1] * i % MOD
+
+# inv_fact[maxN] = pow(fact[maxN], MOD - 2, MOD)
+# for i in range(maxN, 0, -1):
+#     inv_fact[i - 1] = inv_fact[i] * i % MOD
+
+# def countGoodArrays(n: int, m: int, k: int) -> int:
+#     d = n - k
+#     if d < 1:
+#         return 0
+#     term1 = m * pow(m - 1, d - 1, MOD) % MOD
+#     num = n - 1
+#     den1 = d - 1
+#     den2 = k
+#     if den1 < 0 or den2 < 0:
+#         binom_val = 0
+#     else:
+#         binom_val = fact[num] * inv_fact[den1] % MOD
+#         binom_val = binom_val * inv_fact[den2] % MOD
+#     return term1 * binom_val % MOD
