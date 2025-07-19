e5db7b136f481fb9082eb8e5fbd1988457b17105
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-19 14:17:06 +0530

    Time: 7126 ms (15.79%), Space: 50.3 MB (76.32%) - LeetHub

--- 3423-maximum-sum-of-subsequence-with-non-adjacent-elements/3423-maximum-sum-of-subsequence-with-non-adjacent-elements.py
+++ 3423-maximum-sum-of-subsequence-with-non-adjacent-elements/3423-maximum-sum-of-subsequence-with-non-adjacent-elements.py
@@ -1,50 +1,32 @@
 class Solution:
     def maximumSumSubsequence(self, nums: List[int], queries: List[List[int]]) -> int:
         n = len(nums)
-        tree = [[0] * 4 for _ in range(4 * n)]
-        
-        def build(tidx, lo, hi):
-            if lo == hi:
-                tree[tidx][0] = max(0, nums[lo])
-            else:
-                mid = (lo + hi) // 2
-                build(2 * tidx + 1, lo, mid)
-                build(2 * tidx + 2, mid + 1, hi)
-                l0h0A, l1h0A, l0h1A, l1h1A = tree[2 * tidx + 1]
-                l0h0B, l1h0B, l0h1B, l1h1B = tree[2 * tidx + 2]
+        m = 1 << (n.bit_length())
+        nums += [0] * (m - n)
+        n = m
+        tree = [[0] * 4 for _ in range(2 * n + 1)]
+
+        def update(i, x):
+            j = i + n
+            tree[j][0] = max(0, x)
+            while j:
+                j >>= 1
+                l0h0A, l1h0A, l0h1A, l1h1A = tree[2 * j]
+                l0h0B, l1h0B, l0h1B, l1h1B = tree[2 * j + 1]
                 # lo to hi
-                tree[tidx][0] = max(l0h1A + l1h0B, l0h0A + l1h0B, l0h1A + l0h0B)
+                tree[j][0] = max(l0h1A + l1h0B, l0h0A + l1h0B, l0h1A + l0h0B)
                 # lo + 1 to hi
-                tree[tidx][1] = max(l1h1A + l1h0B, l1h0A + l1h0B, l1h1A + l0h0B)
+                tree[j][1] = max(l1h1A + l1h0B, l1h0A + l1h0B, l1h1A + l0h0B)
                 # lo to hi - 1
-                tree[tidx][2] = max(l0h1A + l1h1B, l0h0A + l1h1B, l0h1A + l0h1B)
+                tree[j][2] = max(l0h1A + l1h1B, l0h0A + l1h1B, l0h1A + l0h1B)
                 # lo + 1 to hi - 1
-                tree[tidx][3] = max(l1h1A + l1h1B, l1h0A + l1h1B, l1h1A + l0h1B)
-        
-        build(0, 0, n - 1)
-        
-        def update(tidx, lo, hi, i, val):
-            if lo == hi:
-                tree[tidx][0] = max(0, val)
-            else:
-                mid = (lo + hi) // 2
-                if i > mid:
-                    update(tidx * 2 + 2, mid + 1, hi, i, val)
-                else:
-                    update(tidx * 2 + 1, lo, mid, i, val)
-                l0h0A, l1h0A, l0h1A, l1h1A = tree[2 * tidx + 1]
-                l0h0B, l1h0B, l0h1B, l1h1B = tree[2 * tidx + 2]
-                # lo to hi
-                tree[tidx][0] = max(l0h1A + l1h0B, l0h0A + l1h0B, l0h1A + l0h0B)
-                # lo + 1 to hi
-                tree[tidx][1] = max(l1h1A + l1h0B, l1h0A + l1h0B, l1h1A + l0h0B)
-                # lo to hi - 1
-                tree[tidx][2] = max(l0h1A + l1h1B, l0h0A + l1h1B, l0h1A + l0h1B)
-                # lo + 1 to hi - 1
-                tree[tidx][3] = max(l1h1A + l1h1B, l1h0A + l1h1B, l1h1A + l0h1B)
-        
+                tree[j][3] = max(l1h1A + l1h1B, l1h0A + l1h1B, l1h1A + l0h1B)
+
+        for i, x in enumerate(nums):
+            update(i, x)
+
         ans = 0
         for i, x in queries:
-            update(0, 0, n - 1, i, x)
-            ans += max(tree[0])
+            update(i, x)
+            ans += max(tree[1])
         return ans % 1_000_000_007
