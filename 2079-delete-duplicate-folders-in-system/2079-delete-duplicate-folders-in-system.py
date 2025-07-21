53a12704d33ffc59e0f09df1939de1f65c1e1b6a
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-21 22:31:43 +0530

    Time: 215 ms (25.56%), Space: 47.6 MB (80%) - LeetHub

--- 2079-delete-duplicate-folders-in-system/2079-delete-duplicate-folders-in-system.py
+++ 2079-delete-duplicate-folders-in-system/2079-delete-duplicate-folders-in-system.py
@@ -1,64 +1,64 @@
 class Trie:
-    # current node structure's serialized representation
-    serial: str = ""
-    # current node's child nodes
-    children: dict
-
     def __init__(self):
-        self.children = dict()
+        self.str_repr = ""
+        self.children = defaultdict(Trie)
+
 
 
 class Solution:
     def deleteDuplicateFolder(self, paths: List[List[str]]) -> List[List[str]]:
-        # root node
         root = Trie()
+
+        # build the structure
 
         for path in paths:
             cur = root
             for node in path:
                 if node not in cur.children:
                     cur.children[node] = Trie()
+                
                 cur = cur.children[node]
+        
+        # format string repres
 
-        # hash table records the occurrence times of each serialized representation
-        freq = Counter()
-
-        # post-order traversal based on depth-first search, calculate the serialized representation of each node structure
-        def construct(node: Trie) -> None:
-            # if it is a leaf node, then the serialization is represented as an empty string, and no operation is required.
+        freq = defaultdict(int)
+        def formatPaths(node: Trie):
             if not node.children:
                 return
-
-            v = list()
-            # if it is not a leaf node, the serialization representation of the child node structure needs to be calculated first.
+            
+            cur = []
             for folder, child in node.children.items():
-                construct(child)
-                v.append(folder + "(" + child.serial + ")")
+                formatPaths(child)
+                cur.append(folder + "(" + child.str_repr + ")")
+            
+            print("before sorting", cur)
+            cur.sort()
+            print("after sorting", cur)
 
-            # to prevent issues with order, sorting is needed
-            v.sort()
-            node.serial = "".join(v)
-            # add to hash table
-            freq[node.serial] += 1
+            node.str_repr = ''.join(cur)
+            freq[node.str_repr] += 1
+        
+        formatPaths(root)
 
-        construct(root)
-
-        ans = list()
-        # record the path from the root node to the current node.
-        path = list()
-
-        def operate(node: Trie) -> None:
-            # if the serialization appears more than once in the hash table, it needs to be deleted.
-            if freq[node.serial] > 1:
+        print(root.children, root.str_repr)
+        
+        ans, path = [], []
+        def resolve(node: Trie):
+            if freq[node.str_repr] > 1:
                 return
-            # otherwise add the path to the answer
+            
             if path:
                 ans.append(path[:])
-
+            
             for folder, child in node.children.items():
                 path.append(folder)
-                operate(child)
+                resolve(child)
                 path.pop()
-
-        operate(root)
+        
+        resolve(root)
+            
+        
         return ans
+
+
+
