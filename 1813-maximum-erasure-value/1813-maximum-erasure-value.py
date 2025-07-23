b595a89d71f155619e4e8487041b5b61385dadd2
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-23 23:06:05 +0530

    Time: 193 ms (79.91%), Space: 28.9 MB (50.45%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.py
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.py
@@ -4,25 +4,30 @@ class Solution:
 
         n = len(nums)
 
-        pref_sums = [0] * (n)
+        # pref_sums = [0] * (n)
 
-        pref_sums[0] = nums[0]
+        # pref_sums[0] = nums[0]
 
-        for i in range(1, n):
-            pref_sums[i] = pref_sums[i-1] + nums[i]
+        # for i in range(1, n):
+        #     pref_sums[i] = pref_sums[i-1] + nums[i]
+
+        # don't even need prefix sum array
 
         seen = set()
 
         left = 0
-
+        cur_sum = 0
         max_sum = 0
         for right in range(n):
             while nums[right] in seen:
+                cur_sum -= nums[left]
                 seen.remove(nums[left])
                 left += 1
+
             seen.add(nums[right])
 
-            cur_sum = pref_sums[right] - (pref_sums[left-1] if left > 0 else 0)
+            # cur_sum = pref_sums[right] - (pref_sums[left-1] if left > 0 else 0)
+            cur_sum += nums[right]
 
             max_sum = max(max_sum, cur_sum)
     
