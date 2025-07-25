7820a3f799bce3da794d9622f5f48f0b915ea67d
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-07-25 17:53:53 +0530

    Time: 19 ms (36.75%), Space: 22.7 MB (26.14%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -3,8 +3,9 @@ class Solution:
         group_anag = defaultdict(list)
 
         for s in strs:
-            # key = ''.join(sorted(s))
-            key = [0] * 26
+            # key = ''.join(sorted(s)) # O K logK
+
+            key = [0] * 26 # O(K), this is efficient if the word length is in 1000s or more and can get you a brownie point in interviews if mentioned
             for c in s:
                 key[ord(c) - 97] += 1
             group_anag[tuple(key)].append(s)
