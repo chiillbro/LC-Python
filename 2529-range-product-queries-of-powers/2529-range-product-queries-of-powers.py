560ef1ec9ef5542b6c633a9dace00f67a52a3074
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-08-11 21:32:47 +0530

    Time: 223 ms (18.64%), Space: 44.8 MB (95.65%) - LeetHub

--- 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.py
+++ 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.py
@@ -3,31 +3,38 @@ class Solution:
         MOD = 10**9 + 7
         powers = []
 
-        bin_repr = bin(n)
+        # bin_repr = bin(n)
 
-        j = 0
-        for i in range(len(bin_repr)-1, 1, -1):
-            if bin_repr[i] == "1":
-                powers.append(1 << j)
+        # j = 0
+        # for i in range(len(bin_repr)-1, 1, -1):
+        #     if bin_repr[i] == "1":
+        #         powers.append(1 << j)
             
-            j += 1
+        #     j += 1
+
+        for i in range(30):
+            if (n >> i) & 1:
+                powers.append(1 << i)
 
         prod_arr = [1] * (len(powers))
-        prod_arr[0] = powers[0]
+        prod_arr[0] = powers[0] % MOD
 
         for i in range(1, len(powers)):
-            prod_arr[i] = prod_arr[i-1] * powers[i]
+            prod_arr[i] = (prod_arr[i-1] * powers[i]) % MOD
         
         ans = []
 
         for l, r in queries:
-            cur = 1
+            product_up_to_r = prod_arr[r]
 
             if l == 0:
-                cur = prod_arr[r] % MOD
+                ans.append(product_up_to_r)
             else:
-                cur = prod_arr[r] // prod_arr[l-1] % MOD
-            
-            ans.append(cur)
+                product_up_to_l_minus_1 = prod_arr[l-1]
+                mod_inverse = pow(product_up_to_l_minus_1, MOD - 2, MOD)
+                # cur = prod_arr[r] // prod_arr[l-1]
+                cur = (product_up_to_r * mod_inverse) % MOD
+                
+                ans.append(cur)
         
         return ans
