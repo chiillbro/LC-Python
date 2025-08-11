4e517ae848fce26850e9df3a548e26dd7d8daafc
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-08-11 12:54:09 +0530

    Time: 3 ms (59.39%), Space: 17.8 MB (52.87%) - LeetHub

--- 0900-reordered-power-of-2/0900-reordered-power-of-2.py
+++ 0900-reordered-power-of-2/0900-reordered-power-of-2.py
@@ -1,12 +1,25 @@
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
-        def convert(n):
-            return ''.join(sorted(str(n)))
+        # def convert(n):
+        #     return ''.join(sorted(str(n)))
         
-        target = convert(n)
+        # target = convert(n)
+
+        # for i in range(31):
+        #     if convert(1 << i) == target:
+        #         return True
+        
+        # return False
+
+
+        # Counting Approach
+
+        counter1 = Counter(str(n))
 
         for i in range(31):
-            if convert(1 << i) == target:
+            counter2 = Counter(str(1 << i))
+
+            if counter1 == counter2:
                 return True
         
         return False
