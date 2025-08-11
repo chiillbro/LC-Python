4d2376757020ac6c79443c2a0cdcce5c4070db55
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-08-11 10:31:25 +0530

    Time: 0 ms (100%), Space: 17.7 MB (54.55%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -11,5 +11,10 @@ class Solution:
         # return n == 1
 
 
+        # Intuition: 2's power numbers are special, they have only one bit set ('1'), so we can simply count the '1''s and check whether they are equal to 1
+        # return bin(n).count('1') == 1
 
-        return bin(n).count('1') == 1
+
+        # Bit Manipulation Trick using (AND operator)
+
+        return n > 0 and (n & (n-1)) == 0
