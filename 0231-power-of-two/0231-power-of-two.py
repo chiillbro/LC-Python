d0e1092e34e2f4db13c36f09b7e5cafe8b5bac3b
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-08-11 10:52:15 +0530

    Time: 0 ms (100%), Space: 17.7 MB (54.55%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -2,8 +2,8 @@ class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
 
         # Direct, Initial thought Approach
-        if n <= 0:
-            return False
+        # if n <= 0:
+        #     return False
 
         # while not (n & 1):
         #     n //= 2
@@ -12,9 +12,22 @@ class Solution:
 
 
         # Intuition: 2's power numbers are special, they have only one bit set ('1'), so we can simply count the '1''s and check whether they are equal to 1
-        # return bin(n).count('1') == 1
+        # return n > 0 and bin(n).count('1') == 1 # or,
+        # return n > 0 and n.bit_count() == 1 # we can do something like,
 
 
         # Bit Manipulation Trick using (AND operator)
 
-        return n > 0 and (n & (n-1)) == 0
+        # return n > 0 and (n & (n-1)) == 0
+
+
+
+        # Logarithmic Approach
+
+        if n <= 0:
+            return False
+
+        val = math.log2(n)
+        print("val", val)
+
+        return 2**round(val) == n
