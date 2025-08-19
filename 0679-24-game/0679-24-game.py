69a32807e4831392945a18ab045c61c00d47666c
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-08-19 05:43:48 +0530

    Time: 159 ms (23.81%), Space: 17.9 MB (65.65%) - LeetHub

---
+++ 0679-24-game/0679-24-game.py
@@ -0,0 +1,35 @@
+class Solution:
+    def judgePoint24(self, cards: List[int]) -> bool:
+        # cards: int[...4], each element is in the range 1 to 9
+        THRESHOLD = 10**(-6)
+
+        def backtrack(cur):
+            if len(cur) == 1:
+                return abs(cur[0] - 24) < THRESHOLD
+            
+            n = len(cur)
+            for i in range(n):
+                for j in range(n):
+                    if i == j:
+                        continue
+
+                    rem = [cur[k] for k in range(n) if k != i and k != j]
+                    a, b = cur[i], cur[j]
+                    candidates = [a+b, a-b, b-a, a*b]
+                    
+                    if b > THRESHOLD:
+                        candidates.append(a/b)
+
+                    
+                    if a > THRESHOLD:
+                        candidates.append(b/a)
+
+                    for candidate in candidates:
+                        if backtrack(rem + [candidate]):
+                            return True
+
+            return False
+        
+
+
+        return backtrack(cards)
