b3b4fb4818e7803a880bf10883f44205b0a2c1ee
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-08-20 08:42:56 +0530

    Time: 44 ms (39.55%), Space: 28.6 MB (13.18%) - LeetHub

---
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.py
@@ -0,0 +1,29 @@
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        n = len(nums)
+        left = 0
+
+        res = 0
+
+        for right in range(n):
+            if nums[right] == 0:
+                continue 
+            
+            while nums[left] != 0 and left < right:
+                left += 1
+            
+            length = right - left
+            res += (length * ( length + 1 )) >> 1
+
+            if length > 0:
+                left = right
+        
+        if left == 0 and nums[0] == 0:
+            return (n * (n + 1)) >> 1
+        
+        # while nums[left] != 0 and left < n-1:
+        #     left += 1
+        length = (n-1) - left
+        res += (length * ( length + 1 )) >> 1
+        
+        return res
