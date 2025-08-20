eee3754e1feef75ed9625b2f66dbe730470aff31
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-08-20 08:54:45 +0530

    Time: 39 ms (51.62%), Space: 28.4 MB (83.27%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.py
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.py
@@ -1,26 +1,42 @@
 class Solution:
     def zeroFilledSubarray(self, nums: List[int]) -> int:
         n = len(nums)
-        left = 0
+        # left = 0
 
-        res = 0
+        # res = 0
 
-        for right in range(n):
-            if nums[right] == 0:
-                continue 
+        # for right in range(n):
+        #     if nums[right] == 0:
+        #         continue 
             
-            while nums[left] != 0 and left < right:
-                left += 1
+        #     while nums[left] != 0 and left < right:
+        #         left += 1
             
-            length = right - left
-            if length > 0:
-                res += (length * ( length + 1 )) >> 1
-                left = right
+        #     length = right - left
+        #     if length > 0:
+        #         res += (length * ( length + 1 )) >> 1
+        #         left = right
         
-        if left == 0 and nums[0] == 0:
-            return (n * (n + 1)) >> 1
+        # if left == 0 and nums[0] == 0:
+        #     return (n * (n + 1)) >> 1
         
-        length = (n-1) - left
-        res += (length * ( length + 1 )) >> 1
+        # length = (n-1) - left
+        # res += (length * ( length + 1 )) >> 1
         
+        # return res
+
+
+        zeroes_streak = res = 0
+
+        for i in range(n):
+            if nums[i] == 0:
+                zeroes_streak += 1
+            else:
+                res += (zeroes_streak * (zeroes_streak + 1)) >> 1
+
+                zeroes_streak = 0
+        
+        if zeroes_streak > 0:
+            res += (zeroes_streak * (zeroes_streak + 1)) >> 1
+    
         return res
