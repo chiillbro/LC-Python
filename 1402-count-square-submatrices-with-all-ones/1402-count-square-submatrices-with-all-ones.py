a6cc61e1df727900b1d16c3c3c9f310cc9225ee1
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-08-21 05:02:49 +0530

    Time: 48 ms (93.4%), Space: 20.3 MB (52.71%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.py
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.py
@@ -4,22 +4,39 @@ class Solution:
 
         res = 0
 
-        dp = [[0] * m for _ in range(n)]
+        # dp = [[0] * m for _ in range(n)]
 
-        for i in range(n):
-            dp[i][0] = matrix[i][0]
-            res += dp[i][0]
+        # for i in range(n):
+        #     dp[i][0] = matrix[i][0]
+        #     res += dp[i][0]
         
-        for j in range(1, m):
-            dp[0][j] = matrix[0][j]
-            res += dp[0][j]
+        # for j in range(1, m):
+        #     dp[0][j] = matrix[0][j]
+        #     res += dp[0][j]
+
+        # SPACE OPTIMIZATION
+
+        prev = 0
+        dp = [0] * (m + 1)
+
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if matrix[i-1][j-1] == 1:
+                    temp = dp[j]
+
+                    dp[j] = 1 + min(prev, dp[j-1], dp[j])
+                    prev = temp
+
+                    res += dp[j]
+                else:
+                    dp[j] = 0
         
 
-        for i in range(1, n):
-            for j in range(1, m):
-                if matrix[i][j] == 1:
-                    dp[i][j] = 1 + min(dp[i][j-1], dp[i-1][j-1], dp[i-1][j])
+        # for i in range(1, n):
+        #     for j in range(1, m):
+        #         if matrix[i][j] == 1:
+        #             dp[i][j] = 1 + min(dp[i][j-1], dp[i-1][j-1], dp[i-1][j])
                 
-                res += dp[i][j]
+        #         res += dp[i][j]
         
         return res
