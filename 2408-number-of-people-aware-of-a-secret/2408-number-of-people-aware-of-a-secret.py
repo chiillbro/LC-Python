bd96af797f7bd4ea6451eb33962463a31994bd3a
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-09-09 22:14:33 +0530

    Time: 5 ms (54.74%), Space: 18 MB (50.36%) - LeetHub

---
+++ 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.py
@@ -0,0 +1,22 @@
+class Solution:
+    def peopleAwareOfSecret(self, n: int, delay: int, forget: int) -> int:
+        know, share = deque([(1, 1)]), deque()
+
+        know_cnt, share_cnt = 1, 0
+
+        for day in range(2, n+1):
+            if know and know[0][0] == day - delay:
+                know_cnt -= know[0][1]
+                share_cnt += know[0][1]
+                share.append(know[0])
+                know.popleft()
+            
+            if share and share[0][0] == day - forget:
+                share_cnt -= share[0][1]
+                share.popleft()
+            
+            if share:
+                know_cnt += share_cnt
+                know.append((day, share_cnt))
+        
+        return (know_cnt + share_cnt) % (10**9 + 7)
