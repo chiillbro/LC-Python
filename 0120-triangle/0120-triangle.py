748619957035492637d332025cc827e077f82902
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2025-09-25 21:27:51 +0530

    Time: 3 ms (86%), Space: 18.7 MB (90.91%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -12,22 +12,29 @@ class Solution:
         
         # return backtrack(0, 0)
 
-        dp = []
-
-        for i in range(n):
-            j = len(triangle[i])
-
-            dp.append([0] * j)
+        # dp = [[0] * len(row) for row in triangle]
         
-        dp[0][0] = triangle[0][0]
+        # dp[0][0] = triangle[0][0]
+
+        prev = [triangle[0][0]]
+
+        # Follow up
 
         for i in range(1, n):
+            cur = [0] * len(triangle[i])
             for j in range(len(triangle[i])):
-                prev1 = dp[i-1][j] if j < len(triangle[i-1]) else inf
-                prev2 = dp[i-1][j-1] if j-1 >= 0 else inf
+                # par1 = dp[i-1][j] if j < len(triangle[i-1]) else inf
+                # par2 = dp[i-1][j-1] if j-1 >= 0 else inf
 
-                dp[i][j] = triangle[i][j] + min(prev1, prev2)
+                par1 = prev[j] if j < len(triangle[i-1]) else inf
+                par2 = prev[j-1] if j-1 >= 0 else inf
+
+                # dp[i][j] = triangle[i][j] + min(par1, par2)
+                cur[j] = triangle[i][j] + min(par1, par2)
+
+            prev = cur
         
 
-        return min(dp[-1])
+        # return min(dp[-1])
+        return min(prev)
 
