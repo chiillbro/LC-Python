fed1312b4f6987a6394d0082d19a69e32bd13a49
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2026-02-05 09:40:02 +0530

    Time: 62 ms (43.95%), Space: 19.3 MB (34.53%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.py
+++ 3379-transformed-array/3379-transformed-array.py
@@ -6,17 +6,18 @@ class Solution:
         for i, num in enumerate(nums):
             if num > 0:
                 idx = (num + i) % n
-
                 res.append(nums[idx])
             
             elif num < 0:
-                idx = abs(num)
-                if idx > i:
-                    idx -= i
-                    idx %= n
-                    res.append(nums[-(idx)])
-                else:
-                    res.append(nums[-(n - i + idx)])
+                idx = (n - i + abs(num)) % n
+                res.append(nums[-idx])
+                # idx = abs(num)
+                # if idx > i:
+                #     idx -= i
+                #     idx %= n
+                #     res.append(nums[-(idx)])
+                # else:
+                #     res.append(nums[-(n - i + idx)])
             
             else:
                 res.append(num)
