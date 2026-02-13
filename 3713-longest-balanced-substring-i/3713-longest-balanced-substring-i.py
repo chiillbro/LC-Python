34dd38c81cdbde6582a8a5e76b89bcbd45876dbe
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2026-02-13 23:06:17 +0530

    Time: 2023 ms (78.59%), Space: 19.1 MB (70.95%) - LeetHub

--- 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.py
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.py
@@ -2,8 +2,6 @@ class Solution:
     def longestBalanced(self, s: str) -> int:
         n = len(s)
 
-        seen = [0] * 26
-
         res = 0
 
         for i in range(n):
@@ -12,11 +10,8 @@ class Solution:
             cur = defaultdict(int)
             for j in range(i, n):
                 char = s[j]
-                char_val = ord(char) - 97
 
-                # if seen[char_val] != :
-                cur[char_val] += 1
-                # seen[char_val] = i + 1
+                cur[char] += 1
                 
                 if len(set(cur.values())) == 1:
                     res = max(res, j - i + 1)
