1783226a5adea2d7d89d0301a210163d1016a304
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2026-02-14 00:43:15 +0530

    Time: 918 ms (95.41%), Space: 19.3 MB (46.79%) - LeetHub

--- 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.py
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.py
@@ -7,13 +7,28 @@ class Solution:
         for i in range(n):
             if res >= n - i:
                 break
-            cur = defaultdict(int)
+            # cur = defaultdict(int)
+            freq = [0] * 26
+            dist  = 0
+            max_f = 0
+            cnt = 1
             for j in range(i, n):
                 char = s[j]
+                char_val = ord(char) - 97
 
-                cur[char] += 1
+                dist += freq[char_val] == 0
+
+                freq[char_val] += 1
+                max_f = max(max_f, freq[char_val])
+
+                if dist * max_f == cnt:
+                    res = max(res, cnt)
                 
-                if len(set(cur.values())) == 1:
-                    res = max(res, j - i + 1)
+                cnt += 1
+
+                # cur[char] += 1
+                
+                # if len(set(cur.values())) == 1:
+                #     res = max(res, j - i + 1)
         
         return res
