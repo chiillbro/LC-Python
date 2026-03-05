6d191b57244a577e8851992906eef4b3482e4ddc
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2026-03-06 01:53:06 +0530

    Time: 6 ms (87.57%), Space: 19.3 MB (83.83%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -2,28 +2,51 @@ class Solution:
     def minOperations(self, s: str) -> int:
         n = len(s)
 
-        str1 = "01"
-        str2 = "10"
+        # str1 = "01"
+        # str2 = "10"
 
-        if n & 1:
-            str1 += str1 * ((n-3) >> 1) + "0"
-            str2 += str2 * ((n-3) >> 1) + "1"
+        # if n & 1:
+        #     str1 += str1 * ((n-3) >> 1) + "0"
+        #     str2 += str2 * ((n-3) >> 1) + "1"
         
-        else:
-            str1 += str1 * ((n-2) >> 1)
-            str2 += str2 * ((n-2) >> 1)
+        # else:
+        #     str1 += str1 * ((n-2) >> 1)
+        #     str2 += str2 * ((n-2) >> 1)
         
-        ans1 = 0
-        ans2 = 0
+        # ans1 = 0
+        # ans2 = 0
 
-        for i in range(len(s)):
-            a, b, c = s[i], str1[i], str2[i]
+        # for i in range(len(s)):
+        #     a, b, c = s[i], str1[i], str2[i]
 
-            if a != b:
-                ans1 += 1
+        #     if a != b:
+        #         ans1 += 1
             
-            if a != c:
-                ans2 += 1
+        #     if a != c:
+        #         ans2 += 1
         
 
-        return min(ans1, ans2)
+        # return min(ans1, ans2)
+
+
+        start0 = 0
+        start1 = 0
+
+
+        for i in range(n):
+            if i & 1:
+                if s[i] == "0":
+                    start1 += 1
+                else:
+                    start0 += 1
+                
+            else:
+                if s[i] == "1":
+                    start1 += 1
+                else:
+                    start0 += 1
+        
+
+        return min(start0, start1)
+
+
